5d9b9cb0748a4833faa8702a32d24e60d4fb6518
Sebastian Schlag <sebastian.schlag@kit.edu>
2013-11-29 16:07:41 +0100

    start coarsening implementation

--- src/partition/Coarsener.h
+++ src/partition/Coarsener.h
@@ -1,17 +1,37 @@
 #ifndef PARTITION_COARSENER_H_
 #define PARTITION_COARSENER_H_
 
+#include <stack>
+
 #include "../lib/datastructure/Hypergraph.h"
 
 class Coarsener {
   typedef hgr::HypergraphType HypergraphType;
+  typedef hgr::HypergraphType::ContractionMemento Memento;
   
 public:
-  Coarsener(const HypergraphType& hypergraph) :
-      _hypergraph(hypergraph) {}
+  Coarsener(HypergraphType& hypergraph, int coarsening_limit, int threshold_node_weight) :
+      _hypergraph(hypergraph),
+      _coarsening_limit(coarsening_limit),
+      _threshold_node_weight(threshold_node_weight),
+      _coarsening_history() {}
+
+  void coarsen() {
+    _coarsening_history.push(_hypergraph.contract(0,2));
+  }
+
+  void uncoarsen() {
+    while(!_coarsening_history.empty()) {
+      _hypergraph.uncontract(_coarsening_history.top());
+      _coarsening_history.pop();
+    }
+  }
   
 private:
-  const HypergraphType& _hypergraph;
+  HypergraphType& _hypergraph;
+  const int _coarsening_limit;
+  const int _threshold_node_weight;
+  std::stack<Memento> _coarsening_history;
 };
 
 #endif  // PARTITION_COARSENER_H_
--- src/partition/coarsener_test.cc
+++ src/partition/coarsener_test.cc
@@ -11,11 +11,21 @@ class ACoarsener : public Test {
  public:
   ACoarsener() :
       hypergraph(7,4, hMetisHyperEdgeIndexVector {0,2,6,9,/*sentinel*/12},
-                 hMetisHyperEdgeVector {0,2,0,1,3,4,3,4,6,2,5,6}, nullptr, nullptr) {}
+                 hMetisHyperEdgeVector {0,2,0,1,3,4,3,4,6,2,5,6}, nullptr, nullptr),
+      coarsening_limit(2),
+      threshold_node_weight(5) {}
   
   HypergraphType hypergraph;
+  int coarsening_limit;
+  int threshold_node_weight;
 };
 
-TEST_F(ACoarsener, TakesAHypergraphAsInput) {
-  Coarsener coarsener(hypergraph);
+TEST_F(ACoarsener, TakesAHypergraphAContractionLimitAndAThresholdForNodeWeightAsInput) {
+  Coarsener coarsener(hypergraph, coarsening_limit, threshold_node_weight);
+}
+
+TEST_F(ACoarsener, UsesACoarseningHistoryToRememberAndUndoContractions) {
+  Coarsener coarsener(hypergraph, coarsening_limit, threshold_node_weight);
+  coarsener.coarsen();
+  coarsener.uncoarsen();
 }
