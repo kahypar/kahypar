88fc3e1d0536e2ab085e339ea103bcb0f4971044
Sebastian Schlag <sebastian.schlag@kit.edu>
2014-01-09 11:24:43 +0100

    add gain computation and auxilary functions as preparation for activation of border nodes

--- src/partition/TwoWayFMRefiner.h
+++ src/partition/TwoWayFMRefiner.h
@@ -1,18 +1,99 @@
 #ifndef PARTITION_TWOWAYFMREFINER_H_
 #define PARTITION_TWOWAYFMREFINER_H_
+#include <vector>
+
+#include "gtest/gtest_prod.h"
+
 #include "../lib/datastructure/Hypergraph.h"
+#include "../lib/definitions.h"
 
 namespace partition {
 using datastructure::HypergraphType;
+using defs::PartitionID;
 
 template <class Hypergraph>
 class TwoWayFMRefiner{
+ private:
+  typedef typename Hypergraph::HypernodeID HypernodeID;
+  typedef typename Hypergraph::HyperedgeID HyperedgeID;
+  typedef typename Hypergraph::HyperedgeWeight HyperedgeWeight;
+  typedef typename Hypergraph::IncidenceIterator IncidenceIterator;
+
+  const HypernodeID INVALID = std::numeric_limits<HypernodeID>::max();
+
  public:
   TwoWayFMRefiner(Hypergraph& hypergraph) :
-      _hg(hypergraph) {}
-
+      _hg(hypergraph),
+      _hyperedge_partition_sizes(2 * _hg.initialNumEdges(), INVALID){}
+  
  private:
-  Hypergraph& _hg; 
+  FRIEND_TEST(ATwoWayFMRefiner, IdentifiesBorderHypernodes);
+  FRIEND_TEST(ATwoWayFMRefiner, ComputesPartitionSizesOfHE);
+  FRIEND_TEST(ATwoWayFMRefiner, ChecksIfPartitionSizesOfHEAreAlreadyCalculated);
+  FRIEND_TEST(ATwoWayFMRefiner, ComputesGainOfHypernodeMovement);
+  
+  HyperedgeWeight gain(HypernodeID hn) {
+    HyperedgeWeight gain = 0;
+    ASSERT(_hg.partitionIndex(hn) < 2, "Trying to do gain computation for k-way partitioning");
+    PartitionID target_partition = _hg.partitionIndex(hn) ^ 1;
+    
+    forall_incident_hyperedges(he, hn, _hg) {
+      ASSERT(partitionPinCount(*he, 0) + partitionPinCount(*he, 1) > 1,
+             "Trying to compute gain for single-node HE " << *he);
+      if (partitionPinCount(*he, target_partition) == 0) {
+        gain -= _hg.edgeWeight(*he);
+      } else if (partitionPinCount(*he, _hg.partitionIndex(hn)) == 1) {
+        gain += _hg.edgeWeight(*he);
+      }
+    } endfor
+    return gain;
+  }
+  
+  bool partitionSizesCalculated(HyperedgeID he) {
+    return _hyperedge_partition_sizes[2 * he] != INVALID
+        && _hyperedge_partition_sizes[2 * he + 1] != INVALID;
+  }
+
+  void calculatePartitionSizes(HyperedgeID he) {
+    ASSERT(!partitionSizesCalculated(he), "Partition sizes already calculated for HE " << he);
+    _hyperedge_partition_sizes[2 * he] = 0;
+    _hyperedge_partition_sizes[2 * he + 1] = 0;
+    forall_pins(pin, he, _hg) {
+      if (_hg.partitionIndex(*pin) == 0) {
+        ++_hyperedge_partition_sizes[2 * he];
+      } else {
+        ++_hyperedge_partition_sizes[2 * he + 1];
+      }
+    } endfor
+  }
+
+  HypernodeID partitionPinCount(HyperedgeID he, PartitionID id) {
+    ASSERT(he < _hg.initialNumEdges(), "Invalid HE " << he);
+    if (!partitionSizesCalculated(he)) {
+      calculatePartitionSizes(he);
+    }
+    return _hyperedge_partition_sizes[2 * he + id];
+  }
+  
+  bool isBorderNode(HypernodeID hn) {
+    bool is_border_node = false;
+    PartitionID partition = _hg.partitionIndex(hn);
+    forall_incident_hyperedges(he, hn, _hg) {
+      forall_pins(pin, *he, _hg) {
+        if (partition != _hg.partitionIndex(*pin)) {
+          is_border_node = true;
+          break;
+        } 
+      } endfor
+      if (is_border_node) {
+        break;
+      }
+    } endfor
+    return is_border_node;
+  }
+  
+  Hypergraph& _hg;
+  std::vector<HypernodeID> _hyperedge_partition_sizes;
 };
 
 } // namespace partition
--- src/partition/two_way_fm_refiner_test.cc
+++ src/partition/two_way_fm_refiner_test.cc
@@ -5,6 +5,7 @@
 
 namespace partition {
 using ::testing::Test;
+using ::testing::Eq;
 
 using datastructure::HypergraphType;
 using datastructure::HyperedgeIndexVector;
@@ -15,13 +16,46 @@ class ATwoWayFMRefiner : public Test {
   ATwoWayFMRefiner() :
       hypergraph(7,4, HyperedgeIndexVector {0,2,6,9,/*sentinel*/12},
                  HyperedgeVector {0,2,0,1,3,4,3,4,6,2,5,6}),
-      refiner(hypergraph) {}
+      refiner(hypergraph) {
+    hypergraph.setPartitionIndex(0,0);
+    hypergraph.setPartitionIndex(1,1);
+    hypergraph.setPartitionIndex(2,1);
+    hypergraph.setPartitionIndex(3,0);
+    hypergraph.setPartitionIndex(4,0);
+    hypergraph.setPartitionIndex(5,1);
+    hypergraph.setPartitionIndex(6,1);
+  }
 
   HypergraphType hypergraph;
   TwoWayFMRefiner<HypergraphType> refiner;
 };
 
-TEST_F(ATwoWayFMRefiner, TakesAHypergraphAsInput) {
+TEST_F(ATwoWayFMRefiner, IdentifiesBorderHypernodes) {
+  ASSERT_THAT(refiner.isBorderNode(0), Eq(true));
+  ASSERT_THAT(refiner.isBorderNode(1), Eq(true));
+  ASSERT_THAT(refiner.isBorderNode(5), Eq(false));
 }
 
+TEST_F(ATwoWayFMRefiner, ComputesGainOfHypernodeMovement) {
+  ASSERT_THAT(refiner.gain(1), Eq(1));
+  ASSERT_THAT(refiner.gain(5), Eq(-1));
+}
+
+TEST_F(ATwoWayFMRefiner, ComputesPartitionSizesOfHE) {
+  refiner.calculatePartitionSizes(0);
+  ASSERT_THAT(refiner.partitionPinCount(0, 0), Eq(1));
+  ASSERT_THAT(refiner.partitionPinCount(0 ,1), Eq(1));
+
+  refiner.calculatePartitionSizes(3);
+  ASSERT_THAT(refiner.partitionPinCount(3, 0), Eq(0));
+  ASSERT_THAT(refiner.partitionPinCount(3, 1), Eq(3));
+}
+
+TEST_F(ATwoWayFMRefiner, ChecksIfPartitionSizesOfHEAreAlreadyCalculated) {
+  ASSERT_THAT(refiner.partitionSizesCalculated(0), Eq(false));
+
+  refiner.calculatePartitionSizes(0);
+
+  ASSERT_THAT(refiner.partitionSizesCalculated(0), Eq(true));
+}
 } // namespace partition
