c10cfce587d04d638e2f1fef6fce584141a2510d
Sebastian Schlag <sebastian.schlag@kit.edu>
2014-01-20 12:00:34 +0100

    new random function for seeds of initial partitioning

--- src/tools/RandomFunctions.h
+++ src/tools/RandomFunctions.h
@@ -2,30 +2,37 @@
 #define TOOLS_RANDOMFUNCTIONS_H_
 
 #include <ctime>
+#include <limits>
 
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/uniform_int.hpp>
 
 class Randomize {
- private:
-  static int _seed;
-  static boost::random::mt19937 _gen;
-  static boost::random::uniform_int_distribution<> _dist;
-
  public:
   static bool flipCoin() {
-    return static_cast<bool>(_dist(_gen));
+    return static_cast<bool>(_bool_dist(_gen));
   }
 
+  static int newRandomSeed() {
+    return _int_dist(_gen);
+  }
+  
   static void setSeed(int seed) {
     _seed = seed;
     _gen.seed(_seed);
   }
+  
+ private:
+  static int _seed;
+  static boost::random::mt19937 _gen;
+  static boost::random::uniform_int_distribution<int> _bool_dist;
+  static boost::random::uniform_int_distribution<int> _int_dist;
 
 };
 
   int Randomize::_seed = -1;
   boost::random::mt19937 Randomize::_gen;
-  boost::random::uniform_int_distribution<> Randomize::_dist(0,1);
+  boost::random::uniform_int_distribution<int> Randomize::_bool_dist(0,1);
+  boost::random::uniform_int_distribution<int> Randomize::_int_dist(0,std::numeric_limits<int>::max());
 
 #endif  // TOOLS_RANDOMFUNCTIONS_H_
