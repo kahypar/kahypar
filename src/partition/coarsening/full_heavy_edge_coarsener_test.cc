4ff45907c93705d610c0471413b3f658b963a1d7
Sebastian Schlag <sebastian.schlag@kit.edu>
2014-02-06 14:08:20 +0100

    further refactor fixtures

--- src/partition/coarsening/full_heavy_edge_coarsener_test.cc
+++ src/partition/coarsening/full_heavy_edge_coarsener_test.cc
@@ -35,16 +35,17 @@ class DummyRefiner : public IRefiner<Hypergraph>{
 
 class ACoarsener : public Test {
   public:
-  ACoarsener() :
-    hypergraph(7, 4, HyperedgeIndexVector { 0, 2, 6, 9, /*sentinel*/ 12 },
-               HyperedgeVector { 0, 2, 0, 1, 3, 4, 3, 4, 6, 2, 5, 6 }),
+  explicit ACoarsener(HypergraphType* graph =
+                        new HypergraphType(7, 4, HyperedgeIndexVector { 0, 2, 6, 9, /*sentinel*/ 12 },
+                                           HyperedgeVector { 0, 2, 0, 1, 3, 4, 3, 4, 6, 2, 5, 6 })) :
+    hypergraph(graph),
     config(),
-    coarsener(hypergraph, config),
+    coarsener(*hypergraph, config),
     refiner(new DummyRefiner<HypergraphType>()) {
     config.coarsening.threshold_node_weight = 5;
   }
 
-  HypergraphType hypergraph;
+  std::unique_ptr<HypergraphType> hypergraph;
   Configuration<HypergraphType> config;
   CoarsenerType coarsener;
   std::unique_ptr<IRefiner<HypergraphType> > refiner;
@@ -63,62 +64,62 @@ class ACoarsenerWithThresholdWeight3 : public ACoarsener {
 
 TEST_F(ACoarsener, RemovesHyperedgesOfSizeOneDuringCoarsening) {
   coarsener.coarsen(2);
-  ASSERT_THAT(hypergraph.edgeIsEnabled(0), Eq(false));
-  ASSERT_THAT(hypergraph.edgeIsEnabled(2), Eq(false));
+  ASSERT_THAT(hypergraph->edgeIsEnabled(0), Eq(false));
+  ASSERT_THAT(hypergraph->edgeIsEnabled(2), Eq(false));
 }
 
 TEST_F(ACoarsener, DecreasesNumberOfPinsWhenRemovingHyperedgesOfSizeOne) {
   coarsener.coarsen(6);
-  ASSERT_THAT(hypergraph.edgeIsEnabled(0), Eq(false));
+  ASSERT_THAT(hypergraph->edgeIsEnabled(0), Eq(false));
 
-  ASSERT_THAT(hypergraph.numPins(), Eq(10));
+  ASSERT_THAT(hypergraph->numPins(), Eq(10));
 }
 
 TEST_F(ACoarsener, ReAddsHyperedgesOfSizeOneDuringUncoarsening) {
   coarsener.coarsen(2);
-  ASSERT_THAT(hypergraph.edgeIsEnabled(0), Eq(false));
-  ASSERT_THAT(hypergraph.edgeIsEnabled(2), Eq(false));
+  ASSERT_THAT(hypergraph->edgeIsEnabled(0), Eq(false));
+  ASSERT_THAT(hypergraph->edgeIsEnabled(2), Eq(false));
   coarsener.uncoarsen(*refiner);
-  ASSERT_THAT(hypergraph.edgeIsEnabled(0), Eq(true));
-  ASSERT_THAT(hypergraph.edgeIsEnabled(2), Eq(true));
-  ASSERT_THAT(hypergraph.edgeSize(1), Eq(4));
-  ASSERT_THAT(hypergraph.edgeSize(3), Eq(3));
+  ASSERT_THAT(hypergraph->edgeIsEnabled(0), Eq(true));
+  ASSERT_THAT(hypergraph->edgeIsEnabled(2), Eq(true));
+  ASSERT_THAT(hypergraph->edgeSize(1), Eq(4));
+  ASSERT_THAT(hypergraph->edgeSize(3), Eq(3));
 }
 
 TEST_F(ACoarsener, SelectsNodePairToContractBasedOnHighestRating) {
   coarsener.coarsen(6);
-  ASSERT_THAT(hypergraph.nodeIsEnabled(2), Eq(false));
+  ASSERT_THAT(hypergraph->nodeIsEnabled(2), Eq(false));
   ASSERT_THAT(coarsener._history.top().contraction_memento.u, Eq(0));
   ASSERT_THAT(coarsener._history.top().contraction_memento.v, Eq(2));
 }
 
 TEST_F(ACoarsener, RemovesParallelHyperedgesDuringCoarsening) {
   coarsener.coarsen(2);
-  ASSERT_THAT(hypergraph.edgeIsEnabled(3), Eq(false));
-  ASSERT_THAT(hypergraph.edgeIsEnabled(1), Eq(true));
+  ASSERT_THAT(hypergraph->edgeIsEnabled(3), Eq(false));
+  ASSERT_THAT(hypergraph->edgeIsEnabled(1), Eq(true));
 }
 
 TEST_F(ACoarsener, UpdatesEdgeWeightOfRepresentativeHyperedgeOnParallelHyperedgeRemoval) {
   coarsener.coarsen(2);
-  ASSERT_THAT(hypergraph.edgeWeight(1), Eq(2));
+  ASSERT_THAT(hypergraph->edgeWeight(1), Eq(2));
 }
 TEST_F(ACoarsener, DecreasesNumberOfHyperedgesOnParallelHyperedgeRemoval) {
   coarsener.coarsen(2);
-  ASSERT_THAT(hypergraph.numEdges(), Eq(1));
+  ASSERT_THAT(hypergraph->numEdges(), Eq(1));
 }
 
 TEST_F(ACoarsener, DecreasesNumberOfPinsOnParallelHyperedgeRemoval) {
   coarsener.coarsen(2);
-  ASSERT_THAT(hypergraph.numPins(), Eq(2));
+  ASSERT_THAT(hypergraph->numPins(), Eq(2));
 }
 
 TEST_F(ACoarsener, RestoresParallelHyperedgesDuringUncoarsening) {
   coarsener.coarsen(2);
   coarsener.uncoarsen(*refiner);
-  ASSERT_THAT(hypergraph.edgeSize(1), Eq(4));
-  ASSERT_THAT(hypergraph.edgeSize(3), Eq(3));
-  ASSERT_THAT(hypergraph.edgeWeight(1), Eq(1));
-  ASSERT_THAT(hypergraph.edgeWeight(3), Eq(1));
+  ASSERT_THAT(hypergraph->edgeSize(1), Eq(4));
+  ASSERT_THAT(hypergraph->edgeSize(3), Eq(3));
+  ASSERT_THAT(hypergraph->edgeWeight(1), Eq(1));
+  ASSERT_THAT(hypergraph->edgeWeight(3), Eq(1));
 }
 
 TEST(AnUncoarseningOperation, RestoresParallelHyperedgesInReverseOrder) {
@@ -167,9 +168,10 @@ TEST(AnUncoarseningOperation, RestoresSingleNodeHyperedgesInReverseOrder) {
 
 TEST_F(ACoarsenerWithThresholdWeight3, DoesNotCoarsenUntilCoarseningLimit) {
   coarsener.coarsen(2);
-  forall_hypernodes(hn, hypergraph) {
-    ASSERT_THAT(hypergraph.nodeWeight(*hn), Le(3));
+  HypergraphType& hgr = *hypergraph;
+  forall_hypernodes(hn, hgr) {
+    ASSERT_THAT(hgr.nodeWeight(*hn), Le(3));
   } endfor
-    ASSERT_THAT(hypergraph.numNodes(), Eq(3));
+    ASSERT_THAT(hgr.numNodes(), Eq(3));
 }
 } // namespace partition
