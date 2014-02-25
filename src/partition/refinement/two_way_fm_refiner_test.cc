kahypar/kahypar

commit fec3d61397068a8f28e0e0b75c321856bf212468

fix updating of neighbour-nodes by remembering just activated nodes
Previous version contained a bug in updateNeighbours which lead to
incorrect gain calculation. Applying delta-Gain changes is only correct for
nodes that were already contained in the PQ when the max_gain node was
moved (e.g. it is not correct for newly added (i.e. activated) nodes).
However the updatePin method also accounts for nodes that become
activated due to the current node movement: These nodes are activated
and inserted into the PQ. However, if such a node is incident to more
than one HE that is considered during updateNeighbours, this leads
to the implicit assumption that the node should be updated via
delta-gain updates (since it was only checked whether or not the node is
already in the PQ and not whether or not the node was already active
before the current max_gain_node movement).

Bug was found by assertion that the current cut (calculated by adding
the gain changes) has to match the cut induced by the current hypergraph
configuration.

This commit fixes this bug by introducing a bitvector to remember newly
added nodes which are then excluded from delta gain update.

src/partition/TwoWayFMRefiner.h
@@ -39,6 +39,7 @@ class TwoWayFMRefiner{
       // ToDo: We could also use different storage to avoid initialization like this
       _pq{new RefinementPQ(_hg.initialNumNodes()), new RefinementPQ(_hg.initialNumNodes())},
     _marked(_hg.initialNumNodes()),
+     _just_activated(_hg.initialNumNodes()),
     _performed_moves() {
       _performed_moves.reserve(_hg.initialNumNodes());
     }
@@ -175,6 +176,7 @@ class TwoWayFMRefiner{
   }
 
   void updateNeighbours(HypernodeID moved_node, PartitionID from, PartitionID to) {
+    _just_activated.reset();
     forall_incident_hyperedges(he, moved_node, _hg) {
       HypernodeID new_size0 = _hg.pinCountInPartition(*he, 0);
       HypernodeID new_size1 = _hg.pinCountInPartition(*he, 1);
@@ -255,21 +257,21 @@ class TwoWayFMRefiner{
     return (old_size0 == 1 && new_size0 == 2) || (old_size1 == 1 && new_size1 == 2);
   }
 
-  // need a better method name here!
   void updatePin(HyperedgeID he, HypernodeID pin, Gain sign) {
     if (_pq[_hg.partitionIndex(pin)]->contains(pin)) {
-      if (isBorderNode(he)) {
+      if (isBorderNode(he) && !_just_activated[pin]) {
         Gain old_gain = _pq[_hg.partitionIndex(pin)]->key(pin);
         Gain gain_delta = sign * _hg.edgeWeight(he);
-        // PRINT("*** updating gain of HN " << pin << " from gain " << old_gain
-        //       << " to " << old_gain + gain_delta << " in PQ " << _hg.partitionIndex(pin));
+         // PRINT("*** updating gain of HN " << pin << " from gain " << old_gain
+         //       << " to " << old_gain + gain_delta << " in PQ " << _hg.partitionIndex(pin));
         _pq[_hg.partitionIndex(pin)]->updateKey(pin, old_gain + gain_delta);
       } else {
-        // PRINT("*** deleting pin " << pin << " from PQ " << _hg.partitionIndex(pin));
+         // PRINT("*** deleting pin " << pin << " from PQ " << _hg.partitionIndex(pin));
         _pq[_hg.partitionIndex(pin)]->remove(pin);
       }
     } else {
         activate(pin);
+        _just_activated[pin] = true;
     }
   }
 
@@ -316,6 +318,7 @@ class TwoWayFMRefiner{
   std::vector<int> _hyperedge_partition_sizes;
   std::array<RefinementPQ*,NUM_PQS> _pq;
   boost::dynamic_bitset<uint64_t> _marked;
+  boost::dynamic_bitset<uint64_t> _just_activated;
   std::vector<HypernodeID> _performed_moves;
 };
 

commit c08de50e51264327708f1c48f7c5097945009a03

BUGFIX: do not directly delete newly inserted nodes!
While fixing one bug, commit fec3d61 introduced yet another.
Combing the two checks into one bool expression lead to the problem
that _valid_ just activated hypernodes potentially got removed directly
after adding them to the corresponding PQ.

This commit fixes this bug and adds a testcast to verify the intended behavior.

src/partition/refinement/TwoWayFMRefiner.h
@@ -255,6 +255,7 @@ class TwoWayFMRefiner : public IRefiner<Hypergraph>{
   FRIEND_TEST(AGainUpdateMethod, ActivatesUnmarkedNeighbors);
   FRIEND_TEST(AGainUpdateMethod, RemovesNonBorderNodesFromPQ);
   FRIEND_TEST(ATwoWayFMRefiner, UpdatesPartitionWeightsOnRollBack);
+  FRIEND_TEST(AGainUpdateMethod, DoesNotDeleteJustActivatedNodes);
 
   bool queuesAreEmpty() const {
     return _pq[0]->empty() && _pq[1]->empty();
@@ -316,13 +317,15 @@ class TwoWayFMRefiner : public IRefiner<Hypergraph>{
 
   void updatePin(HyperedgeID he, HypernodeID pin, Gain sign) {
     if (_pq[_hg.partitionIndex(pin)]->contains(pin)) {
-      if (isBorderNode(pin) && !_just_activated[pin]) {
-        Gain old_gain = _pq[_hg.partitionIndex(pin)]->key(pin);
-        Gain gain_delta = sign * _hg.edgeWeight(he);
-        DBG(dbg_refinement_2way_fm_gain_update, "TwoWayFM updating gain of HN " << pin
-            << " from gain " << old_gain << " to " << old_gain + gain_delta << " in PQ "
-            << _hg.partitionIndex(pin));
-        _pq[_hg.partitionIndex(pin)]->updateKey(pin, old_gain + gain_delta);
+      if (isBorderNode(pin)) {
+        if (!_just_activated[pin]) {
+          Gain old_gain = _pq[_hg.partitionIndex(pin)]->key(pin);
+          Gain gain_delta = sign * _hg.edgeWeight(he);
+          DBG(dbg_refinement_2way_fm_gain_update, "TwoWayFM updating gain of HN " << pin
+              << " from gain " << old_gain << " to " << old_gain + gain_delta << " in PQ "
+              << _hg.partitionIndex(pin));
+          _pq[_hg.partitionIndex(pin)]->updateKey(pin, old_gain + gain_delta);
+        }
       } else {
         DBG(dbg_refinement_2way_fm_gain_update, "TwoWayFM deleting pin " << pin << " from PQ "
             << _hg.partitionIndex(pin));

src/partition/refinement/two_way_fm_refiner_test.cc
@@ -373,4 +373,24 @@ TEST_F(AGainUpdateMethod, ActivatesUnmarkedNeighbors) {
   ASSERT_THAT(refiner._pq[0]->contains(2), Eq(true));
   ASSERT_THAT(refiner._pq[0]->key(2), Eq(0));
 }
+
+TEST_F(AGainUpdateMethod, DoesNotDeleteJustActivatedNodes) {
+  HypergraphType hypergraph(5, 3, HyperedgeIndexVector { 0, 2, 5, 8 },
+                            HyperedgeVector { 0, 1, 2, 3, 4, 2, 3, 4 });
+  hypergraph.changeNodePartition(0, INVALID_PARTITION, 0);
+  hypergraph.changeNodePartition(1, INVALID_PARTITION, 0);
+  hypergraph.changeNodePartition(2, INVALID_PARTITION, 0);
+  hypergraph.changeNodePartition(3, INVALID_PARTITION, 1);
+  hypergraph.changeNodePartition(4, INVALID_PARTITION, 0);
+  TwoWayFMRefinerSimpleStopping refiner(hypergraph, config);
+  refiner.initialize();
+
+  // bypassing activate
+  refiner._pq[0]->insert(2, refiner.computeGain(2));
+  refiner.moveHypernode(2, 0, 1);
+  refiner.updateNeighbours(2, 0, 1);
+
+  ASSERT_THAT(refiner._pq[0]->contains(4), Eq(true));
+  ASSERT_THAT(refiner._pq[1]->contains(3), Eq(true));
+}
 } // namespace partition