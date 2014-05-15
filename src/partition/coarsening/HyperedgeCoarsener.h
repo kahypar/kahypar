4dd878d339dee500109bdbbbd9a5b381058dac1a
Sebastian Schlag <sebastian.schlag@kit.edu>
2014-05-15 15:01:44 +0200

    start coarsening implementation
    currently implemented:
    rating
    contraction
    single-HE removal
    
    Good think about the way we contract a Hyperedge is that we don't need
    to explicitly remove nested ones. Since we contract the pins one by one
    all nested HEs will end up as single-pin HEs on the representative hypernode

--- src/partition/coarsening/HyperedgeCoarsener.h
+++ src/partition/coarsening/HyperedgeCoarsener.h
@@ -5,19 +5,131 @@
 #ifndef SRC_PARTITION_COARSENING_HYPEREDGECOARSENER_H_
 #define SRC_PARTITION_COARSENING_HYPEREDGECOARSENER_H_
 
+#include <vector>
+
+#include "lib/datastructure/Hypergraph.h"
+#include "lib/datastructure/PriorityQueue.h"
+#include "partition/Configuration.h"
+#include "partition/coarsening/CoarsenerBase.h"
 #include "partition/coarsening/ICoarsener.h"
 #include "partition/refinement/IRefiner.h"
+#include "tools/RandomFunctions.h"
+
+using datastructure::HypergraphType;
+using datastructure::PriorityQueue;
+using datastructure::MetaKeyDouble;
+using datastructure::HypernodeID;
+using datastructure::HyperedgeID;
 
 namespace partition {
-class HyperedgeCoarsener : public ICoarsener {
-  public:
-  HyperedgeCoarsener() { }
+struct HyperedgeCoarseningMemento {
+  int one_pin_hes_begin;        // start of removed single pin hyperedges
+  int one_pin_hes_size;         // # removed single pin hyperedges
+  int parallel_hes_begin;       // start of removed parallel hyperedges
+  int parallel_hes_size;        // # removed parallel hyperedges
+  int mementos_begin;           // start of mementos corresponding to HE contraction
+  int mementos_size;            // # mementos
+  explicit HyperedgeCoarseningMemento() :
+    one_pin_hes_begin(0),
+    one_pin_hes_size(0),
+    parallel_hes_begin(0),
+    parallel_hes_size(0),
+    mementos_begin(0),
+    mementos_size(0) { }
+};
 
-  void coarsen(int limit) { }
+template <class Rater>
+class HyperedgeCoarsener : public ICoarsener,
+                           private CoarsenerBase<HyperedgeCoarseningMemento>{
+  private:
+  typedef typename Rater::RatingType RatingType;
+  typedef typename HypergraphType::ContractionMemento ContractionMemento;
+  bool dbg_coarsening_coarsen = true;
+
+  public:
+  using CoarsenerBase::removeSingleNodeHyperedges;
+
+  HyperedgeCoarsener(HypergraphType& hypergraph, const Configuration& config) :
+    CoarsenerBase(hypergraph, config),
+    _rater(),
+    _pq(_hg.initialNumEdges()),
+    _contraction_mementos() { }
+
+  void coarsen(int limit) {
+    _pq.clear();
+    rateAllHyperedges();
+
+    HyperedgeID he_to_contract;
+    while (!_pq.empty() && _hg.numNodes() > limit) {
+      he_to_contract = _pq.max();
+      DBG(dbg_coarsening_coarsen, "Contracting HE" << he_to_contract << " prio: " << _pq.maxKey());
+      ASSERT(_hg.numNodes() - _hg.edgeSize(he_to_contract) + 1 >= limit,
+             " Contraction of HE " << he_to_contract << " violates contraction limit: "
+             << (_hg.numNodes() - _hg.edgeSize(he_to_contract) + 1) << " < " << limit);
+
+      HypernodeID rep_node = performContraction(he_to_contract);
+      _pq.remove(he_to_contract);
+
+      removeSingleNodeHyperedges(rep_node);
+
+      // remove parallel HEs
+      // remove nested HEs
+      // rerate incident hyperedges
+      // do we need to rerate hyperedges affected by parallel HE removal?
+
+      // we need a new kind of Coarsening-Memento that keeps track of:
+      // _removed_single_node_hyperedges
+      // _remvoed_parallel_hyperedges
+      // _contraction_mementos (If it turns our that hyperedge coarsening is a valid approach, then we might add special HE-contraction
+      // functionality to the hypergraph-DS)
+
+      // think about moving base functionality currently in HeavyEdgeCoarsenerBase up one level to CoarsenerBase for better reuse
+    }
+  }
 
   void uncoarsen(IRefiner& refiner) { }
 
   private:
+  void rateAllHyperedges() {
+    std::vector<HyperedgeID> permutation;
+    permutation.reserve(_hg.initialNumNodes());
+    forall_hyperedges(he, _hg) {
+      permutation.push_back(*he);
+    } endfor
+    Randomize::shuffleVector(permutation);
+
+    RatingType rating = 0.0;
+    for (auto he : permutation) {
+      rating = _rater.rate(he, _hg, _config.coarsening.threshold_node_weight);
+      if (rating != Rater::INVALID_RATING) {
+        DBG(true, "Inserting HE " << he << " rating=" << rating);
+        _pq.insert(he, rating);
+      }
+    }
+  }
+
+  HypernodeID performContraction(HyperedgeID he) {
+    IncidenceIterator pins_begin, pins_end;
+    std::tie(pins_begin, pins_end) = _hg.pins(he);
+    HypernodeID representative = *pins_begin;
+    ++pins_begin;
+    //TODO(schlag): modify hypergraph DS such that we can do index-based iteration over incidence array
+    //              via custom iterator (so that we do not need to collect the IDs to contract upfront).
+    std::vector<HypernodeID> hns_to_contract;
+    while (pins_begin != pins_end) {
+      hns_to_contract.push_back(*pins_begin);
+      ++pins_begin;
+    }
+    for (auto hn_to_contract : hns_to_contract) {
+      DBG(true, "Contracting (" << representative << "," << hn_to_contract << ") from HE " << he);
+      _contraction_mementos.push_back(_hg.contract(representative, hn_to_contract));
+    }
+    return representative;
+  }
+
+  Rater _rater;
+  PriorityQueue<HyperedgeID, RatingType, MetaKeyDouble> _pq;
+  std::vector<ContractionMemento> _contraction_mementos;
 };
 } // namespace partition
 
--- src/partition/coarsening/hyperedge_coarsener_test.cc
+++ src/partition/coarsening/hyperedge_coarsener_test.cc
@@ -4,10 +4,46 @@
 
 #include "gmock/gmock.h"
 
+#include "lib/datastructure/Hypergraph.h"
+#include "lib/definitions.h"
+#include "partition/Configuration.h"
+#include "partition/coarsening/HeavyEdgeCoarsener_TestFixtures.h"
 #include "partition/coarsening/HyperedgeCoarsener.h"
+#include "partition/coarsening/Rater.h"
 
 namespace partition {
-TEST(AHyperedgeCoarsener, CanBeInstanciated) {
-  HyperedgeCoarsener hec;
+class AHyperedgeCoarsener : public Test {
+  public:
+  explicit AHyperedgeCoarsener(HypergraphType* graph =
+                                 new HypergraphType(7, 4, HyperedgeIndexVector { 0, 2, 6, 9, /*sentinel*/ 12 },
+                                                    HyperedgeVector { 0, 2, 0, 1, 3, 4, 3, 4, 6, 2, 5, 6 })) :
+    hypergraph(graph),
+    config(),
+    coarsener(*hypergraph, config),
+    refiner(new DummyRefiner()) {
+    config.coarsening.threshold_node_weight = 5;
+  }
+
+  std::unique_ptr<HypergraphType> hypergraph;
+  Configuration config;
+  HyperedgeCoarsener<HyperedgeRater<defs::RatingType> > coarsener;
+  std::unique_ptr<IRefiner> refiner;
+
+  private:
+  DISALLOW_COPY_AND_ASSIGN(AHyperedgeCoarsener);
+};
+
+TEST(AHyperedgeCoarsener, RemovesNestedHyperedgesAsPartOfTheContractionRoutine) {
+  HypergraphType hypergraph(5, 4, HyperedgeIndexVector { 0, 3, 7, 9, /*sentinel*/ 11 },
+                            HyperedgeVector { 0, 1, 2, 0, 1, 2, 3, 2, 3, 3, 4 });
+  hypergraph.setEdgeWeight(1, 5);
+  Configuration config;
+  config.coarsening.threshold_node_weight = 5;
+  HyperedgeCoarsener<HyperedgeRater<defs::RatingType> > coarsener(hypergraph, config);
+
+  coarsener.coarsen(2);
+  ASSERT_THAT(hypergraph.edgeIsEnabled(0), Eq(false));
+  ASSERT_THAT(hypergraph.edgeIsEnabled(1), Eq(false));
+  ASSERT_THAT(hypergraph.edgeIsEnabled(2), Eq(false));
 }
 } // namespace partition
