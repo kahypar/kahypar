kahypar/kahypar

commit 1da08861ac4fd2d920e01444884b29e98d6a4ff2

ensure consistency between pq values and hypergraph state on all coarseners

src/partition/coarsening/HeuristicHeavyEdgeCoarsener.h
@@ -63,6 +63,9 @@ class HeuristicHeavyEdgeCoarsener : public ICoarsener,
       ASSERT(_hg.nodeWeight(rep_node) + _hg.nodeWeight(target[rep_node])
              <= _rater.thresholdNodeWeight(),
              "Trying to contract nodes violating maximum node weight");
+      ASSERT(_pq.maxKey() == _rater.rate(rep_node).value,
+             "Key in PQ != rating calculated by rater:" << _pq.maxKey() << "!="
+             << _rater.rate(rep_node).value);
 
       performContraction(rep_node, contracted_node);
       _pq.remove(contracted_node);

src/partition/coarsening/FullHeavyEdgeCoarsener.h
@@ -66,6 +66,9 @@ class FullHeavyEdgeCoarsener : public ICoarsener,
       ASSERT(_hg.nodeWeight(rep_node) + _hg.nodeWeight(target[rep_node])
              <= _rater.thresholdNodeWeight(),
              "Trying to contract nodes violating maximum node weight");
+      ASSERT(_pq.maxKey() == _rater.rate(rep_node).value,
+             "Key in PQ != rating calculated by rater:" << _pq.maxKey() << "!="
+             << _rater.rate(rep_node).value);
 
       performContraction(rep_node, contracted_node);
       _pq.remove(contracted_node);

src/partition/coarsening/HyperedgeCoarsener.h
@@ -78,6 +78,11 @@ class HyperedgeCoarsener : public ICoarsener,
              " Contraction of HE " << he_to_contract << " violates contraction limit: "
              << (_hg.numNodes() - _hg.edgeSize(he_to_contract) + 1) << " < " << limit);
 
+      ASSERT(_pq.maxKey() == _rater.rate(he_to_contract, _hg,
+                                         _config.coarsening.threshold_node_weight).value,
+             "Key in PQ != rating calculated by rater:" << _pq.maxKey() << "!="
+             << _rater.rate(he_to_contract, _hg, _config.coarsening.threshold_node_weight).value);
+
       //ToDo(schlag): If contraction would lead to too few hypernodes, we are not allowed to contract
       //              this HE. Instead we just remove it from the PQ? -> make a testcase!
       //              Or do we just say we coarsen until there are no more than 150 nodes left?

commit e42914c54ab5e40359f82ca8cd284aa989dbe38f

Uncomment assertion for rating consistency
This assertion was introduced with commit
1da08861ac4fd2d920e01444884b29e98d6a4ff2
as part of a consolidation effort. However it is in the nature of the
heuristic heavy-edge coarsener to __not__ update the ratings of all
hypernodes. Instead it updates only the ratings for hypernodes that are
directly affected by the contraction (i.e. the hypernodes that had
either the representative or the contracted node as contraction target).
Therefore ratings might become slightly inconsistent during coarsening.

src/partition/coarsening/HeuristicHeavyEdgeCoarsener.h
@@ -66,9 +66,13 @@ class HeuristicHeavyEdgeCoarsener : public ICoarsener,
       ASSERT(_hg.nodeWeight(rep_node) + _hg.nodeWeight(target[rep_node])
              <= _rater.thresholdNodeWeight(),
              "Trying to contract nodes violating maximum node weight");
-      ASSERT(_pq.maxKey() == _rater.rate(rep_node).value,
-             "Key in PQ != rating calculated by rater:" << _pq.maxKey() << "!="
-             << _rater.rate(rep_node).value);
+      // The heuristic heavy edge coarsener does not update __all__ hypernodes
+      // that should be updated. It only updates those nodes, which have the
+      // contracted hypernode as target. Therefore this assertion does __not__
+      // hold for the heuristic coarsener!
+      // ASSERT(_pq.maxKey() == _rater.rate(rep_node).value,
+      //        "Key in PQ != rating calculated by rater:" << _pq.maxKey() << "!="
+      //        << _rater.rate(rep_node).value);
 
       performContraction(rep_node, contracted_node);
       _pq.remove(contracted_node);