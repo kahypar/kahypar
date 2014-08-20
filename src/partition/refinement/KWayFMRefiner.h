e56f655ff0ad643ce36a2ad64bdabcd667638a52
Sebastian Schlag <sebastian.schlag@kit.edu>
2014-08-20 15:03:48 +0200

    introduce kway refiner

---
+++ src/partition/refinement/KWayFMRefiner.h
@@ -0,0 +1,106 @@
+/***************************************************************************
+ *  Copyright (C) 2014 Sebastian Schlag <sebastian.schlag@kit.edu>
+ **************************************************************************/
+
+#ifndef SRC_PARTITION_REFINEMENT_KWAYFMREFINER_H_
+#define SRC_PARTITION_REFINEMENT_KWAYFMREFINER_H_
+
+#include <limits>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "gtest/gtest_prod.h"
+
+#include "lib/TemplateParameterToString.h"
+#include "lib/core/Mandatory.h"
+#include "lib/datastructure/PriorityQueue.h"
+#include "lib/definitions.h"
+#include "partition/Configuration.h"
+#include "partition/refinement/IRefiner.h"
+#include "tools/RandomFunctions.h"
+
+using datastructure::PriorityQueue;
+
+using defs::Hypergraph;
+using defs::HypernodeID;
+using defs::HyperedgeID;
+using defs::PartitionID;
+using defs::HyperedgeWeight;
+using defs::HypernodeWeight;
+
+namespace partition {
+template <class StoppingPolicy = Mandatory>
+class KWayFMRefiner : public IRefiner {
+  typedef HyperedgeWeight Gain;
+  typedef std::pair<Gain, PartitionID> GainPartitionPair;
+
+  public:
+  KWayFMRefiner(Hypergraph& hypergraph, const Configuration& config) :
+    _hg(hypergraph),
+    _config(config),
+    _stats(),
+    _tmp_gains(_config.partitioning.k, 0) { }
+
+  void refineImpl(std::vector<HypernodeID>& refinement_nodes, size_t num_refinement_nodes,
+                  HyperedgeWeight& best_cut, double max_imbalance, double& best_imbalance) final { }
+
+  int numRepetitionsImpl() const final {
+    return _config.two_way_fm.num_repetitions;
+  }
+
+  std::string policyStringImpl() const final {
+    return std::string(" StoppingPolicy=" + templateToString<StoppingPolicy>());
+  }
+
+  const Stats & statsImpl() const {
+    return _stats;
+  }
+
+  private:
+  FRIEND_TEST(AKWayFMRefiner, IdentifiesBorderHypernodes);
+  FRIEND_TEST(AKWayFMRefiner, ComputesGainOfHypernodeMoves);
+
+  bool isBorderNode(HypernodeID hn) const {
+    for (auto && he : _hg.incidentEdges(hn)) {
+      if (_hg.pinCountInPart(he, _hg.partID(hn)) < _hg.edgeSize(he)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  GainPartitionPair computeMaxGain(HypernodeID hn) {
+    for (auto && he : _hg.incidentEdges(hn)) {
+      for (PartitionID i = 0; i != _config.partitioning.k; ++i) {
+        if (_hg.pinCountInPart(he, i) == 0) {
+          ASSERT(i != _hg.partID(hn), "inconsistent Hypergraph-DS");
+          _tmp_gains[i] -= _hg.edgeWeight(he);
+        } else if (_hg.pinCountInPart(he, _hg.partID(hn)) == 1) {
+          _tmp_gains[i] += _hg.edgeWeight(he);
+        }
+      }
+    }
+
+    PartitionID max_gain_part = Hypergraph::kInvalidPartition;
+    Gain max_gain = std::numeric_limits<Gain>::min() + 1;
+    _tmp_gains[_hg.partID(hn)] = std::numeric_limits<Gain>::min();
+    for (PartitionID i = 0; i != _config.partitioning.k; ++i) {
+      if ((_tmp_gains[i] > max_gain) ||
+          ((_tmp_gains[i] == max_gain) && Randomize::flipCoin())) {
+        max_gain = _tmp_gains[i];
+        max_gain_part = i;
+      }
+      _tmp_gains[i] = 0;
+    }
+    return GainPartitionPair(max_gain, max_gain_part);
+  }
+
+  Hypergraph& _hg;
+  const Configuration& _config;
+  Stats _stats;
+  std::vector<Gain> _tmp_gains;
+  DISALLOW_COPY_AND_ASSIGN(KWayFMRefiner);
+};
+} // namespace partition
+#endif  // SRC_PARTITION_REFINEMENT_KWAYFMREFINER_H_
---
+++ src/partition/refinement/k_way_fm_refiner_test.cc
@@ -0,0 +1,84 @@
+/***************************************************************************
+ *  Copyright (C) 2014 Sebastian Schlag <sebastian.schlag@kit.edu>
+ **************************************************************************/
+
+#include "gmock/gmock.h"
+
+#include "lib/definitions.h"
+#include "partition/refinement/KWayFMRefiner.h"
+#include "partition/refinement/policies/FMStopPolicies.h"
+
+using::testing::Test;
+using::testing::Eq;
+
+using defs::Hypergraph;
+using defs::HyperedgeIndexVector;
+using defs::HyperedgeVector;
+using defs::HyperedgeWeight;
+using defs::HypernodeID;
+
+namespace partition {
+typedef KWayFMRefiner<NumberOfFruitlessMovesStopsSearch> KWayFMRefinerSimpleStopping;
+
+class AKWayFMRefiner : public Test {
+  public:
+  AKWayFMRefiner() :
+    hypergraph(8, 5, HyperedgeIndexVector { 0, 2, 6, 9, 12, /*sentinel*/ 14 },
+               HyperedgeVector { 0, 2, 0, 1, 3, 4, 3, 4, 6, 2, 5, 6, 2, 7 }, 4),
+    config(),
+    refiner(nullptr) {
+    config.partitioning.k = 4;
+    hypergraph.setNodePart(0, 0);
+    hypergraph.setNodePart(1, 0);
+    hypergraph.setNodePart(2, 1);
+    hypergraph.setNodePart(3, 2);
+    hypergraph.setNodePart(4, 2);
+    hypergraph.setNodePart(5, 3);
+    hypergraph.setNodePart(6, 3);
+    hypergraph.setNodePart(7, 1);
+    config.two_way_fm.max_number_of_fruitless_moves = 50;
+    refiner = new KWayFMRefinerSimpleStopping(hypergraph, config);
+    refiner->initialize();
+  }
+
+  ~AKWayFMRefiner() {
+    delete refiner;
+  }
+
+  Hypergraph hypergraph;
+  Configuration config;
+  KWayFMRefinerSimpleStopping* refiner;
+};
+
+TEST_F(AKWayFMRefiner, IdentifiesBorderHypernodes) {
+  ASSERT_THAT(refiner->isBorderNode(0), Eq(true));
+  ASSERT_THAT(refiner->isBorderNode(6), Eq(true));
+  ASSERT_THAT(refiner->isBorderNode(7), Eq(false));
+}
+
+TEST_F(AKWayFMRefiner, ComputesGainOfHypernodeMoves) {
+  Hypergraph hgr(7, 4, HyperedgeIndexVector { 0, 2, 4, 8, /*sentinel*/ 10 },
+                 HyperedgeVector { 0, 1, 1, 2, 2, 3, 4, 5, 5, 6 }, 4);
+  hgr.setNodePart(0, 0);
+  hgr.setNodePart(1, 1);
+  hgr.setNodePart(2, 0);
+  hgr.setNodePart(3, 3);
+  hgr.setNodePart(4, 3);
+  hgr.setNodePart(5, 2);
+  hgr.setNodePart(6, 2);
+  delete refiner;
+  refiner = new KWayFMRefinerSimpleStopping(hgr, config);
+
+  // positive gain
+  ASSERT_THAT(refiner->computeMaxGain(1).first, Eq(2));
+  ASSERT_THAT(refiner->computeMaxGain(1).second, Eq(0));
+
+  // zero gain
+  ASSERT_THAT(refiner->computeMaxGain(4).first, Eq(0));
+  ASSERT_THAT(refiner->computeMaxGain(4).second, Eq(0));
+
+  // negative gain
+  ASSERT_THAT(refiner->computeMaxGain(6).first, Eq(-1));
+  ASSERT_THAT(refiner->computeMaxGain(6).second, Eq(3));
+}
+} // namespace partition
