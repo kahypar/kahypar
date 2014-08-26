35919fb2c2f4828dc119e7ff0a501e608091a790
Sebastian Schlag <sebastian.schlag@kit.edu>
2014-08-26 16:53:38 +0200

    first version of direct k-way refiner

--- src/partition/refinement/KWayFMRefiner.h
+++ src/partition/refinement/KWayFMRefiner.h
@@ -5,6 +5,8 @@
 #ifndef SRC_PARTITION_REFINEMENT_KWAYFMREFINER_H_
 #define SRC_PARTITION_REFINEMENT_KWAYFMREFINER_H_
 
+#include <boost/dynamic_bitset.hpp>
+
 #include <limits>
 #include <string>
 #include <utility>
@@ -32,8 +34,13 @@ using defs::HyperedgeWeight;
 using defs::HypernodeWeight;
 
 namespace partition {
-static const bool dbg_refinement_kway_fm_activation = true;
-
+static const bool dbg_refinement_kway_fm_activation = false;
+static const bool dbg_refinement_kway_fm_improvements = true;
+static const bool dbg_refinement_kway_fm_stopping_crit = false;
+static const bool dbg_refinement_kway_fm_min_cut_idx = false;
+static const bool dbg_refinement_kway_fm_gain_update = false;
+static const bool dbg_refinement_kway_fm_move = false;
+static const bool dbg_refinement_kway_fm_gain_comp = false;
 
 template <class StoppingPolicy = Mandatory>
 class KWayFMRefiner : public IRefiner {
@@ -43,30 +50,113 @@ class KWayFMRefiner : public IRefiner {
                         std::numeric_limits<HyperedgeWeight>,
                         PartitionID> KWayRefinementPQ;
 
+  struct RollbackInfo {
+    HypernodeID hn;
+    PartitionID from_part;
+    PartitionID to_part;
+  };
+
   public:
   KWayFMRefiner(Hypergraph& hypergraph, const Configuration& config) :
     _hg(hypergraph),
     _config(config),
     _tmp_gains(_config.partitioning.k, 0),
     _pq(_hg.initialNumNodes()),
-    _stats() { }
+    _marked(_hg.initialNumNodes()),
+    _just_updated(_hg.initialNumNodes()),
+    _performed_moves(),
+    _stats() {
+    _performed_moves.reserve(_hg.initialNumNodes());
+  }
+
+  void initializeImpl() final { }
 
   void refineImpl(std::vector<HypernodeID>& refinement_nodes, size_t num_refinement_nodes,
                   HyperedgeWeight& best_cut, double max_imbalance, double& best_imbalance) final {
     ASSERT(best_cut == metrics::hyperedgeCut(_hg),
            "initial best_cut " << best_cut << "does not equal cut induced by hypergraph "
            << metrics::hyperedgeCut(_hg));
-    ASSERT(FloatingPoint<double>(best_imbalance).AlmostEquals(
-             FloatingPoint<double>(metrics::imbalance(_hg))),
-           "initial best_imbalance " << best_imbalance << "does not equal imbalance induced"
-           << " by hypergraph " << metrics::imbalance(_hg));
+    // ASSERT(FloatingPoint<double>(best_imbalance).AlmostEquals(
+    //          FloatingPoint<double>(metrics::imbalance(_hg))),
+    //        "initial best_imbalance " << best_imbalance << "does not equal imbalance induced"
+    //        << " by hypergraph " << metrics::imbalance(_hg));
 
     _pq.clear();
+    _marked.reset();
 
     Randomize::shuffleVector(refinement_nodes, num_refinement_nodes);
     for (size_t i = 0; i < num_refinement_nodes; ++i) {
       activate(refinement_nodes[i]);
     }
+
+#ifndef NDEBUG
+    HyperedgeWeight initial_cut = best_cut;
+#endif
+
+    HyperedgeWeight cut = best_cut;
+    int min_cut_index = -1;
+    int step = 0; // counts total number of loop iterations - might be removed
+    int num_moves = 0;
+    StoppingPolicy::resetStatistics();
+
+    while (!_pq.empty() && !StoppingPolicy::searchShouldStop(min_cut_index, num_moves, _config,
+                                                             best_cut, cut)) {
+      Gain max_gain = _pq.maxKey();
+      HypernodeID max_gain_node = _pq.max();
+      PartitionID from_part = _hg.partID(max_gain_node);
+      PartitionID to_part = _pq.data(max_gain_node);
+      _pq.deleteMax();
+
+      DBG(false, "cut=" << cut << " max_gain_node=" << max_gain_node << " gain=" << max_gain << " target_part=" << to_part);
+
+      ASSERT(!_marked[max_gain_node],
+             "HN " << max_gain_node << "is marked and not eligable to be moved");
+      ASSERT(max_gain == computeMaxGain(max_gain_node).first,
+             "Inconsistent gain caculation");
+      // to_part cannot be double-checked, since random tie-breaking might lead to a different to_part
+
+      ASSERT([&]() {
+               _hg.changeNodePart(max_gain_node, from_part, to_part);
+               ASSERT((cut - max_gain) == metrics::hyperedgeCut(_hg), "cut=" << cut - max_gain << "!=" << metrics::hyperedgeCut(_hg));
+               _hg.changeNodePart(max_gain_node, to_part, from_part);
+               return true;
+             } ()
+             , "BARFG!");
+
+      bool move_successful = moveHypernode(max_gain_node, from_part, to_part);
+
+      if (move_successful) {
+        cut -= max_gain;
+        StoppingPolicy::updateStatistics(max_gain);
+
+        ASSERT(cut == metrics::hyperedgeCut(_hg),
+               "Calculated cut (" << cut << ") and cut induced by hypergraph ("
+               << metrics::hyperedgeCut(_hg) << ") do not match");
+
+        updateNeighbours(max_gain_node);
+
+        if (cut < best_cut || (cut == best_cut && Randomize::flipCoin())) {
+          DBG(dbg_refinement_kway_fm_improvements,
+              "TwoWayFM improved cut from " << best_cut << " to " << cut);
+          best_cut = cut;
+          min_cut_index = num_moves;
+          StoppingPolicy::resetStatistics();
+        }
+        _performed_moves[num_moves] = { max_gain_node, from_part, to_part };
+        ++num_moves;
+      }
+      ++step;
+    }
+    DBG(dbg_refinement_kway_fm_stopping_crit, "KWayFM performed " << num_moves - 1
+        << " local search movements (" << step << " steps): stopped because of "
+        << (StoppingPolicy::searchShouldStop(min_cut_index, num_moves, _config, best_cut, cut) == true ?
+            "policy" : "empty queue"));
+    DBG(dbg_refinement_kway_fm_min_cut_idx, "min_cut_index=" << min_cut_index);
+
+    rollback(num_moves - 1, min_cut_index);
+    ASSERT(best_cut == metrics::hyperedgeCut(_hg), "Incorrect rollback operation");
+    ASSERT(best_cut <= initial_cut, "Cut quality decreased from "
+           << initial_cut << " to" << best_cut);
   }
 
   int numRepetitionsImpl() const final {
@@ -86,6 +176,86 @@ class KWayFMRefiner : public IRefiner {
   FRIEND_TEST(AKWayFMRefiner, ComputesGainOfHypernodeMoves);
   FRIEND_TEST(AKWayFMRefiner, ActivatesBorderNodes);
   FRIEND_TEST(AKWayFMRefiner, DoesNotActivateInternalNodes);
+  FRIEND_TEST(AKWayFMRefiner, DoesNotPerformMovesThatWouldLeadToImbalancedPartitions);
+  FRIEND_TEST(AKWayFMRefiner, PerformsMovesThatDontLeadToImbalancedPartitions);
+  FRIEND_TEST(AKWayFMRefiner, ComputesCorrectGainValues);
+
+  bool isCutHyperedge(HyperedgeID he) const {
+    IncidenceIterator begin = _hg.pins(he).begin();
+    IncidenceIterator end = _hg.pins(he).end();
+    ASSERT(begin != end, "Accessing empty hyperedge");
+    PartitionID partition = _hg.partID(*begin);
+    ++begin;
+    for (IncidenceIterator pin_it = begin; pin_it != end; ++pin_it) {
+      if (partition != _hg.partID(*pin_it)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void rollback(int last_index, int min_cut_index) {
+    DBG(false, "min_cut_index=" << min_cut_index);
+    DBG(false, "last_index=" << last_index);
+    while (last_index != min_cut_index) {
+      const HypernodeID hn = _performed_moves[last_index].hn;
+      const PartitionID from_part = _performed_moves[last_index].to_part;
+      const PartitionID to_part = _performed_moves[last_index].from_part;
+      _hg.changeNodePart(hn, from_part, to_part);
+      --last_index;
+    }
+  }
+
+  void updateNeighbours(HypernodeID hn) {
+    _just_updated.reset();
+    for (auto && he : _hg.incidentEdges(hn)) {
+      for (auto && pin : _hg.pins(he)) {
+        updatePin(pin);
+      }
+    }
+  }
+
+  void updatePin(HypernodeID pin) {
+    if (_pq.contains(pin)) {
+      ASSERT(!_marked[pin], " Trying to update marked HN " << pin);
+      if (isBorderNode(pin)) {
+        if (!_just_updated[pin]) {
+          GainPartitionPair pair = computeMaxGain(pin);
+          DBG(dbg_refinement_kway_fm_gain_update, "updating gain of HN " << pin
+              << " from gain " << _pq.key(pin) << " to " << pair.first << " (to_part="
+              << pair.second << ")");
+          _pq.updateKey(pin, pair.first);
+          PartitionID& target_part = _pq.data(pin);
+          target_part = pair.second;
+          _just_updated[pin] = true;
+        }
+      } else {
+        DBG(dbg_refinement_kway_fm_gain_update, "deleting pin " << pin << " from PQ ");
+        _pq.remove(pin);
+      }
+    } else {
+      if (!_marked[pin]) {
+        // border node check is performed in activate
+        activate(pin);
+        _just_updated[pin] = true;
+      }
+    }
+  }
+
+  bool moveHypernode(HypernodeID hn, PartitionID from_part, PartitionID to_part) {
+    ASSERT(isBorderNode(hn), "Hypernode " << hn << " is not a border node!");
+    _marked[hn] = true;
+    if ((_hg.partWeight(to_part) + _hg.nodeWeight(hn)
+         >= _config.partitioning.partition_size_upper_bound) ||
+        (_hg.partSize(from_part) - 1 == 0)) {
+      DBG(dbg_refinement_kway_fm_move, "skipping move of HN " << hn << " (" << from_part << "->" << to_part << ")");
+      return false;
+    }
+    DBG(dbg_refinement_kway_fm_move, "moving HN" << hn << " from " << from_part
+        << " to " << to_part << " (weight=" << _hg.nodeWeight(hn) << ")");
+    _hg.changeNodePart(hn, from_part, to_part);
+    return true;
+  }
 
   void activate(HypernodeID hn) {
     if (isBorderNode(hn)) {
@@ -111,11 +281,15 @@ class KWayFMRefiner : public IRefiner {
   GainPartitionPair computeMaxGain(HypernodeID hn) {
     ASSERT(isBorderNode(hn), "Cannot compute gain for non-border HN " << hn);
     for (auto && he : _hg.incidentEdges(hn)) {
+      ASSERT(_hg.edgeSize(he) > 1, "Computing gain for Single-Node HE");
+      HypernodeID num_pins_in_source_part =
+        _hg.pinCountInPart(he, _hg.partID(hn));
       for (PartitionID i = 0; i != _config.partitioning.k; ++i) {
-        if (_hg.pinCountInPart(he, i) == 0) {
+        if (_hg.pinCountInPart(he, i) == 0 && num_pins_in_source_part == _hg.edgeSize(he)) {
           ASSERT(i != _hg.partID(hn), "inconsistent Hypergraph-DS");
           _tmp_gains[i] -= _hg.edgeWeight(he);
-        } else if (_hg.pinCountInPart(he, _hg.partID(hn)) == 1) {
+        }
+        if (num_pins_in_source_part == 1 && _hg.pinCountInPart(he, i) == _hg.edgeSize(he) - 1) {
           _tmp_gains[i] += _hg.edgeWeight(he);
         }
       }
@@ -124,6 +298,14 @@ class KWayFMRefiner : public IRefiner {
     PartitionID max_gain_part = Hypergraph::kInvalidPartition;
     Gain max_gain = std::numeric_limits<Gain>::min() + 1;
     _tmp_gains[_hg.partID(hn)] = std::numeric_limits<Gain>::min();
+
+    DBG(dbg_refinement_kway_fm_gain_comp, [&]() {
+          for (PartitionID i = 0; i != _config.partitioning.k; ++i) {
+            LOG("gain(" << i << ")=" << _tmp_gains[i]);
+          }
+          return std::string("");
+        } ());
+
     for (PartitionID i = 0; i != _config.partitioning.k; ++i) {
       if ((_tmp_gains[i] > max_gain) ||
           ((_tmp_gains[i] == max_gain) && Randomize::flipCoin())) {
@@ -132,6 +314,7 @@ class KWayFMRefiner : public IRefiner {
       }
       _tmp_gains[i] = 0;
     }
+    DBG(dbg_refinement_kway_fm_gain_comp, "gain(" << hn << ")=" << max_gain << " part=" << max_gain_part);
     return GainPartitionPair(max_gain, max_gain_part);
   }
 
@@ -139,6 +322,9 @@ class KWayFMRefiner : public IRefiner {
   const Configuration& _config;
   std::vector<Gain> _tmp_gains;
   KWayRefinementPQ _pq;
+  boost::dynamic_bitset<uint64_t> _marked;
+  boost::dynamic_bitset<uint64_t> _just_updated;
+  std::vector<RollbackInfo> _performed_moves;
   Stats _stats;
   DISALLOW_COPY_AND_ASSIGN(KWayFMRefiner);
 };
--- src/partition/refinement/k_way_fm_refiner_test.cc
+++ src/partition/refinement/k_way_fm_refiner_test.cc
@@ -23,67 +23,33 @@ typedef KWayFMRefiner<NumberOfFruitlessMovesStopsSearch> KWayFMRefinerSimpleStop
 class AKWayFMRefiner : public Test {
   public:
   AKWayFMRefiner() :
-    // example hypergraph with one additional HN and one additional HE
-    hypergraph(8, 5, HyperedgeIndexVector { 0, 2, 6, 9, 12, /*sentinel*/ 14 },
-               HyperedgeVector { 0, 2, 0, 1, 3, 4, 3, 4, 6, 2, 5, 6, 2, 7 }, 4),
     config(),
-    refiner(nullptr) {
+    // example hypergraph with one additional HN and one additional HE
+    hypergraph(new Hypergraph(8, 5, HyperedgeIndexVector { 0, 2, 6, 9, 12, /*sentinel*/ 14 },
+                              HyperedgeVector { 0, 2, 0, 1, 3, 4, 3, 4, 6, 2, 5, 6, 2, 7 }, 4)),
+    refiner(new KWayFMRefinerSimpleStopping(*hypergraph, config)) {
     config.partitioning.k = 4;
-    hypergraph.setNodePart(0, 0);
-    hypergraph.setNodePart(1, 0);
-    hypergraph.setNodePart(2, 1);
-    hypergraph.setNodePart(3, 2);
-    hypergraph.setNodePart(4, 2);
-    hypergraph.setNodePart(5, 3);
-    hypergraph.setNodePart(6, 3);
-    hypergraph.setNodePart(7, 1);
+    hypergraph->setNodePart(0, 0);
+    hypergraph->setNodePart(1, 0);
+    hypergraph->setNodePart(2, 1);
+    hypergraph->setNodePart(3, 2);
+    hypergraph->setNodePart(4, 2);
+    hypergraph->setNodePart(5, 3);
+    hypergraph->setNodePart(6, 3);
+    hypergraph->setNodePart(7, 1);
     config.two_way_fm.max_number_of_fruitless_moves = 50;
-    refiner = new KWayFMRefinerSimpleStopping(hypergraph, config);
     refiner->initialize();
   }
 
-  ~AKWayFMRefiner() {
-    delete refiner;
-  }
-
-  Hypergraph hypergraph;
   Configuration config;
-  KWayFMRefinerSimpleStopping* refiner;
+  std::unique_ptr<Hypergraph> hypergraph;
+  std::unique_ptr<KWayFMRefinerSimpleStopping> refiner;
 };
 
 TEST_F(AKWayFMRefiner, IdentifiesBorderHypernodes) {
   ASSERT_THAT(refiner->isBorderNode(0), Eq(true));
   ASSERT_THAT(refiner->isBorderNode(6), Eq(true));
   ASSERT_THAT(refiner->isBorderNode(7), Eq(false));
-}
-
-TEST_F(AKWayFMRefiner, ComputesGainOfHypernodeMoves) {
-  // hypergraph with positive, zero and negative gain nodes
-  Hypergraph hgr(9, 5, HyperedgeIndexVector { 0, 2, 4, 8, 10, /*sentinel*/ 13 },
-                 HyperedgeVector { 0, 1, 1, 2, 2, 3, 4, 5, 5, 6, 6, 7, 8 }, 4);
-  hgr.setNodePart(0, 0);
-  hgr.setNodePart(1, 1);
-  hgr.setNodePart(2, 0);
-  hgr.setNodePart(3, 3);
-  hgr.setNodePart(4, 3);
-  hgr.setNodePart(5, 2);
-  hgr.setNodePart(6, 2);
-  hgr.setNodePart(7, 2);
-  hgr.setNodePart(8, 1);
-  delete refiner;
-  refiner = new KWayFMRefinerSimpleStopping(hgr, config);
-
-  // positive gain
-  ASSERT_THAT(refiner->computeMaxGain(1).first, Eq(2));
-  ASSERT_THAT(refiner->computeMaxGain(1).second, Eq(0));
-
-  // zero gain
-  ASSERT_THAT(refiner->computeMaxGain(4).first, Eq(0));
-  ASSERT_THAT(refiner->computeMaxGain(4).second, Eq(0));
-
-  // negative gain
-  ASSERT_THAT(refiner->computeMaxGain(6).first, Eq(-1));
-  ASSERT_THAT(refiner->computeMaxGain(6).second, Eq(1));
 }
 
 TEST_F(AKWayFMRefiner, ActivatesBorderNodes) {
@@ -97,5 +63,143 @@ TEST_F(AKWayFMRefiner, DoesNotActivateInternalNodes) {
   refiner->activate(7);
 
   ASSERT_THAT(refiner->_pq.contains(7), Eq(false));
+}
+
+TEST_F(AKWayFMRefiner, ComputesGainOfHypernodeMoves) {
+  // hypergraph with positive, zero and negative gain nodes
+  hypergraph.reset(new Hypergraph(9, 5, HyperedgeIndexVector { 0, 2, 4, 8, 10, /*sentinel*/ 13 },
+                                  HyperedgeVector { 0, 1, 1, 2, 2, 3, 4, 5, 5, 6, 6, 7, 8 }, 4));
+  refiner.reset(new KWayFMRefinerSimpleStopping(*hypergraph, config));
+  hypergraph->setNodePart(0, 0);
+  hypergraph->setNodePart(1, 1);
+  hypergraph->setNodePart(2, 0);
+  hypergraph->setNodePart(3, 3);
+  hypergraph->setNodePart(4, 3);
+  hypergraph->setNodePart(5, 2);
+  hypergraph->setNodePart(6, 2);
+  hypergraph->setNodePart(7, 2);
+  hypergraph->setNodePart(8, 1);
+
+  // positive gain
+  ASSERT_THAT(refiner->computeMaxGain(1).first, Eq(2));
+  ASSERT_THAT(refiner->computeMaxGain(1).second, Eq(0));
+
+  // zero gain
+  ASSERT_THAT(refiner->computeMaxGain(4).first, Eq(0));
+  ASSERT_THAT(refiner->computeMaxGain(4).second, Eq(2));
+
+  // negative gain
+  ASSERT_THAT(refiner->computeMaxGain(6).first, Eq(-1));
+  ASSERT_THAT(refiner->computeMaxGain(6).second, Eq(1));
+}
+
+TEST_F(AKWayFMRefiner, DoesNotPerformMovesThatWouldLeadToImbalancedPartitions) {
+  hypergraph.reset(new Hypergraph(8, 4, HyperedgeIndexVector { 0, 2, 4, 7, /*sentinel*/ 9 },
+                                  HyperedgeVector { 0, 1, 2, 3, 4, 5, 7, 5, 6 }, 4));
+  hypergraph->setNodePart(0, 0);
+  hypergraph->setNodePart(1, 0);
+  hypergraph->setNodePart(2, 1);
+  hypergraph->setNodePart(3, 1);
+  hypergraph->setNodePart(4, 2);
+  hypergraph->setNodePart(5, 2);
+  hypergraph->setNodePart(6, 3);
+  hypergraph->setNodePart(7, 3);
+  config.partitioning.k = 4;
+  config.partitioning.epsilon = 0.02;
+  config.partitioning.partition_size_upper_bound = (1 + config.partitioning.epsilon)
+                                                   * ceil(hypergraph->numNodes() /
+                                                          static_cast<double>(config.partitioning.k));
+
+  refiner.reset(new KWayFMRefinerSimpleStopping(*hypergraph, config));
+
+  ASSERT_THAT(refiner->moveHypernode(7, 3, 2), Eq(false));
+}
+
+TEST_F(AKWayFMRefiner, PerformsMovesThatDontLeadToImbalancedPartitions) {
+  hypergraph.reset(new Hypergraph(8, 4, HyperedgeIndexVector { 0, 2, 4, 7, /*sentinel*/ 9 },
+                                  HyperedgeVector { 0, 1, 2, 3, 4, 5, 7, 5, 6 }, 4));
+  hypergraph->setNodePart(0, 0);
+  hypergraph->setNodePart(1, 0);
+  hypergraph->setNodePart(2, 1);
+  hypergraph->setNodePart(3, 1);
+  hypergraph->setNodePart(4, 2);
+  hypergraph->setNodePart(5, 2);
+  hypergraph->setNodePart(6, 3);
+  hypergraph->setNodePart(7, 3);
+  config.partitioning.k = 4;
+  config.partitioning.epsilon = 1.0;
+  config.partitioning.partition_size_upper_bound =
+    (1 + config.partitioning.epsilon)
+    * ceil(hypergraph->numNodes() / static_cast<double>(config.partitioning.k));
+
+  refiner.reset(new KWayFMRefinerSimpleStopping(*hypergraph, config));
+
+  ASSERT_THAT(refiner->moveHypernode(7, 3, 2), Eq(true));
+}
+
+TEST_F(AKWayFMRefiner, PerformsCompleteRollbackIfNoImprovementCouldBeFound) {
+  hypergraph.reset(new Hypergraph(8, 6, HyperedgeIndexVector { 0, 2, 5, 7, 9, 11, /*sentinel*/ 13 },
+                                  HyperedgeVector { 0, 1, 0, 1, 6, 1, 6, 2, 3, 4, 5, 6, 7 }, 4));
+  hypergraph->setNodePart(0, 0);
+  hypergraph->setNodePart(1, 0);
+  hypergraph->setNodePart(2, 1);
+  hypergraph->setNodePart(3, 1);
+  hypergraph->setNodePart(4, 2);
+  hypergraph->setNodePart(5, 2);
+  hypergraph->setNodePart(6, 3);
+  hypergraph->setNodePart(7, 3);
+
+  Hypergraph orig_hgr(8, 6, HyperedgeIndexVector { 0, 2, 5, 7, 9, 11, /*sentinel*/ 13 },
+                      HyperedgeVector { 0, 1, 0, 1, 6, 1, 6, 2, 3, 4, 5, 6, 7 }, 4);
+  orig_hgr.setNodePart(0, 0);
+  orig_hgr.setNodePart(1, 0);
+  orig_hgr.setNodePart(2, 1);
+  orig_hgr.setNodePart(3, 1);
+  orig_hgr.setNodePart(4, 2);
+  orig_hgr.setNodePart(5, 2);
+  orig_hgr.setNodePart(6, 3);
+  orig_hgr.setNodePart(7, 3);
+
+  config.partitioning.k = 4;
+  config.partitioning.epsilon = 1.0;
+  config.partitioning.partition_size_upper_bound =
+    (1 + config.partitioning.epsilon)
+    * ceil(hypergraph->numNodes() / static_cast<double>(config.partitioning.k));
+
+  refiner.reset(new KWayFMRefinerSimpleStopping(*hypergraph, config));
+
+  double old_imbalance = metrics::imbalance(*hypergraph);
+  HyperedgeWeight old_cut = metrics::hyperedgeCut(*hypergraph);
+  std::vector<HypernodeID> refinement_nodes = { 0, 1 };
+
+  refiner->refine(refinement_nodes, 2, old_cut, 1.0, old_imbalance);
+
+  ASSERT_THAT(verifyEquivalence(orig_hgr, *hypergraph), Eq(true));
+}
+
+TEST_F(AKWayFMRefiner, ComputesCorrectGainValues) {
+  hypergraph.reset(new Hypergraph(10, 5, HyperedgeIndexVector { 0, 4, 7, 9, 12, /*sentinel*/ 15 },
+                                  HyperedgeVector { 1, 2, 5, 6, 2, 3, 4, 0, 2, 2, 8, 9, 0, 2, 8 }, 4));
+  hypergraph->setNodePart(0, 0);
+  hypergraph->setNodePart(1, 0);
+  hypergraph->setNodePart(2, 1);
+  hypergraph->setNodePart(3, 2);
+  hypergraph->setNodePart(4, 2);
+  hypergraph->setNodePart(5, 2);
+  hypergraph->setNodePart(6, 3);
+  hypergraph->setNodePart(7, 3);
+  hypergraph->setNodePart(8, 1);
+  hypergraph->setNodePart(9, 1);
+
+  config.partitioning.k = 4;
+  config.partitioning.epsilon = 1.0;
+  config.partitioning.partition_size_upper_bound =
+    (1 + config.partitioning.epsilon)
+    * ceil(hypergraph->numNodes() / static_cast<double>(config.partitioning.k));
+
+  refiner.reset(new KWayFMRefinerSimpleStopping(*hypergraph, config));
+
+  ASSERT_THAT(refiner->computeMaxGain(2).first, Eq(0));
+  ASSERT_THAT(refiner->computeMaxGain(2).second, Eq(0));
 }
 } // namespace partition
