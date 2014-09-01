e2eb1c4daf7c7359275696de2ceba53b6da23359
Sebastian Schlag <sebastian.schlag@kit.edu>
2014-09-01 15:34:06 +0200

    remove benchmark files from repo

--- benchmark/results/2014-03-04_hmetis_rb/extract_results.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd "output"
-
-echo "graph,cut" >> "../results2.csv"
-for file in *.txt;
-do
-echo "$(basename "$file" | sed -n -e 's/_.*//p'),$(cat $file | grep "                Hyperedge Cut:" | sed -n -e 's/^.*  Hyperedge Cut: //p' | sed -n -e 's/(minimize)//p' | sed -e 's/[ \t]*$//')" >> "../results2.csv"
-done
