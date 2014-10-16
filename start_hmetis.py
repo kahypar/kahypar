0f243a331b1733002538f8bc6a803c104d5411a3
Vitali Henne <vitali.henne@gmail.com>
2014-10-16 12:51:41 +0200

    hmetis start script now computes the imbalance

--- start_hmetis.py
+++ start_hmetis.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 from subprocess import Popen, PIPE
 import ntpath
 import argparse
 import time
+import re
 
 
 parser = argparse.ArgumentParser()
@@ -23,7 +24,7 @@ k = args.k
 seed = args.seed
 
 start = time.time()
-p = Popen(['/home/schlag/hmetis-2.0pre1/Linux-x86_64/hmetis2.0pre1',
+p = Popen(['/software/hmetis-2.0pre1/Linux-x86_64/hmetis2.0pre1',
            '-ptype=kway',
            '-ufactor='+str(ufactor),
            '-nruns='+str(nruns),
@@ -40,37 +41,51 @@ result_string = ("RESULT graph="+ntpath.basename(graph) +
 
 i = 0
 results = []
+part_sizes = []
+hg_weight = 0
 for line in iter(p.stdout.readline, b''):
     s = str(line).strip()
-    print(s)
+    #print(s)
     if ("Vtxs" in s):
-        print(s.split())
-        result_string += (" numHNs="+str(int(s.split()[4][:-1])))
-        result_string += (" numHEs="+str(int(s.split()[6][:-1])))
+        #print(s.split(','))
+        result_string += (" numHNs="+str(int(s.split(',')[1][7:])))
+        result_string += (" numHEs="+str(int(s.split(',')[2][10:])))
+        hg_weight = int(s.split(',')[1][7:])
     if ("initial cut" in s):
-        result_string += (" initialCut"+str(i)+"="+str(float(s.split()[3][:-3])))
+        result_string += (" initialCut"+str(i)+"="+str(float(s.split(':')[1][1:-3])))
         i = i+1
     if ("Multilevel" in s):
-        result_string += (" totalPartitionTime="+str(float(s.split()[3][:-3])))
+        result_string += (" totalPartitionTime="+str(float(s.split()[1])))
         results.append(s)
     if ("Coarsening" in s):
-        result_string += (" coarseningTime="+str(float(s.split()[3][:-3])))
+        result_string += (" coarseningTime="+str(float(s.split()[1])))
         results.append(s)
     if ("Initial Partition" in s):
-        result_string += (" initialPartitionTime="+str(float(s.split()[4][:-3])))
+        result_string += (" initialPartitionTime="+str(float(s.split()[2])))
         results.append(s)
     if ("Uncoarsening" in s):
-        result_string += (" uncoarseningRefinementTime="+str(float(s.split()[3][:-3])))
+        result_string += (" uncoarseningRefinementTime="+str(float(s.split()[1])))
         results.append(s)
     if ("Total" in s):
         results.append(s)
     if ("Hyperedge Cut" in s):
-        result_string += (" cut="+str(float(s.split()[3][:-19])))
+        result_string += (" cut="+str(float(s.split()[2])))
         results.append(s)
     if ("Sum of External" in s):
-        result_string += (" soed="+str(float(s.split()[5][:-19])))
+        result_string += (" soed="+str(float(s.split()[4])))
         results.append(s)
+    if ("[" in s):
+        split = s.split(']')
+        for token in split[:-1]:
+            t = re.search('\(.*?\)', token)
+            part_size = float(t.group(0)[1:-1])
+            part_sizes.append(part_size)
+
 p.communicate()  # close p.stdout, wait for the subprocess to exit
 end = time.time()
 
-print(result_string + " measuredTotalPartitionTime=" + str(end-start))
+# compute imbalance
+max_part_size = max(part_sizes)
+total_weight = sum(part_sizes)
+
+print(result_string + " measuredTotalPartitionTime=" + str(end-start) + " imbalance=" + str(1.0*k*max_part_size / total_weight - 1.0))
