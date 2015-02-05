7a35f05dfbf37cf0df2fb625eb5dbbd65e3f78d9
Sebastian Schlag <sebastian.schlag@kit.edu>
2015-02-05 13:26:26 +0100

    enhance KFM pq to handle enabling/disabling of heaps
    The current version automatically enables each encountered part
    and thus works exactly as the previous version with no significant
    performance penalty.
    
    The upcoming versions of Refiners will explicitly enable and disable
    underloaded/overloaded parts. In this case, the PQ has to be adapted to
    not enable heaps by default and the 2 disabled testcases can be activated.

--- src/lib/datastructure/KWayPriorityQueue.h
+++ src/lib/datastructure/KWayPriorityQueue.h
@@ -5,6 +5,10 @@
 #ifndef SRC_LIB_DATASTRUCTURE_KWAYPRIORITYQUEUE_H_
 #define SRC_LIB_DATASTRUCTURE_KWAYPRIORITYQUEUE_H_
 
+#include <algorithm>
+#include <limits>
+#include <vector>
+
 #include "external/binary_heap/NoDataBinaryMaxHeap.h"
 #include "external/binary_heap/QueueStorages.hpp"
 #include "lib/core/Mandatory.h"
@@ -16,7 +20,6 @@ using external::NoDataBinaryMaxHeap;
 using external::ArrayStorage;
 
 namespace datastructure {
-
 template <typename IDType = Mandatory,
           typename KeyType = Mandatory,
           typename MetaKey = Mandatory,
@@ -27,41 +30,66 @@ class KWayPriorityQueue {
   static const size_t kInvalidIndex = std::numeric_limits<size_t>::max();
   static const PartitionID kInvalidPart = std::numeric_limits<PartitionID>::max();
 
- public:
+  public:
   KWayPriorityQueue(const IDType size, const PartitionID k) :
-      _heaps(k, Heap(size)),
-      _index(k, kInvalidIndex),
-      _part(k, kInvalidPart),
-      _num_entries(0),
-      _num_nonempty_pqs(0) { }
+    _heaps(k, Heap(size)),
+    _index(k, kInvalidIndex),
+    _part(k, kInvalidPart),
+    _num_entries(0),
+    _num_nonempty_pqs(0),
+    _num_enabled_pqs(0) { }
 
   size_t size(const PartitionID part) const {
     ASSERT(static_cast<unsigned int>(part) < _heaps.size(), "Invalid " << V(part));
-    ASSERT(_index[part] != kInvalidIndex, V(part));
-    return _heaps[_index[part]].size();
+    return (_index[part] < _num_nonempty_pqs ? _heaps[_index[part]].size() : 0);
   }
 
+  // Counts all elements in all non-empty heaps and therefore includes
+  // the elements in disabled heaps as well
   size_t size() const {
     return _num_entries;
   }
 
   bool empty(const PartitionID part) const {
-    ASSERT(_index[part] != kInvalidIndex, V(part));
-    return _heaps[_index[part]].size() == 0;
+    return isUnused(part);
   }
 
   bool empty() const {
-    return _num_entries == 0;
+    return _num_enabled_pqs == 0 || _num_entries == 0;
+  }
+
+  PartitionID numEnabledParts() const {
+    return _num_enabled_pqs;
+  }
+
+  PartitionID numNonEmptyParts() const {
+    return _num_nonempty_pqs;
+  }
+
+  void enablePart(const PartitionID part) {
+    ASSERT(_index[part] < _num_nonempty_pqs, V(part));
+    if (!isEnabled(part)) {
+      swap(_index[part], _num_enabled_pqs);
+      ++_num_enabled_pqs;
+      ASSERT(_num_enabled_pqs <= _num_nonempty_pqs, V(_num_enabled_pqs));
+    }
+  }
+
+  void disablePart(const PartitionID part) {
+    ASSERT(isEnabled(part), V(part));
+    --_num_enabled_pqs;
+    swap(_index[part], _num_enabled_pqs);
   }
 
   void insert(const IDType id, const PartitionID part, const KeyType key) {
     ASSERT(static_cast<unsigned int>(part) < _heaps.size(), "Invalid " << V(part));
     DBG(false, "Insert: (" << id << "," << part << "," << key << ")");
-    if (_index[part] == kInvalidIndex) {
+    if (isUnused(part)) {
       ASSERT(_part[_num_nonempty_pqs] == kInvalidPart, V(_part[_num_nonempty_pqs]));
       _part[_num_nonempty_pqs] = part;
       _heaps[_num_nonempty_pqs].clear(); // lazy clear
       _index[part] = _num_nonempty_pqs++;
+      ++_num_enabled_pqs;
     }
     _heaps[_index[part]].push(id, key);
     ++_num_entries;
@@ -69,47 +97,32 @@ class KWayPriorityQueue {
 
   void deleteMax(IDType& max_id, KeyType& max_key, PartitionID& max_part) {
     size_t max_index = maxIndex();
+    ASSERT(max_index < _num_enabled_pqs, V(max_index));
+
     max_part = _part[max_index];
     max_id = _heaps[max_index].getMax();
     max_key = _heaps[max_index].getMaxKey();
 
-    ASSERT(max_index != kInvalidIndex,V(max_index));
+    ASSERT(_part[_index[max_part]] == max_part, V(max_part));
+    ASSERT(max_index != kInvalidIndex, V(max_index));
     ASSERT(max_key != MetaKey::max(), V(max_key));
     ASSERT(max_part != kInvalidPart, V(max_part) << V(max_id));
 
     _heaps[max_index].deleteMax();
     if (_heaps[max_index].empty()) {
-      --_num_nonempty_pqs;
-      swap(max_index, _num_nonempty_pqs);
-      _part[_num_nonempty_pqs] = kInvalidPart;
-      _index[max_part] = kInvalidIndex;
-
+      --_num_enabled_pqs;  // now points to the last enabled pq
+      --_num_nonempty_pqs; // now points to the last non-empty disbabled pq
+      swap(_index[max_part], _num_enabled_pqs);
+      swap(_index[max_part], _num_nonempty_pqs);
+      markUnused(max_part);
     }
     --_num_entries;
-  }
-
-  void swap(const size_t index_a, const size_t index_b) {
-    using std::swap;
-    swap(_heaps[index_a], _heaps[index_b]);
-    swap(_part[index_a], _part[index_b]);
-    swap(_index[_part[index_a]], _index[_part[index_b]]);
-    ASSERT(_index[_part[index_a]] == index_a
-           && _index[_part[index_b]] ==index_b, "Swap failed");
-  }
-
-  IDType max() const {
-    LOG("Should only be used for testing");
-    return _heaps[maxIndex()].getMax();
-  }
-
-  KeyType maxKey() const {
-    LOG("Should only be used for testing");
-    return _heaps[maxIndex()].getMaxKey();;
   }
 
   KeyType key(const IDType id, const PartitionID part) const {
     ASSERT(static_cast<unsigned int>(part) < _heaps.size(), "Invalid " << V(part));
     ASSERT(_index[part] < _num_nonempty_pqs, V(part));
+    ASSERT(_heaps[_index[part]].contains(id), V(id));
     return _heaps[_index[part]].getKey(id);
   }
 
@@ -125,7 +138,7 @@ class KWayPriorityQueue {
         return true;
       }
     }
-      return false;
+    return false;
   }
 
   void updateKey(const IDType id, const PartitionID part, const KeyType key) {
@@ -140,10 +153,11 @@ class KWayPriorityQueue {
     ASSERT(_heaps[_index[part]].contains(id), V(id) << V(part));
     _heaps[_index[part]].deleteNode(id);
     if (_heaps[_index[part]].empty()) {
-      --_num_nonempty_pqs;
+      --_num_enabled_pqs;  // now points to the last enabled pq
+      --_num_nonempty_pqs; // now points to the last non-empty disbabled pq
+      swap(_index[part], _num_enabled_pqs);
       swap(_index[part], _num_nonempty_pqs);
-      _part[_num_nonempty_pqs] = kInvalidPart;
-      _index[part] = kInvalidIndex;
+      markUnused(part);
     }
     --_num_entries;
   }
@@ -155,13 +169,39 @@ class KWayPriorityQueue {
     }
     _num_entries = 0;
     _num_nonempty_pqs = 0;
+    _num_enabled_pqs = 0;
   }
 
- private:
+  IDType max() const {
+    //Should only be used for testing
+    return _heaps[maxIndex()].getMax();
+  }
+
+  KeyType maxKey() const {
+    //Should only be used for testing
+    return _heaps[maxIndex()].getMaxKey();
+  }
+
+  private:
+  FRIEND_TEST(AKWayPriorityQueue, DisablesInternalHeapIfItBecomesEmptyDueToRemoval);
+  FRIEND_TEST(AKWayPriorityQueue, DISABLED_ChoosesMaxKeyAmongAllEnabledInternalHeaps);
+  FRIEND_TEST(AKWayPriorityQueue, DoesNotConsiderDisabledHeapForChoosingMax);
+  FRIEND_TEST(AKWayPriorityQueue, ReconsidersDisabledHeapAgainAfterEnabling);
+
+
+  void swap(const size_t index_a, const size_t index_b) {
+    using std::swap;
+    swap(_heaps[index_a], _heaps[index_b]);
+    swap(_part[index_a], _part[index_b]);
+    swap(_index[_part[index_a]], _index[_part[index_b]]);
+    ASSERT(_index[_part[index_a]] == index_a &&
+           _index[_part[index_b]] == index_b, "Swap failed");
+  }
+
   size_t maxIndex() const {
     size_t max_index = kInvalidIndex;
     KeyType max_key = std::numeric_limits<KeyType>::min();
-    for (size_t index = 0; index < _num_nonempty_pqs; ++index) {
+    for (size_t index = 0; index < _num_enabled_pqs; ++index) {
       ASSERT(!_heaps[index].empty(), V(index));
       const KeyType key = _heaps[index].getMaxKey();
       if (key > max_key) {
@@ -169,28 +209,43 @@ class KWayPriorityQueue {
         max_index = index;
       }
     }
-    ASSERT(max_index != kInvalidIndex,V(max_index));
+    ASSERT(max_index != kInvalidIndex, V(max_index));
     return max_index;
   }
 
+  bool isEnabled(const PartitionID part) const {
+    return _index[part] < _num_enabled_pqs;
+  }
+
+  bool isUnused(const PartitionID part) const {
+    ASSERT((_index[part] != kInvalidIndex ? _part[_index[part]] != kInvalidPart : true), V(part));
+    return _index[part] == kInvalidIndex;
+  }
+
+  void markUnused(const PartitionID part) {
+    _part[_index[part]] = kInvalidPart;
+    _index[part] = kInvalidIndex;
+  }
+
   std::vector<Heap> _heaps;
-  std::vector<size_t> _index; // part to index mapping
+  std::vector<size_t> _index;     // part to index mapping
   std::vector<PartitionID> _part; // index to part mapping
   size_t _num_entries;
   size_t _num_nonempty_pqs;
+  size_t _num_enabled_pqs;
   DISALLOW_COPY_AND_ASSIGN(KWayPriorityQueue);
 };
 
 template <typename IDType,
           typename KeyType,
           typename MetaKey,
-          class Storage >
-constexpr size_t KWayPriorityQueue<IDType,KeyType,MetaKey,Storage>::kInvalidIndex;
+          class Storage>
+constexpr size_t KWayPriorityQueue<IDType, KeyType, MetaKey, Storage>::kInvalidIndex;
 template <typename IDType,
           typename KeyType,
           typename MetaKey,
-          class Storage >
-constexpr PartitionID KWayPriorityQueue<IDType,KeyType,MetaKey,Storage>::kInvalidPart;
+          class Storage>
+constexpr PartitionID KWayPriorityQueue<IDType, KeyType, MetaKey, Storage>::kInvalidPart;
 } // namespace datastructure
 
 #endif  // SRC_LIB_DATASTRUCTURE_KWAYPRIORITYQUEUE_H_
---
+++ src/lib/datastructure/kway_priority_queue_test.cc
@@ -0,0 +1,202 @@
+/***************************************************************************
+ *  Copyright (C) 2014 Sebastian Schlag <sebastian.schlag@kit.edu>
+ **************************************************************************/
+
+#include "gmock/gmock.h"
+
+#include "lib/datastructure/KWayPriorityQueue.h"
+#include "lib/definitions.h"
+
+using::testing::Eq;
+using::testing::Test;
+
+using defs::HypernodeID;
+using defs::PartitionID;
+using defs::HyperedgeWeight;
+
+namespace datastructure {
+class AKWayPriorityQueue : public Test {
+  public:
+  AKWayPriorityQueue() :
+    prio_queue(5, 4) { }
+
+  KWayPriorityQueue<HypernodeID, HyperedgeWeight,
+                    std::numeric_limits<HyperedgeWeight> > prio_queue;
+};
+
+typedef AKWayPriorityQueue AKWayPriorityQueueDeathTest;
+
+TEST_F(AKWayPriorityQueue, IsEmptyIfItContainsNoElements) {
+  ASSERT_THAT(prio_queue.empty(), Eq(true));
+}
+
+TEST_F(AKWayPriorityQueue, DISABLED_IsEmptyIfNoInternalHeapsAreEnabled) {
+  prio_queue.insert(1, 2, 25);
+  ASSERT_THAT(prio_queue.empty(), Eq(true));
+}
+
+TEST_F(AKWayPriorityQueue, IsNotEmptyIfThereExistsANonemptyAndEnabledInternalHeap) {
+  prio_queue.insert(1, 2, 25);
+  prio_queue.enablePart(2);
+  ASSERT_THAT(prio_queue.empty(), Eq(false));
+}
+
+TEST_F(AKWayPriorityQueue, MustEnableAnInternalHeapInOrderToConsiderItDuringDeleteMax) {
+  prio_queue.insert(1, 2, 25);
+  prio_queue.enablePart(2);
+  HypernodeID max_id = -1;
+  HyperedgeWeight max_gain = -1;
+  PartitionID max_part = -1;
+
+  prio_queue.deleteMax(max_id, max_gain, max_part);
+
+  ASSERT_THAT(max_id, Eq(1));
+  ASSERT_THAT(max_gain, Eq(25));
+  ASSERT_THAT(max_part, Eq(2));
+}
+
+TEST_F(AKWayPriorityQueue, IsEmptyIfLastEnabledInternalHeapBecomesEmpty) {
+  prio_queue.insert(1, 2, 25);
+  prio_queue.enablePart(2);
+  HypernodeID max_id = -1;
+  HyperedgeWeight max_gain = -1;
+  PartitionID max_part = -1;
+
+  prio_queue.deleteMax(max_id, max_gain, max_part);
+
+  ASSERT_THAT(prio_queue.empty(), Eq(true));
+}
+
+TEST_F(AKWayPriorityQueue, IsEmptyIfLastEnabledInternalHeapBecomesDisabled) {
+  prio_queue.insert(1, 2, 25);
+  prio_queue.enablePart(2);
+
+  prio_queue.disablePart(2);
+
+  ASSERT_THAT(prio_queue.empty(), Eq(true));
+}
+
+TEST_F(AKWayPriorityQueue, DISABLED_ChoosesMaxKeyAmongAllEnabledInternalHeaps) {
+  prio_queue.insert(1, 2, 25);
+  prio_queue.insert(4, 1, 6);
+  prio_queue.insert(0, 3, 42);
+  prio_queue.insert(3, 0, 23);
+
+  prio_queue.enablePart(1);
+  ASSERT_THAT(prio_queue.max(), Eq(4));
+  ASSERT_THAT(prio_queue.maxKey(), Eq(6));
+
+  prio_queue.enablePart(2);
+  ASSERT_THAT(prio_queue.max(), Eq(1));
+  ASSERT_THAT(prio_queue.maxKey(), Eq(25));
+
+  prio_queue.enablePart(3);
+  ASSERT_THAT(prio_queue.max(), Eq(0));
+  ASSERT_THAT(prio_queue.maxKey(), Eq(42));
+
+  prio_queue.enablePart(0);
+  ASSERT_THAT(prio_queue.max(), Eq(0));
+  ASSERT_THAT(prio_queue.maxKey(), Eq(42));
+}
+
+TEST_F(AKWayPriorityQueue, DoesNotConsiderDisabledHeapForChoosingMax) {
+  prio_queue.insert(1, 2, 25);
+  prio_queue.insert(4, 1, 6);
+  prio_queue.insert(0, 3, 42);
+  prio_queue.insert(3, 0, 23);
+  prio_queue.enablePart(0);
+  prio_queue.enablePart(1);
+  prio_queue.enablePart(2);
+  prio_queue.enablePart(3);
+  ASSERT_THAT(prio_queue.max(), Eq(0));
+  ASSERT_THAT(prio_queue.maxKey(), Eq(42));
+
+  prio_queue.disablePart(3);
+  ASSERT_THAT(prio_queue.isEnabled(3), Eq(false));
+  ASSERT_THAT(prio_queue.max(), Eq(1));
+  ASSERT_THAT(prio_queue.maxKey(), Eq(25));
+}
+
+TEST_F(AKWayPriorityQueue, ReconsidersDisabledHeapAgainAfterEnabling) {
+  prio_queue.insert(1, 2, 25);
+  prio_queue.insert(4, 1, 6);
+  prio_queue.insert(0, 3, 42);
+  prio_queue.insert(3, 0, 23);
+  prio_queue.enablePart(0);
+  prio_queue.enablePart(1);
+  prio_queue.enablePart(2);
+  prio_queue.enablePart(3);
+  ASSERT_THAT(prio_queue.max(), Eq(0));
+  ASSERT_THAT(prio_queue.maxKey(), Eq(42));
+  prio_queue.disablePart(3);
+  ASSERT_THAT(prio_queue.max(), Eq(1));
+  ASSERT_THAT(prio_queue.maxKey(), Eq(25));
+
+  prio_queue.enablePart(3);
+  ASSERT_THAT(prio_queue.max(), Eq(0));
+  ASSERT_THAT(prio_queue.maxKey(), Eq(42));
+}
+
+TEST_F(AKWayPriorityQueue, DisablesInternalHeapIfItBecomesEmptyDueToRemoval) {
+  prio_queue.insert(1, 2, 25);
+  prio_queue.insert(0, 3, 42);
+  prio_queue.enablePart(2);
+  prio_queue.enablePart(3);
+  ASSERT_THAT(prio_queue.max(), Eq(0));
+  ASSERT_THAT(prio_queue.maxKey(), Eq(42));
+
+  prio_queue.remove(0, 3);
+  ASSERT_THAT(prio_queue.isEnabled(3), Eq(false));
+}
+
+TEST_F(AKWayPriorityQueue, ReturnsFalseIfUnusedHeapIsEmpty) {
+  prio_queue.insert(1, 2, 25);
+  ASSERT_THAT(prio_queue.empty(0), Eq(true));
+}
+
+TEST_F(AKWayPriorityQueue, ReturnsSizeZeroForUnusedInternalHeaps) {
+  prio_queue.insert(1, 2, 25);
+  prio_queue.insert(0, 0, 42);
+  ASSERT_THAT(prio_queue.size(3), Eq(0));
+}
+
+TEST_F(AKWayPriorityQueue, ReturnsCorrectSizeForUsedInternalHeaps) {
+  prio_queue.insert(1, 2, 25);
+  prio_queue.insert(2, 2, 23);
+  prio_queue.insert(0, 0, 42);
+  ASSERT_THAT(prio_queue.size(2), Eq(2));
+  ASSERT_THAT(prio_queue.size(0), Eq(1));
+}
+
+TEST_F(AKWayPriorityQueue, AnswersContainmentChecksForAllNonEmptyPQs) {
+  prio_queue.insert(1, 2, 25);
+  // disabled part
+  ASSERT_THAT(prio_queue.contains(1, 2), Eq(true));
+
+  prio_queue.insert(0, 0, 1);
+  prio_queue.enablePart(0);
+  // enabled part
+  ASSERT_THAT(prio_queue.contains(0, 0), Eq(true));
+
+  // unused part
+  ASSERT_THAT(prio_queue.contains(1, 1), Eq(false));
+}
+
+TEST_F(AKWayPriorityQueue, ReturnsNumberOfEnabledParts) {
+  ASSERT_THAT(prio_queue.numEnabledParts(), Eq(0));
+  prio_queue.insert(1, 2, 25);
+  prio_queue.enablePart(2);
+  ASSERT_THAT(prio_queue.numEnabledParts(), Eq(1));
+}
+
+TEST_F(AKWayPriorityQueueDeathTest, EnsuresThatEmptyPartsCannotBeEnabled) {
+  ASSERT_EXIT(prio_queue.enablePart(2), ::testing::KilledBySignal(SIGABRT),
+              ".*");
+}
+
+TEST_F(AKWayPriorityQueue, ReturnsNumberOfNonEmptyParts) {
+  ASSERT_THAT(prio_queue.numNonEmptyParts(), Eq(0));
+  prio_queue.insert(1, 2, 25);
+  ASSERT_THAT(prio_queue.numNonEmptyParts(), Eq(1));
+}
+} // namespace datastructure
