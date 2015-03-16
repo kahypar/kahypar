3475350263a4715cabb6abb596d89d9b37aaf65c
Vitali Henne <vitali.henne@gmail.com>
2015-03-16 18:21:36 +0100

    changed typelists from macro based to variadic template based. added more random functions

--- src/tools/RandomFunctions.h
+++ src/tools/RandomFunctions.h
@@ -31,11 +31,31 @@ class Randomize {
     std::shuffle(vector.begin(), vector.begin() + num_elements, _gen);
   }
 
+  // returns uniformly random int from the interval [low, high]
+  static int getRandomInt(int low, int high)
+  {
+    return _int_dist(_gen, std::uniform_int_distribution<int>::param_type(low,high));
+  }
+
+  // returns uniformly random float from the interval [low, high)
+  static float getRandomFloat(float low, float high)
+  {
+    return _float_dist(_gen, std::uniform_real_distribution<float>::param_type(low,high));
+  }
+
+  static float getNormalDistributedFloat(float mean, float std_dev)
+  {
+    return _norm_dist(_gen, std::normal_distribution<float>::param_type(mean, std_dev));
+  }
+
+
   private:
   static int _seed;
   static std::mt19937 _gen;
   static std::uniform_int_distribution<int> _bool_dist;
   static std::uniform_int_distribution<int> _int_dist;
+  static std::uniform_real_distribution<float> _float_dist;
+  static std::normal_distribution<float> _norm_dist;
 };
 
 #endif  // SRC_TOOLS_RANDOMFUNCTIONS_H_
