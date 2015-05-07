dd2246a61b605fb0f120e720d481be3198904a1a
Sebastian Schlag <sebastian.schlag@kit.edu>
2015-05-07 15:26:29 +0200

    embed gmock and gtest as submodules

--- cmake/modules/gmock.cmake
+++ cmake/modules/gmock.cmake
@@ -1,5 +1,5 @@
-include_directories(SYSTEM "${PROJECT_SOURCE_DIR}/gmock/include")
-include_directories(SYSTEM "${PROJECT_SOURCE_DIR}/gmock/gtest/include")
+include_directories(SYSTEM "${PROJECT_SOURCE_DIR}/external_tools/gmock/include")
+include_directories(SYSTEM "${PROJECT_SOURCE_DIR}/external_tools/gtest/include")
 set(Libgmock "${PROJECT_BINARY_DIR}/gmock-prefix/src/gmock-build/libgmock_main.a")
 
 # taken from http://johnlamp.net/cmake-tutorial-5-functionally-improved-testing.html
