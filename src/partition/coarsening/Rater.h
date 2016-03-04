dce0a3c2c96a985234d323ca3567b0356c576af9
Sebastian Schlag <sebastian.schlag@kit.edu>
2016-03-04 12:56:08 +0100

    use sparse map in Rater

--- src/partition/coarsening/Rater.h
+++ src/partition/coarsening/Rater.h
@@ -9,13 +9,13 @@
 #include <stack>
 #include <vector>
 
-#include "lib/datastructure/SparseSet.h"
+#include "lib/datastructure/SparseMap.h"
 #include "lib/definitions.h"
 #include "lib/macros.h"
 #include "partition/Configuration.h"
 #include "partition/coarsening/RatingTieBreakingPolicies.h"
 
-using datastructure::SparseSet;
+using datastructure::SparseMap;
 using defs::Hypergraph;
 using defs::HypernodeID;
 using defs::HyperedgeID;
@@ -63,8 +63,7 @@ class Rater {
   Rater(Hypergraph& hypergraph, const Configuration& config) noexcept :
     _hg(hypergraph),
     _config(config),
-    _tmp_ratings(_hg.initialNumNodes()),
-    _used_entries(_hg.initialNumNodes()) { }
+    _tmp_ratings(_hg.initialNumNodes()) { }
 
   Rater(const Rater&) = delete;
   Rater& operator= (const Rater&) = delete;
@@ -84,25 +83,23 @@ class Rater {
             belowThresholdNodeWeight(weight_u, _hg.nodeWeight(v)) &&
             (part_u == _hg.partID(v))) {
           _tmp_ratings[v] += score;
-          _used_entries.add(v);
         }
       }
     }
 
     RatingType max_rating = std::numeric_limits<RatingType>::min();
     HypernodeID target = std::numeric_limits<HypernodeID>::max();
-    for (auto it = _used_entries.crbegin(); it != _used_entries.crend(); ++it) {
-      const HypernodeID tmp_target = *it;
-      const RatingType tmp = _tmp_ratings[tmp_target] /
+    for (auto it = _tmp_ratings.crbegin(); it != _tmp_ratings.crend(); ++it) {
+      const HypernodeID tmp_target = it->key;
+      const RatingType tmp = it->value /
                              (weight_u * _hg.nodeWeight(tmp_target));
-      _tmp_ratings[tmp_target] = 0.0;
       DBG(false, "r(" << u << "," << tmp_target << ")=" << tmp);
       if (acceptRating(tmp, max_rating)) {
         max_rating = tmp;
         target = tmp_target;
       }
     }
-    _used_entries.clear();
+    _tmp_ratings.clear();
     HeavyEdgeRating ret;
     if (max_rating != std::numeric_limits<RatingType>::min()) {
       ASSERT(target != std::numeric_limits<HypernodeID>::max(), "invalid contraction target");
@@ -139,8 +136,7 @@ class Rater {
 
   Hypergraph& _hg;
   const Configuration& _config;
-  std::vector<RatingType> _tmp_ratings;
-  SparseSet<HypernodeID> _used_entries;
+  SparseMap<HypernodeID, RatingType> _tmp_ratings;
 };
 #pragma GCC diagnostic pop
 }  // namespace partition
