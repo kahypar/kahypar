492f6be33ee45c6c66fd747930eb92f5dde7fd7a
Sebastian Schlag <sebastian.schlag@kit.edu>
2016-03-10 14:52:32 +0100

    speedup PairingHeapWrapper
    It is actually faster to maintain an update stack than to use a
    sparse map in this case.

--- src/lib/datastructure/heaps/PairingHeapWrapper.h
+++ src/lib/datastructure/heaps/PairingHeapWrapper.h
@@ -4,36 +4,38 @@
 #ifndef SRC_LIB_DATASTRUCTURE_HEAPS_PAIRINGHEAPWRAPPER_H_
 #define SRC_LIB_DATASTRUCTURE_HEAPS_PAIRINGHEAPWRAPPER_H_
 
-#include <functional>
+#include <algorithm>
 #include <ext/pb_ds/priority_queue.hpp>
+#include <functional>
+#include <limits>
+#include <utility>
 #include <vector>
 
 #include "lib/macros.h"
-#include "lib/datastructure/SparseMap.h"
 
 namespace datastructure {
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Weffc++"
 // id_slot : ID Type for stored elements
 // key_slot: type of key_slot used
-// Meta key slot: min/max values for key_slot accessible via static functions ::max() / ::min()
+// Meta key slot: unused for wrapper
 template <typename id_slot, typename key_slot,
           typename meta_key_slot = std::numeric_limits<key_slot> >
 class PairingHeapWrapper {
  private:
-  using HeapElement = std::pair<id_slot,key_slot>;
+  using HeapElement = std::pair<id_slot, key_slot>;
 
   struct LessBy2nd {
-    bool operator()(const HeapElement& left, const HeapElement& right) const {
-      return left.second < right.second; };
+    bool operator() (const HeapElement& left, const HeapElement& right) const {
+      return left.second < right.second;
+    }
   };
 
   using Heap = __gnu_pbds::priority_queue<HeapElement,
                                           LessBy2nd,
                                           __gnu_pbds::pairing_heap_tag>;
 
-  Heap _heap;
-  SparseMap<id_slot, typename Heap::point_iterator> _map;
+  using HeapIterator = typename Heap::point_iterator;
 
  public:
   using value_type = id_slot;
@@ -43,7 +45,8 @@ class PairingHeapWrapper {
   explicit PairingHeapWrapper(const id_slot& storage_initializer,
                               const key_slot UNUSED(unused) = 0) noexcept :
     _heap(),
-    _map(storage_initializer) { }
+    _map(storage_initializer, nullptr),
+    _updates() { }
 
   PairingHeapWrapper(const PairingHeapWrapper&) = delete;
   PairingHeapWrapper& operator= (const PairingHeapWrapper&) = delete;
@@ -56,6 +59,7 @@ class PairingHeapWrapper {
     // necessary to call member swap
     _heap.swap(other._heap);
     swap(_map, other._map);
+    swap(_updates, other._updates);
   }
 
   size_t size() const noexcept {
@@ -66,22 +70,22 @@ class PairingHeapWrapper {
     return _heap.empty();
   }
 
-  // push an element onto the heap
   inline void push(const id_slot& id, const key_slot& key) noexcept {
     ASSERT(!contains(id), V(id));
     _map[id] = _heap.push(HeapElement(id, key));
+    _updates.push_back(id);
   }
 
   inline void deleteMax() noexcept {
-    ASSERT(_map.contains(_heap.top().first), V(_heap.top().first));
-    _map.remove(_heap.top().first);
+    ASSERT(contains(_heap.top().first), V(_heap.top().first));
+    _map[_heap.top().first] = nullptr;
     _heap.pop();
   }
 
   inline void deleteNode(const id_slot& id) noexcept {
-    ASSERT(_map.contains(id), V(id));
-    _heap.erase(_map.get(id));
-    _map.remove(id);
+    ASSERT(contains(id), V(id));
+    _heap.erase(_map[id]);
+    _map[id] = nullptr;
   }
 
   inline const id_slot & getMax() const noexcept {
@@ -93,48 +97,60 @@ class PairingHeapWrapper {
   }
 
   inline const key_slot & getKey(const id_slot& id) const noexcept {
-    ASSERT(_map.contains(id), V(id));
-    ASSERT(_map.get(id)->first == id, V(id));
-    return _map.get(id)->second;
+    ASSERT(contains(id), V(id));
+    return _map[id]->second;
   }
 
   inline bool contains(const id_slot& id) const noexcept {
-    return _map.contains(id);
+    return _map[id] != nullptr;
   }
 
   inline void decreaseKey(const id_slot& id, const key_slot& new_key) noexcept {
-    _heap.modify(_map.get(id), HeapElement(id, new_key));
+    ASSERT(contains(id), V(id));
+    _heap.modify(_map[id], HeapElement(id, new_key));
   }
 
   inline void increaseKey(const id_slot& id, const key_slot& new_key) noexcept {
-    _heap.modify(_map.get(id), HeapElement(id, new_key));
+    ASSERT(contains(id), V(id));
+    _heap.modify(_map[id], HeapElement(id, new_key));
   }
 
   inline void updateKey(const id_slot& id, const key_slot& new_key) noexcept {
-    _heap.modify(_map.get(id), HeapElement(id,new_key));
+    ASSERT(contains(id), V(id));
+    _heap.modify(_map[id], HeapElement(id, new_key));
   }
 
   inline void decreaseKeyBy(const id_slot& id, const key_slot& key_delta) noexcept {
-    _heap.modify(_map.get(id), HeapElement(id, _map.get(id)->second - key_delta));
+    ASSERT(contains(id), V(id));
+    _heap.modify(_map[id], HeapElement(id, _map[id]->second - key_delta));
   }
 
   inline void increaseKeyBy(const id_slot& id, const key_slot& key_delta) noexcept {
-    _heap.modify(_map.get(id), HeapElement(id, _map.get(id)->second + key_delta));
+    ASSERT(contains(id), V(id));
+    _heap.modify(_map[id], HeapElement(id, _map[id]->second + key_delta));
   }
 
   inline void updateKeyBy(const id_slot& id, const key_slot& key_delta) noexcept {
-    _heap.modify(_map.get(id), HeapElement(id,_map.get(id)->second + key_delta));
+    ASSERT(contains(id), V(id));
+    _heap.modify(_map[id], HeapElement(id, _map[id]->second + key_delta));
   }
 
   inline void clear() noexcept {
     _heap.clear();
-    _map.clear();
+    for (const auto id : _updates) {
+      _map[id] = nullptr;
+    }
+    _updates.clear();
   }
 
   inline void clearHeap() noexcept {
     clear();
   }
 
+ private:
+  Heap _heap;
+  std::vector<HeapIterator> _map;
+  std::vector<id_slot> _updates;
 };
 #pragma GCC diagnostic pop
 
