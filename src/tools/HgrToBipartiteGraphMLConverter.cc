09b8961a45b1c204b87a1158a89cb8ee26fac8c9
Sebastian Schlag <sebastian.schlag@kit.edu>
2016-06-01 15:45:02 +0200

    allow variable # blocks - lift bipartition restriction

--- src/tools/HgrToBipartiteGraphMLConverter.cc
+++ src/tools/HgrToBipartiteGraphMLConverter.cc
@@ -21,24 +21,28 @@ int main(int argc, char* argv[]) {
   }
   std::string hgr_filename(argv[1]);
 
-  Hypergraph hypergraph(io::createHypergraphFromFile(hgr_filename, 2));
-
-  PartitionID max_part = 0;
+  PartitionID max_part = 1;
+  std::vector<defs::PartitionID> partition;
   if (argc == 3) {
-    std::vector<defs::PartitionID> partition;
     std::string partition_filename(argv[2]);
     std::cout << "Reading partition file: " << partition_filename << std::endl;
     io::readPartitionFile(partition_filename, partition);
-    if (partition.size() != hypergraph.numNodes()) {
-      std::cout << "partition file has incorrect size. Exiting." << std::endl;
-      exit(-1);
-    }
-
     for (size_t index = 0; index < partition.size(); ++index) {
       max_part = std::max(max_part, partition[index]);
-      hypergraph.setNodePart(index, partition[index]);
     }
   }
+
+  Hypergraph hypergraph(io::createHypergraphFromFile(hgr_filename, max_part + 1));
+
+  if (partition.size() != 0 && partition.size() != hypergraph.numNodes()) {
+    std::cout << "partition file has incorrect size. Exiting." << std::endl;
+    exit(-1);
+  }
+
+  for (size_t index = 0; index < partition.size(); ++index) {
+    hypergraph.setNodePart(index, partition[index]);
+  }
+
   std::string graphml_filename(hgr_filename + ".k" + std::to_string(max_part + 1) + ".graphml");
 
   std::cout << "write graphml file: " << graphml_filename << std::endl;
