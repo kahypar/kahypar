cc65b3d0d3ac5caff9d144dd297ea9a2cfa3b741
Sebastian Schlag <sebastian.schlag@kit.edu>
2016-07-24 14:26:00 +0200

    implement mod to wrap around before pos in case of i=0

--- src/lib/datastructure/IncidenceSet.h
+++ src/lib/datastructure/IncidenceSet.h
@@ -155,7 +155,7 @@ class IncidenceSet {
 
   bool contains(const T key) const {
     const Position start_position = utils::crc32(key) % _max_sparse_size;
-    const Position before = start_position != 0 ? start_position - 1 : _max_sparse_size - 1;
+    const Position before = mod(static_cast<std::uint32_t>(start_position) - 1, _max_sparse_size);
     for (Position position = start_position; position < _max_sparse_size; position = (position + 1) % _max_sparse_size) {
       if (sparse()[position].first == empty) {
         return false;
@@ -255,6 +255,10 @@ class IncidenceSet {
 
   const Element* sparse() const {
     return reinterpret_cast<const Element*>(dense() + _max_size + 1);
+  }
+
+  std::int32_t mod(std::int32_t a, std::int32_t b) const {
+    return (a%b+b)%b;
   }
 
   T* _memory;
