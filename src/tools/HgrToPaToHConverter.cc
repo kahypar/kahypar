b0d222a93e292626ef809f7014349c510df0a1f9
Sebastian Schlag <sebastian.schlag@kit.edu>
2016-08-01 15:56:05 +0200

    add infrastructure stuff for kPaToH

---
+++ src/tools/HgrToPaToHConverter.cc
@@ -0,0 +1,29 @@
+/***************************************************************************
+ *  Copyright (C) 2016 Sebastian Schlag <sebastian.schlag@kit.edu>
+ **************************************************************************/
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include "lib/definitions.h"
+#include "lib/io/HypergraphIO.h"
+#include "lib/macros.h"
+
+int main(int argc, char* argv[]) {
+  if (argc != 3) {
+    std::cout << "No .hgr file specified" << std::endl;
+    std::cout << "Usage: HgrToPaToHConverter <.hgr> <outfile>" << std::endl;
+    exit(0);
+  }
+  std::string hgr_filename(argv[1]);
+  std::string out_filename(argv[2]);
+
+  Hypergraph hypergraph(
+    io::createHypergraphFromFile(hgr_filename,2));
+
+  io::writeHypergraphForPaToHPartitioning(hypergraph, out_filename);
+
+  return 0;
+}
