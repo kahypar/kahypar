0ef65e518327b2e30088728cff326ea33b93e70b
Sebastian Schlag <sebastian.schlag@kit.edu>
2016-09-08 15:47:58 +0200

    explicitly define randmization members in KaHyPar.cc

--- src/application/KaHyPar.cc
+++ src/application/KaHyPar.cc
@@ -574,6 +574,13 @@ void processCommandLineInput(Configuration& config, std::string& result_file,
   }
 }
 
+int Randomize::_seed = -1;
+std::mt19937 Randomize::_gen;
+std::uniform_int_distribution<int> Randomize::_bool_dist(0, 1);
+std::uniform_int_distribution<int> Randomize::_int_dist(0, std::numeric_limits<int>::max());
+std::uniform_real_distribution<float> Randomize::_float_dist(0, 1);
+std::normal_distribution<float> Randomize::_norm_dist(0, 1);
+
 int main(int argc, char* argv[]) {
   Configuration config;
   std::string result_file;
