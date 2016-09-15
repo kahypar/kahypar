bb568fc3901ef26d2ec2a4300aeb970ae16adaaa
Sebastian Schlag <sebastian.schlag@kit.edu>
2016-09-15 10:05:13 +0200

    colllect ip headers into one header

---
+++ kahypar/partition/initial_partitioning/initial_partitioning.h
@@ -0,0 +1,15 @@
+/***************************************************************************
+ *  Copyright (C) 2016 Sebastian Schlag <sebastian.schlag@kit.edu>
+ **************************************************************************/
+
+#pragma once
+
+#include "partition/initial_partitioning/bfs_initial_partitioner.h"
+#include "partition/initial_partitioning/greedy_hypergraph_growing_initial_partitioner.h"
+#include "partition/initial_partitioning/i_initial_partitioner.h"
+#include "partition/initial_partitioning/label_propagation_initial_partitioner.h"
+#include "partition/initial_partitioning/policies/ip_gain_computation_policy.h"
+#include "partition/initial_partitioning/policies/ip_greedy_queue_selection_policy.h"
+#include "partition/initial_partitioning/policies/ip_start_node_selection_policy.h"
+#include "partition/initial_partitioning/pool_initial_partitioner.h"
+#include "partition/initial_partitioning/random_initial_partitioner.h"
