0af5e6f472badb0866a52b322fabb75902baf669
Sebastian Schlag <sebastian.schlag@kit.edu>
2016-09-28 14:14:19 +0200

    add integration test for KaHyPar-R
    with km1 and cut-net objective

--- tests/end_to_end/kahypar_integration_tests.cc
+++ tests/end_to_end/kahypar_integration_tests.cc
@@ -11,10 +11,9 @@
 #include "kahypar/utils/randomize.h"
 
 namespace kahypar {
-
-class KaHyPar : public ::testing::Test {
+class KaHyParK : public::testing::Test {
  public:
-  KaHyPar() :
+  KaHyParK() :
     config() {
     config.partition.mode = Mode::direct_kway;
     config.partition.objective = Objective::cut;
@@ -37,15 +36,42 @@ class KaHyPar : public ::testing::Test {
     config.partition.graph_filename = "test_instances/ISPD98_ibm01.hgr";
 
     kahypar::Randomize::instance().setSeed(config.partition.seed);
-
   }
 
   Configuration config;
 };
 
 
+class KaHyParR : public::testing::Test {
+ public:
+  KaHyParR() :
+    config() {
+    config.partition.mode = Mode::recursive_bisection;
+    config.partition.objective = Objective::cut;
+    config.partition.seed = 2;
+    config.coarsening.algorithm = CoarseningAlgorithm::heavy_lazy;
+    config.coarsening.max_allowed_weight_multiplier = 3.25;
+    config.coarsening.contraction_limit_multiplier = 160;
+    config.initial_partitioning.tool = InitialPartitioner::KaHyPar;
+    config.initial_partitioning.mode = Mode::direct_kway;
+    config.initial_partitioning.technique = InitialPartitioningTechnique::flat;
+    config.initial_partitioning.algo = InitialPartitionerAlgorithm::pool;
+    config.initial_partitioning.nruns = 20;
+    config.initial_partitioning.local_search.algorithm = RefinementAlgorithm::twoway_fm;
+    config.initial_partitioning.local_search.iterations_per_level = std::numeric_limits<int>::max();
+    config.local_search.iterations_per_level = std::numeric_limits<int>::max();
+    config.local_search.fm.stopping_rule = RefinementStoppingRule::simple;
+    config.local_search.fm.max_number_of_fruitless_moves = 50;
+    config.partition.graph_filename = "test_instances/ISPD98_ibm01.hgr";
 
-TEST_F(KaHyPar, ComputesDirectKwayCutPartitioning) {
+    kahypar::Randomize::instance().setSeed(config.partition.seed);
+  }
+
+  Configuration config;
+};
+
+
+TEST_F(KaHyParK, ComputesDirectKwayCutPartitioning) {
   config.partition.k = 8;
   config.partition.epsilon = 0.03;
   config.partition.objective = Objective::cut;
@@ -62,10 +88,9 @@ TEST_F(KaHyPar, ComputesDirectKwayCutPartitioning) {
   ASSERT_EQ(metrics::hyperedgeCut(hypergraph), 887);
   ASSERT_EQ(metrics::soed(hypergraph), 2134);
   ASSERT_EQ(metrics::km1(hypergraph), 1247);
-
 }
 
-TEST_F(KaHyPar, ComputesDirectKwayKm1Partitioning) {
+TEST_F(KaHyParK, ComputesDirectKwayKm1Partitioning) {
   config.partition.k = 8;
   config.partition.epsilon = 0.03;
   config.partition.objective = Objective::km1;
@@ -85,4 +110,41 @@ TEST_F(KaHyPar, ComputesDirectKwayKm1Partitioning) {
 }
 
 
-} // namespace kahypar
+TEST_F(KaHyParR, ComputesRecursiveBisectionCutPartitioning) {
+  config.partition.k = 8;
+  config.partition.epsilon = 0.03;
+  config.partition.objective = Objective::cut;
+  config.local_search.algorithm = RefinementAlgorithm::twoway_fm;
+
+  Hypergraph hypergraph(
+    kahypar::io::createHypergraphFromFile(config.partition.graph_filename,
+                                          config.partition.k));
+
+  Partitioner partitioner;
+  partitioner.partition(hypergraph, config);
+  kahypar::io::printPartitioningResults(hypergraph, config, std::chrono::duration<double>(0.0));
+
+  ASSERT_EQ(metrics::hyperedgeCut(hypergraph), 892);
+  ASSERT_EQ(metrics::soed(hypergraph), 1989);
+  ASSERT_EQ(metrics::km1(hypergraph), 1097);
+}
+
+TEST_F(KaHyParR, ComputesRecursiveBisectionKm1Partitioning) {
+  config.partition.k = 8;
+  config.partition.epsilon = 0.03;
+  config.partition.objective = Objective::km1;
+  config.local_search.algorithm = RefinementAlgorithm::twoway_fm;
+
+  Hypergraph hypergraph(
+    kahypar::io::createHypergraphFromFile(config.partition.graph_filename,
+                                          config.partition.k));
+
+  Partitioner partitioner;
+  partitioner.partition(hypergraph, config);
+  kahypar::io::printPartitioningResults(hypergraph, config, std::chrono::duration<double>(0.0));
+
+  ASSERT_EQ(metrics::hyperedgeCut(hypergraph), 895);
+  ASSERT_EQ(metrics::soed(hypergraph), 1850);
+  ASSERT_EQ(metrics::km1(hypergraph), 955);
+}
+}  // namespace kahypar
