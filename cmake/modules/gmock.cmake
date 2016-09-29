54faa72b56149a80991531b00f8ea39373366b62
Sebastian Schlag <sebastian.schlag@kit.edu>
2016-09-29 16:19:19 +0200

    do not directly depend on pthreads for MSVC
    [skip ci]

--- cmake/modules/gmock.cmake
+++ cmake/modules/gmock.cmake
@@ -1,7 +1,10 @@
 # taken from http://johnlamp.net/cmake-tutorial-5-functionally-improved-testing.html
 function(add_gmock_test target)
     add_executable(${target} ${ARGN})
-    target_link_libraries(${target} gtest gtest_main pthread)
+    target_link_libraries(${target} gtest gtest_main ${CMAKE_THREAD_LIBS_INIT})
+
+    set_property(TARGET ${target} PROPERTY CXX_STANDARD 14)
+    set_property(TARGET ${target} PROPERTY CXX_STANDARD_REQUIRED ON)
 
     add_test(${target} ${target})
 
