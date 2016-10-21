c24acea035775b33e994342daafca3741e8cfe9d
Sebastian Schlag <sebastian.schlag@kit.edu>
2016-10-21 16:01:19 +0200

    apply codestyle and improve constness
    [skip ci]

--- kahypar/utils/math.h
+++ kahypar/utils/math.h
@@ -103,65 +103,63 @@ static inline T hash(const T& x) {
 
 template <typename Key>
 class MurmurHash {
-public:
-    using HashValue = std::uint64_t;
+ public:
+  using HashValue = std::uint64_t;
 
-    explicit MurmurHash(uint32_t seed = 0)
-            :   _seed(seed)
-    {}
+  explicit MurmurHash(const uint32_t seed = 0) :
+    _seed(seed)
+  { }
 
-    void reset(uint32_t seed) {
-      _seed = seed;
+  void reset(const uint32_t seed) {
+    _seed = seed;
+  }
+
+  inline HashValue operator() (const Key& key) const {
+    return hash(reinterpret_cast<const void*>(&key), sizeof(key), _seed);
+  }
+
+ private:
+  inline uint64_t hash(const void* key, const uint32_t len, const uint32_t seed) const {
+    const uint64_t m = 0xc6a4a7935bd1e995;
+    const int r = 47;
+
+    uint64_t h = seed ^ (len * m);
+
+    const uint64_t* data = (const uint64_t*)key;
+    const uint64_t* end = data + (len / 8);
+
+    while (data != end) {
+      uint64_t k = *data++;
+
+      k *= m;
+      k ^= k >> r;
+      k *= m;
+
+      h ^= k;
+      h *= m;
     }
 
-    inline HashValue operator() (const Key& key) const {
-        return hash(reinterpret_cast<const void*>(&key), sizeof(key), _seed);
-    }
+    const unsigned char* data2 = (const unsigned char*)data;
 
-private:
-    uint32_t _seed;
-
-    inline uint64_t hash(const void* key, uint32_t len, uint32_t seed) const {
-        const uint64_t m = 0xc6a4a7935bd1e995;
-        const int r = 47;
-
-        uint64_t h = seed ^ (len * m);
-
-        const uint64_t * data = (const uint64_t *)key;
-        const uint64_t * end = data + (len/8);
-
-        while(data != end)
-        {
-            uint64_t k = *data++;
-
-            k *= m;
-            k ^= k >> r;
-            k *= m;
-
-            h ^= k;
-            h *= m;
-        }
-
-        const unsigned char * data2 = (const unsigned char*)data;
-
-        switch(len & 7)
-        {
-            case 7: h ^= uint64_t(data2[6]) << 48;
-            case 6: h ^= uint64_t(data2[5]) << 40;
-            case 5: h ^= uint64_t(data2[4]) << 32;
-            case 4: h ^= uint64_t(data2[3]) << 24;
-            case 3: h ^= uint64_t(data2[2]) << 16;
-            case 2: h ^= uint64_t(data2[1]) << 8;
-            case 1: h ^= uint64_t(data2[0]);
-                h *= m;
-        };
-
-        h ^= h >> r;
+    switch (len & 7) {
+      case 7: h ^= uint64_t(data2[6]) << 48;
+      case 6: h ^= uint64_t(data2[5]) << 40;
+      case 5: h ^= uint64_t(data2[4]) << 32;
+      case 4: h ^= uint64_t(data2[3]) << 24;
+      case 3: h ^= uint64_t(data2[2]) << 16;
+      case 2: h ^= uint64_t(data2[1]) << 8;
+      case 1: h ^= uint64_t(data2[0]);
         h *= m;
-        h ^= h >> r;
-
-        return h;
     }
+
+    h ^= h >> r;
+    h *= m;
+    h ^= h >> r;
+
+    return h;
+  }
+
+  uint32_t _seed;
 };
 
 #define XXH_PRIVATE_API
@@ -170,7 +168,7 @@ template <typename Key>
 struct XXHash {
   using HashValue = std::uint64_t;
 
-  KAHYPAR_ATTRIBUTE_ALWAYS_INLINE uint64_t operator()(const Key k) const {
+  KAHYPAR_ATTRIBUTE_ALWAYS_INLINE uint64_t operator() (const Key k) const {
     return XXH64(&k, sizeof(Key), _seed);
   }
 
