kahypar/kahypar

commit 37c3f95053d6e39da0ddbcb9709572fc4d267d05

remove code duplication of heavy edge rater

kahypar/partition/coarsening/heavy_edge_rater.h
@@ -24,19 +24,26 @@
 #include <stack>
 #include <vector>
 
+#include "kahypar/datastructure/fast_reset_flag_array.h"
 #include "kahypar/datastructure/sparse_map.h"
 #include "kahypar/definitions.h"
 #include "kahypar/macros.h"
+#include "kahypar/partition/coarsening/policies/rating_acceptance_policy.h"
+#include "kahypar/partition/coarsening/policies/rating_heavy_node_penalty_policy.h"
 #include "kahypar/partition/configuration.h"
 #include "kahypar/partition/preprocessing/louvain.h"
 #include "kahypar/utils/stats.h"
 
 namespace kahypar {
-template <typename _RatingType, class _TieBreakingPolicy>
+template <typename _RatingType,
+          class _TieBreakingPolicy,
+          class _AcceptancePolicy = BestRatingWithRandomTieBreaking<_TieBreakingPolicy>,
+          class _NodeWeightPenalty = MultiplicativePenalty>
 class HeavyEdgeRater {
  private:
   static constexpr bool debug = false;
-  using TieBreakingPolicy = _TieBreakingPolicy;
+  using AcceptancePolicy = _AcceptancePolicy;
+  using NodeWeightPenalty = _NodeWeightPenalty;
 
   class HeavyEdgeRating {
  public:
@@ -71,7 +78,8 @@ class HeavyEdgeRater {
     _hg(hypergraph),
     _config(config),
     _tmp_ratings(_hg.initialNumNodes()),
-    _comm() {
+    _comm(),
+    _already_matched(_hg.initialNumNodes()) {
     if (_config.preprocessing.enable_louvain_community_detection) {
       _comm = detectCommunities(_hg, _config);
     } else {
@@ -95,8 +103,7 @@ class HeavyEdgeRater {
       ASSERT(_hg.edgeSize(he) > 1, V(he));
       const RatingType score = static_cast<RatingType>(_hg.edgeWeight(he)) / (_hg.edgeSize(he) - 1);
       for (const HypernodeID& v : _hg.pins(he)) {
-        if (v != u && _comm[u] == _comm[v] &&
-            belowThresholdNodeWeight(weight_u, _hg.nodeWeight(v)) &&
+        if (v != u && belowThresholdNodeWeight(weight_u, _hg.nodeWeight(v)) &&
             (part_u == _hg.partID(v))) {
           _tmp_ratings[v] += score;
         }
@@ -107,14 +114,18 @@ class HeavyEdgeRater {
     HypernodeID target = std::numeric_limits<HypernodeID>::max();
     for (auto it = _tmp_ratings.end() - 1; it >= _tmp_ratings.begin(); --it) {
       const HypernodeID tmp_target = it->key;
-      const RatingType tmp = it->value /
-                             (weight_u * _hg.nodeWeight(tmp_target));
-      DBG << "r(" << u << "," << tmp_target << ")=" << tmp;
-      if (acceptRating(tmp, max_rating)) {
-        max_rating = tmp;
+      const RatingType tmp_rating = it->value /
+                                    NodeWeightPenalty::penalty(weight_u,
+                                                               _hg.nodeWeight(tmp_target));
+      DBG << "r(" << u << "," << tmp_target << ")=" << tmp_rating;
+      if (_comm[u] == _comm[tmp_target] &&
+          AcceptancePolicy::acceptRating(tmp_rating, max_rating,
+                                         target, tmp_target, _already_matched)) {
+        max_rating = tmp_rating;
         target = tmp_target;
       }
     }
+
     _tmp_ratings.clear();
     HeavyEdgeRating ret;
     if (max_rating != std::numeric_limits<RatingType>::min()) {
@@ -122,6 +133,7 @@ class HeavyEdgeRater {
       ret.value = max_rating;
       ret.target = target;
       ret.valid = true;
+      ASSERT(_comm[u] == _comm[ret.target]);
     }
     ASSERT([&]() {
         bool flag = true;
@@ -135,6 +147,14 @@ class HeavyEdgeRater {
     return ret;
   }
 
+  void markAsMatched(const HypernodeID hn) {
+    _already_matched.set(hn, true);
+  }
+
+  void resetMatches() {
+    _already_matched.reset();
+  }
+
   HypernodeWeight thresholdNodeWeight() const {
     return _config.coarsening.max_allowed_node_weight;
   }
@@ -145,13 +165,10 @@ class HeavyEdgeRater {
     return weight_v + weight_u <= _config.coarsening.max_allowed_node_weight;
   }
 
-  bool acceptRating(const RatingType tmp, const RatingType max_rating) const {
-    return max_rating < tmp || (max_rating == tmp && TieBreakingPolicy::acceptEqual());
-  }
-
   Hypergraph& _hg;
   const Configuration& _config;
   ds::SparseMap<HypernodeID, RatingType> _tmp_ratings;
   std::vector<ClusterID> _comm;
+  ds::FastResetFlagArray<> _already_matched;
 };
 }  // namespace kahypar

kahypar/partition/coarsening/ml_coarsener.h
@@ -24,10 +24,13 @@
 #include <string>
 #include <vector>
 
-#include "kahypar/datastructure/fast_reset_flag_array.h"
+
 #include "kahypar/datastructure/sparse_map.h"
 #include "kahypar/definitions.h"
 #include "kahypar/macros.h"
+#include "kahypar/partition/coarsening/heavy_edge_rater.h"
+#include "kahypar/partition/coarsening/policies/rating_acceptance_policy.h"
+#include "kahypar/partition/coarsening/policies/rating_heavy_node_penalty_policy.h"
 #include "kahypar/partition/coarsening/policies/rating_tie_breaking_policy.h"
 #include "kahypar/partition/preprocessing/louvain.h"
 #include "kahypar/partition/preprocessing/modularity.h"
@@ -40,40 +43,18 @@ class MLCoarsener final : public ICoarsener,
   static constexpr bool debug = false;
 
   static constexpr HypernodeID kInvalidTarget = std::numeric_limits<HypernodeID>::max();
-  static constexpr RatingType kInvalidScore = std::numeric_limits<RatingType>::min();
 
   using Base = VertexPairCoarsenerBase;
-
-  struct Rating {
-    Rating(HypernodeID trgt, RatingType val, bool is_valid) :
-      target(trgt),
-      value(val),
-      valid(is_valid) { }
-
-    Rating() :
-      target(kInvalidTarget),
-      value(kInvalidScore),
-      valid(false) { }
-
-    Rating(const Rating&) = delete;
-    Rating& operator= (const Rating&) = delete;
-
-    Rating(Rating&&) = default;
-    Rating& operator= (Rating&&) = delete;
-
-    ~Rating() = default;
-
-    HypernodeID target;
-    RatingType value;
-    bool valid;
-  };
+  using Rater = HeavyEdgeRater<RatingType, RandomRatingWins,
+                               BestRatingPreferringUnmatched<RandomRatingWins>,
+                               NoWeightPenalty>;
+  using Rating = typename Rater::Rating;
 
  public:
   MLCoarsener(Hypergraph& hypergraph, const Configuration& config,
               const HypernodeWeight weight_of_heaviest_node) :
     Base(hypergraph, config, weight_of_heaviest_node),
-    _tmp_ratings(_hg.initialNumNodes()),
-    _comm(_hg.initialNumNodes(), 0) { }
+    _rater(_hg, _config) { }
 
   ~MLCoarsener() override = default;
 
@@ -87,18 +68,13 @@ class MLCoarsener final : public ICoarsener,
   void coarsenImpl(const HypernodeID limit) override final {
     int pass_nr = 0;
     std::vector<HypernodeID> current_hns;
-    ds::FastResetFlagArray<> already_matched(_hg.initialNumNodes());
-
-    if (_config.preprocessing.enable_louvain_community_detection) {
-      _comm = detectCommunities(_hg, _config);
-    }
 
     while (_hg.currentNumNodes() > limit) {
       DBG << V(pass_nr);
       DBG << V(_hg.currentNumNodes());
       DBG << V(_hg.currentNumEdges());
 
-      already_matched.reset();
+      _rater.resetMatches();
       current_hns.clear();
 
       const HypernodeID num_hns_before_pass = _hg.currentNumNodes();
@@ -113,13 +89,13 @@ class MLCoarsener final : public ICoarsener,
 
       for (const HypernodeID& hn : current_hns) {
         if (_hg.nodeIsEnabled(hn)) {
-          const Rating rating = contractionPartner(hn, already_matched);
+          const Rating rating = _rater.rate(hn);
 
           if (rating.target != kInvalidTarget) {
-            already_matched.set(hn, true);
-            already_matched.set(rating.target, true);
+            _rater.markAsMatched(hn);
+            _rater.markAsMatched(rating.target);
             // if (_hg.nodeDegree(hn) > _hg.nodeDegree(rating.target)) {
-            ASSERT(_comm[hn] == _comm[rating.target], "Contracted nodes are not in same community!");
+
             performContraction(hn, rating.target);
             // } else {
             //   contract(rating.target, hn);
@@ -140,62 +116,6 @@ class MLCoarsener final : public ICoarsener,
     Stats::instance().addToTotal(_config, "hns_after_coarsening", _hg.currentNumNodes());
   }
 
-  Rating contractionPartner(const HypernodeID u, const ds::FastResetFlagArray<>& already_matched) {
-    DBG << "Calculating rating for HN" << u;
-    const HypernodeWeight weight_u = _hg.nodeWeight(u);
-    for (const HyperedgeID& he : _hg.incidentEdges(u)) {
-      ASSERT(_hg.edgeSize(he) > 1, V(he));
-      if (_hg.edgeSize(he) <= _config.partition.hyperedge_size_threshold) {
-        const RatingType score = static_cast<RatingType>(_hg.edgeWeight(he)) / (_hg.edgeSize(he) - 1);
-        for (const HypernodeID& v : _hg.pins(he)) {
-          if ((v != u && belowThresholdNodeWeight(weight_u, _hg.nodeWeight(v)))) {
-            _tmp_ratings[v] += score;
-          }
-        }
-      }
-    }
-    ASSERT(!_tmp_ratings.contains(u), V(u));
-
-    RatingType max_rating = std::numeric_limits<RatingType>::min();
-    HypernodeID target = std::numeric_limits<HypernodeID>::max();
-    for (auto it = _tmp_ratings.end() - 1; it >= _tmp_ratings.begin(); --it) {
-      const HypernodeID tmp_target = it->key;
-      const RatingType tmp_rating = it->value;
-      DBG << "r(" << u << "," << tmp_target << ")=" << tmp_rating;
-      if (_comm[u] == _comm[tmp_target] && acceptRating(tmp_rating, max_rating, target, tmp_target, already_matched)) {
-        max_rating = tmp_rating;
-        target = tmp_target;
-      }
-    }
-
-    Rating ret;
-    if (max_rating != std::numeric_limits<RatingType>::min()) {
-      ASSERT(target != std::numeric_limits<HypernodeID>::max());
-      ASSERT(_tmp_ratings[target] == max_rating, V(target));
-      ret.value = max_rating;
-      ret.target = target;
-      ret.valid = true;
-    }
-
-    _tmp_ratings.clear();
-
-    ASSERT(!ret.valid || (_hg.partID(u) == _hg.partID(ret.target)));
-    DBG << "rating=(" << ret.value << "," << ret.target << "," << ret.valid << ")";
-    return ret;
-  }
-
-  bool acceptRating(const RatingType tmp, const RatingType max_rating,
-                    const HypernodeID old_target, const HypernodeID new_target,
-                    const ds::FastResetFlagArray<>& already_matched) const {
-    return max_rating < tmp ||
-           ((max_rating == tmp) &&
-            ((already_matched[old_target] && !already_matched[new_target]) ||
-             (already_matched[old_target] && already_matched[new_target] &&
-              RandomRatingWins::acceptEqual()) ||
-             (!already_matched[old_target] && !already_matched[new_target] &&
-              RandomRatingWins::acceptEqual())));
-  }
-
   bool uncoarsenImpl(IRefiner& refiner) override final {
     return doUncoarsen(refiner);
   }
@@ -204,16 +124,10 @@ class MLCoarsener final : public ICoarsener,
     return std::string("");
   }
 
-  bool belowThresholdNodeWeight(const HypernodeWeight weight_u,
-                                const HypernodeWeight weight_v) const {
-    return weight_v + weight_u <= _config.coarsening.max_allowed_node_weight;
-  }
-
   using Base::_pq;
   using Base::_hg;
   using Base::_config;
   using Base::_history;
-  ds::SparseMap<HypernodeID, RatingType> _tmp_ratings;
-  std::vector<ClusterID> _comm;
+  Rater _rater;
 };
 }  // namespace kahypar

kahypar/partition/coarsening/policies/rating_acceptance_policy.h
@@ -1,0 +1,70 @@
+/*******************************************************************************
+ * This file is part of KaHyPar.
+ *
+ * Copyright (C) 2017 Sebastian Schlag <sebastian.schlag@kit.edu>
+ *
+ * KaHyPar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KaHyPar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with KaHyPar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+******************************************************************************/
+
+#pragma once
+
+#include "kahypar/definitions.h"
+
+namespace kahypar {
+template <class TieBreakingPolicy>
+class BestRatingWithRandomTieBreaking {
+ public:
+  static bool acceptRating(const RatingType tmp, const RatingType max_rating,
+                           const HypernodeID, const HypernodeID,
+                           const ds::FastResetFlagArray<>&) {
+    return max_rating < tmp || (max_rating == tmp && TieBreakingPolicy::acceptEqual());
+  }
+
+  BestRatingWithRandomTieBreaking(const BestRatingWithRandomTieBreaking&) = delete;
+  BestRatingWithRandomTieBreaking& operator= (const BestRatingWithRandomTieBreaking&) = delete;
+
+  BestRatingWithRandomTieBreaking(BestRatingWithRandomTieBreaking&&) = delete;
+  BestRatingWithRandomTieBreaking& operator= (BestRatingWithRandomTieBreaking&&) = delete;
+
+ protected:
+  ~BestRatingWithRandomTieBreaking() = default;
+};
+
+
+template <class TieBreakingPolicy>
+class BestRatingPreferringUnmatched {
+ public:
+  static bool acceptRating(const RatingType tmp, const RatingType max_rating,
+                           const HypernodeID old_target, const HypernodeID new_target,
+                           const ds::FastResetFlagArray<>& already_matched) {
+    return max_rating < tmp ||
+           ((max_rating == tmp) &&
+            ((already_matched[old_target] && !already_matched[new_target]) ||
+             (already_matched[old_target] && already_matched[new_target] &&
+              TieBreakingPolicy::acceptEqual()) ||
+             (!already_matched[old_target] && !already_matched[new_target] &&
+              TieBreakingPolicy::acceptEqual())));
+  }
+
+  BestRatingPreferringUnmatched(const BestRatingPreferringUnmatched&) = delete;
+  BestRatingPreferringUnmatched& operator= (const BestRatingPreferringUnmatched&) = delete;
+
+  BestRatingPreferringUnmatched(BestRatingPreferringUnmatched&&) = delete;
+  BestRatingPreferringUnmatched& operator= (BestRatingPreferringUnmatched&&) = delete;
+
+ protected:
+  ~BestRatingPreferringUnmatched() = default;
+};
+}  // namespace kahypar

kahypar/partition/coarsening/policies/rating_heavy_node_penalty_policy.h
@@ -1,0 +1,58 @@
+/*******************************************************************************
+ * This file is part of KaHyPar.
+ *
+ * Copyright (C) 2017 Sebastian Schlag <sebastian.schlag@kit.edu>
+ *
+ * KaHyPar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KaHyPar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with KaHyPar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+******************************************************************************/
+
+#pragma once
+
+#include "kahypar/definitions.h"
+
+
+namespace kahypar {
+class MultiplicativePenalty {
+ public:
+  static HypernodeWeight penalty(const HypernodeWeight weight_u, const HypernodeWeight weight_v) {
+    return weight_u * weight_v;
+  }
+
+  MultiplicativePenalty(const MultiplicativePenalty&) = delete;
+  MultiplicativePenalty& operator= (const MultiplicativePenalty&) = delete;
+
+  MultiplicativePenalty(MultiplicativePenalty&&) = delete;
+  MultiplicativePenalty& operator= (MultiplicativePenalty&&) = delete;
+
+ protected:
+  ~MultiplicativePenalty() = default;
+};
+
+class NoWeightPenalty {
+ public:
+  static HypernodeWeight penalty(const HypernodeWeight, const HypernodeWeight) {
+    return 1;
+  }
+
+  NoWeightPenalty(const NoWeightPenalty&) = delete;
+  NoWeightPenalty& operator= (const NoWeightPenalty&) = delete;
+
+  NoWeightPenalty(NoWeightPenalty&&) = delete;
+  NoWeightPenalty& operator= (NoWeightPenalty&&) = delete;
+
+ protected:
+  ~NoWeightPenalty() = default;
+};
+}  // namespace kahypar

kahypar/partition/coarsening/policies/rating_tie_breaking_policy.h
@@ -35,7 +35,8 @@ class LastRatingWins {
   LastRatingWins(LastRatingWins&&) = delete;
   LastRatingWins& operator= (LastRatingWins&&) = delete;
 
- protected: ~LastRatingWins() = default;
+ protected:
+  ~LastRatingWins() = default;
 };
 
 class FirstRatingWins {
@@ -50,7 +51,8 @@ class FirstRatingWins {
   FirstRatingWins(FirstRatingWins&&) = delete;
   FirstRatingWins& operator= (FirstRatingWins&&) = delete;
 
- protected: ~FirstRatingWins() = default;
+ protected:
+  ~FirstRatingWins() = default;
 };
 
 class RandomRatingWins {
@@ -65,6 +67,7 @@ class RandomRatingWins {
   RandomRatingWins(RandomRatingWins&&) = delete;
   RandomRatingWins& operator= (RandomRatingWins&&) = delete;
 
- protected: ~RandomRatingWins() = default;
+ protected:
+  ~RandomRatingWins() = default;
 };
 }  // namespace kahypar

commit 37228bc08517e8b91b26e2b0de8dcd17ddab617c

fix regression introduced in 37c3f95053d6e39da0ddbcb9709572fc4d267d05
Due do missing

if (_hg.edgeSize(he) <= _context.partition.hyperedge_size_threshold) {
...
}

local search took up to 2x longer and partitioning results got worse.
Unfortunately this was not caught by the end_to_end integration tests.

kahypar/partition/coarsening/heavy_edge_rater.h
@@ -110,11 +110,13 @@ class HeavyEdgeRater {
     const PartitionID part_u = _hg.partID(u);
     for (const HyperedgeID& he : _hg.incidentEdges(u)) {
       ASSERT(_hg.edgeSize(he) > 1, V(he));
-      const RatingType score = static_cast<RatingType>(_hg.edgeWeight(he)) / (_hg.edgeSize(he) - 1);
-      for (const HypernodeID& v : _hg.pins(he)) {
-        if (v != u && belowThresholdNodeWeight(weight_u, _hg.nodeWeight(v)) &&
-            (part_u == _hg.partID(v))) {
-          _tmp_ratings[v] += score;
+      if (_hg.edgeSize(he) <= _context.partition.hyperedge_size_threshold) {
+        const RatingType score = static_cast<RatingType>(_hg.edgeWeight(he)) / (_hg.edgeSize(he) - 1);
+        for (const HypernodeID& v : _hg.pins(he)) {
+          if (v != u && belowThresholdNodeWeight(weight_u, _hg.nodeWeight(v)) &&
+              (part_u == _hg.partID(v))) {
+            _tmp_ratings[v] += score;
+          }
         }
       }
     }
@@ -135,23 +137,17 @@ class HeavyEdgeRater {
       }
     }
 
-    _tmp_ratings.clear();
     HeavyEdgeRating ret;
     if (max_rating != std::numeric_limits<RatingType>::min()) {
       ASSERT(target != std::numeric_limits<HypernodeID>::max(), "invalid contraction target");
+      ASSERT(_tmp_ratings[target] == max_rating, V(target));
       ret.value = max_rating;
       ret.target = target;
       ret.valid = true;
       ASSERT(_comm[u] == _comm[ret.target]);
     }
-    ASSERT([&]() {
-        bool flag = true;
-        if (ret.valid && (_hg.partID(u) != _hg.partID(ret.target))) {
-          flag = false;
-        }
-        return flag;
-      } (), "Representative" << u << "& contraction target" << ret.target
-                             << "are in different parts!");
+    ASSERT(!ret.valid || (_hg.partID(u) == _hg.partID(ret.target)));
+    _tmp_ratings.clear();
     DBG << "rating=(" << ret.value << "," << ret.target << "," << ret.valid << ")";
     return ret;
   }