856001470b492ed96ca6b842551bab57d76c9d46
Sebastian Schlag <sebastian.schlag@kit.edu>
2017-05-05 12:49:45 +0200

    fix file names

---
+++ scripts/run_regression_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Checking out HEAD~1..."
+git checkout HEAD~1
+echo "Running tests..."
+make regression_tests
+echo "Checking out current commit..."
+git checkout -
+echo "Cleanup..."
+make clean
+echo "Running tests..."
+make regression_tests
