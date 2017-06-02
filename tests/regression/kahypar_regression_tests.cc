kahypar/kahypar

commit 901176a6fe78ccd756af84c9adba9867d63e1b26

Recursive bisection community detection (#14)
* move community structure into hypergraph

* remove community detection from rater

This version only works for direct k-way partitioning as described
in the SEA'17 paper. There is more work to do to get recursive
bisection done.

* fix warnings

* integrate community detection into multilevel rb

* allow community structure to be reused in subsequent bisections

* add community info to testcases

* verify communities after reindexing

* output new flag in context and sqlplottools

* allow direct k-way IP to reuse community structure across bisections

* remove whitespace

kahypar/partition/partitioner.h
@@ -180,7 +180,7 @@ inline void Partitioner::setupContext(const Hypergraph& hypergraph, Context& con
 inline void Partitioner::preprocess(Hypergraph& hypergraph, const Context& context) {
   if (context.partition.verbose_output) {
     LOG << "\n********************************************************************************";
-    LOG << "*                               Preprocessing...                               *";
+    LOG << "*                          Top Level Preprocessing..                           *";
     LOG << "********************************************************************************";
   }
   const auto result = _single_node_he_remover.removeSingleNodeHyperedges(hypergraph);
@@ -201,6 +201,13 @@ inline void Partitioner::preprocess(Hypergraph& hypergraph, const Context& conte
       context.stats.topLevel().preprocessing("LargeHEremovalTime") +=
         std::chrono::duration<double>(end - start).count();
     }
+  }
+
+  // In recursive bisection mode, we perform community detection before each
+  // bisection. Therefore the 'top-level' preprocessing is disabled in this case.
+  if (context.partition.mode != Mode::recursive_bisection &&
+      context.preprocessing.enable_louvain_community_detection) {
+    detectCommunities(hypergraph, context);
   }
 }
 

config/km1_direct_kway_sea17.ini
@@ -17,6 +17,7 @@ p-large-net-removal=false
 # main -> preprocessing -> community detection
 p-use-louvain=true
 p-use-louvain-in-ip=true
+p-reuse-communities=false
 p-max-louvain-pass-iterations=100
 p-min-eps-improvement=0.0001
 p-louvain-edge-weight=hybrid

kahypar/application/command_line_options.h
@@ -202,7 +202,11 @@ void processCommandLineInput(Context& context, int argc, char* argv[]) {
     " - uniform\n"
     " - non_uniform\n"
     " - degree \n"
-    "(default: hybrid)");
+    "(default: hybrid)")
+    ("p-reuse-communities",
+    po::value<bool>(&context.preprocessing.louvain_community_detection.reuse_communities)->value_name("<bool>"),
+    "Reuse the community structure identified in the first bisection for all other bisections.\n"
+    "(default: false)");
 
   po::options_description coarsening_options("Coarsening Options", num_columns);
   coarsening_options.add_options()

kahypar/datastructure/hypergraph.h
@@ -518,6 +518,7 @@ class GenericHypergraph {
     _hypernodes(_num_hypernodes, Hypernode(0, 0, 1)),
     _hyperedges(_num_hyperedges, Hyperedge(0, 0, 1)),
     _incidence_array(2 * _num_pins, 0),
+    _communities(_num_hypernodes, 0),
     _part_info(_k),
     _pins_in_part(_num_hyperedges * k),
     _connectivity_sets(_num_hyperedges, k),
@@ -594,6 +595,7 @@ class GenericHypergraph {
     _hypernodes(),
     _hyperedges(),
     _incidence_array(),
+    _communities(),
     _part_info(_k),
     _pins_in_part(),
     _connectivity_sets(),
@@ -1356,6 +1358,7 @@ class GenericHypergraph {
   // ! Resets the hypergraph to initial state after construction
   void reset() {
     resetPartitioning();
+    std::fill(_communities.begin(), _communities.end(), 0);
     for (HyperedgeID i = 0; i < _num_hyperedges; ++i) {
       hyperedge(i).hash = kEdgeHashSeed;
       for (const HypernodeID& pin : pins(i)) {
@@ -1640,6 +1643,22 @@ class GenericHypergraph {
     return _total_weight;
   }
 
+  // ! Returns the community structure of the hypergraph
+  const std::vector<PartitionID> & communities() const {
+    return _communities;
+  }
+
+  // ! Sets the community structure of the hypergraph
+  void setCommunities(std::vector<PartitionID>&& communities) {
+    ASSERT(communities.size() == _current_num_hypernodes);
+    _communities = std::move(communities);
+  }
+
+  void resetCommunities() {
+    std::fill(_communities.begin(), _communities.end(), 0);
+  }
+
+
   // ! Returns the sum of the weights of all hypernodes in a block
   HypernodeWeight partWeight(const PartitionID id) const {
     ASSERT(id < _k && id != kInvalidPartition, "Partition ID" << id << "is out of bounds");
@@ -1680,6 +1699,10 @@ class GenericHypergraph {
   FRIEND_TEST(AHypergraph, ExtractedFromAPartitionedHypergraphHasInitializedPartitionInformation);
   FRIEND_TEST(AHypergraph, RemovesEmptyHyperedgesOnHypernodeIsolation);
   FRIEND_TEST(AHypergraph, RestoresRemovedEmptyHyperedgesOnRestoreOfIsolatedHypernodes);
+  FRIEND_TEST(APartitionedHypergraph, CanBeDecomposedIntoHypergraphs);
+  FRIEND_TEST(AHypergraph, WithContractedHypernodesCanBeReindexed);
+  FRIEND_TEST(AHypergraph,
+              WithOnePartitionEqualsTheExtractedHypergraphExceptForPartitionRelatedInfos);
 
   /*!
    * Returns true if hypernode is a border-node.
@@ -1971,6 +1994,9 @@ class GenericHypergraph {
   // ! Incidence structure containing the ids of of pins of all hyperedges
   // ! and the ids of the incident edges of all hypernodes.
   std::vector<VertexID> _incidence_array;
+  // ! Stores the community structure revealed by community detection algorithms.
+  // ! If community detection is disabled, all HNs are in the same community.
+  std::vector<PartitionID> _communities;
 
   // ! Weight and size information for all blocks.
   std::vector<PartInfo> _part_info;
@@ -2026,6 +2052,7 @@ bool verifyEquivalenceWithoutPartitionInfo(const Hypergraph& expected, const Hyp
          V(expected._current_num_pins) << V(actual._current_num_pins));
   ASSERT(expected._hypernodes == actual._hypernodes, "Error!");
   ASSERT(expected._hyperedges == actual._hyperedges, "Error!");
+  ASSERT(expected._communities == actual._communities, "Error!");
 
   std::vector<unsigned int> expected_incidence_array(expected._incidence_array);
   std::vector<unsigned int> actual_incidence_array(actual._incidence_array);
@@ -2045,6 +2072,7 @@ bool verifyEquivalenceWithoutPartitionInfo(const Hypergraph& expected, const Hyp
          expected._current_num_pins == actual._current_num_pins &&
          expected._hypernodes == actual._hypernodes &&
          expected._hyperedges == actual._hyperedges &&
+         expected._communities == actual._communities &&
          expected_incidence_array == actual_incidence_array;
 }
 
@@ -2055,6 +2083,7 @@ bool verifyEquivalenceWithPartitionInfo(const Hypergraph& expected, const Hyperg
 
   ASSERT(expected._part_info == actual._part_info, "Error");
   ASSERT(expected._pins_in_part == actual._pins_in_part, "Error");
+  ASSERT(expected._communities == actual._communities, "Error");
 
   bool connectivity_sets_valid = true;
   for (const HyperedgeID& he : actual.edges()) {
@@ -2071,12 +2100,17 @@ bool verifyEquivalenceWithPartitionInfo(const Hypergraph& expected, const Hyperg
   }
 
   bool num_incident_cut_hes_valid = true;
+  bool community_structure_valid = true;
   for (const HypernodeID& hn : actual.nodes()) {
-    ASSERT(expected.hypernode(hn).num_incident_cut_hes == actual.hypernode(hn).num_incident_cut_hes
-           , V(hn));
+    ASSERT(expected.hypernode(hn).num_incident_cut_hes == actual.hypernode(hn).num_incident_cut_hes,
+           V(hn));
+    ASSERT(expected._communities[hn] == actual._communities[hn], V(hn));
     if (expected.hypernode(hn).num_incident_cut_hes != actual.hypernode(hn).num_incident_cut_hes) {
       num_incident_cut_hes_valid = false;
       break;
+    }
+    if (expected._communities[hn] != actual._communities[hn]) {
+      community_structure_valid = false;
     }
   }
 
@@ -2084,6 +2118,7 @@ bool verifyEquivalenceWithPartitionInfo(const Hypergraph& expected, const Hyperg
          expected._part_info == actual._part_info &&
          expected._pins_in_part == actual._pins_in_part &&
          num_incident_cut_hes_valid &&
+         community_structure_valid &&
          connectivity_sets_valid;
 }
 
@@ -2106,6 +2141,19 @@ reindex(const Hypergraph& hypergraph) {
     original_to_reindexed[hn] = reindexed_to_original.size();
     reindexed_to_original.push_back(hn);
     ++num_hypernodes;
+  }
+
+  if (!hypergraph._communities.empty()) {
+    reindexed_hypergraph->_communities.resize(num_hypernodes, -1);
+    for (const HypernodeID& hn : hypergraph.nodes()) {
+      const HypernodeID reindexed_hn = original_to_reindexed[hn];
+      reindexed_hypergraph->_communities[reindexed_hn] = hypergraph._communities[hn];
+    }
+    ASSERT(std::none_of(reindexed_hypergraph->_communities.cbegin(),
+                        reindexed_hypergraph->_communities.cend(),
+                        [](typename Hypergraph::PartitionID i) {
+          return i == -1;
+        }));
   }
 
   reindexed_hypergraph->_hypernodes.resize(num_hypernodes);
@@ -2192,6 +2240,19 @@ extractPartAsUnpartitionedHypergraphForBisection(const Hypergraph& hypergraph,
   if (num_hypernodes > 0) {
     subhypergraph->_hypernodes.resize(num_hypernodes);
     subhypergraph->_num_hypernodes = num_hypernodes;
+
+    if (!hypergraph._communities.empty()) {
+      subhypergraph->_communities.resize(num_hypernodes, -1);
+      for (const HypernodeID& subhypergraph_hn : subhypergraph->nodes()) {
+        const HypernodeID original_hn = subhypergraph_to_hypergraph[subhypergraph_hn];
+        subhypergraph->_communities[subhypergraph_hn] = hypergraph._communities[original_hn];
+      }
+      ASSERT(std::none_of(subhypergraph->_communities.cbegin(),
+                          subhypergraph->_communities.cend(),
+                          [](typename Hypergraph::PartitionID i) {
+            return i == -1;
+          }));
+    }
 
     HyperedgeID num_hyperedges = 0;
     HypernodeID pin_index = 0;

kahypar/io/sql_plottools_serializer.h
@@ -82,6 +82,8 @@ static inline void serialize(const Context& context, const Hypergraph& hypergrap
       << " max_louvain_pass_iterations=" << context.preprocessing.louvain_community_detection.max_pass_iterations
       << " min_louvain_eps_improvement=" << context.preprocessing.louvain_community_detection.min_eps_improvement
       << " louvain_edge_weight=" << toString(context.preprocessing.louvain_community_detection.edge_weight)
+      << " reuse_community_structure=" << std::boolalpha
+      << context.preprocessing.louvain_community_detection.reuse_communities
       << " coarsening_algo=" << toString(context.coarsening.algorithm)
       << " coarsening_max_allowed_weight_multiplier=" << context.coarsening.max_allowed_weight_multiplier
       << " coarsening_contraction_limit_multiplier=" << context.coarsening.contraction_limit_multiplier

kahypar/partition/coarsening/heavy_edge_rater.h
@@ -78,23 +78,7 @@ class HeavyEdgeRater {
     _hg(hypergraph),
     _context(context),
     _tmp_ratings(_hg.initialNumNodes()),
-    _comm(),
-    _already_matched(_hg.initialNumNodes()) {
-    if (_context.preprocessing.enable_louvain_community_detection) {
-      const bool verbose_output = (_context.type == ContextType::main &&
-                                   _context.partition.verbose_output);
-      if (verbose_output) {
-        LOG << "Performing community detection:";
-      }
-      _comm = detectCommunities(_hg, _context);
-      if (verbose_output) {
-        LOG << "  # communities = " << context.stats.preprocessing("Communities");
-        LOG << "  modularity    = " << context.stats.preprocessing("Modularity");
-      }
-    } else {
-      _comm.resize(_hg.initialNumNodes(), 0);
-    }
-  }
+    _already_matched(_hg.initialNumNodes()) { }
 
   HeavyEdgeRater(const HeavyEdgeRater&) = delete;
   HeavyEdgeRater& operator= (const HeavyEdgeRater&) = delete;
@@ -123,13 +107,14 @@ class HeavyEdgeRater {
 
     RatingType max_rating = std::numeric_limits<RatingType>::min();
     HypernodeID target = std::numeric_limits<HypernodeID>::max();
+    const std::vector<PartitionID>& communities = _hg.communities();
     for (auto it = _tmp_ratings.end() - 1; it >= _tmp_ratings.begin(); --it) {
       const HypernodeID tmp_target = it->key;
       const RatingType tmp_rating = it->value /
                                     NodeWeightPenalty::penalty(weight_u,
                                                                _hg.nodeWeight(tmp_target));
       DBG << "r(" << u << "," << tmp_target << ")=" << tmp_rating;
-      if (_comm[u] == _comm[tmp_target] &&
+      if (communities[u] == communities[tmp_target] &&
           AcceptancePolicy::acceptRating(tmp_rating, max_rating,
                                          target, tmp_target, _already_matched)) {
         max_rating = tmp_rating;
@@ -143,7 +128,7 @@ class HeavyEdgeRater {
       ret.value = max_rating;
       ret.target = target;
       ret.valid = true;
-      ASSERT(_comm[u] == _comm[ret.target]);
+      ASSERT(communities[u] == communities[ret.target]);
     }
     ASSERT(!ret.valid || (_hg.partID(u) == _hg.partID(ret.target)));
     _tmp_ratings.clear();
@@ -172,7 +157,6 @@ class HeavyEdgeRater {
   Hypergraph& _hg;
   const Context& _context;
   ds::SparseMap<HypernodeID, RatingType> _tmp_ratings;
-  std::vector<ClusterID> _comm;
   ds::FastResetFlagArray<> _already_matched;
 };
 }  // namespace kahypar

kahypar/partition/context.h
@@ -45,6 +45,7 @@ struct MinHashSparsifierParameters {
 
 struct LouvainCommunityDetection {
   bool enable_in_initial_partitioning = false;
+  bool reuse_communities = false;
   LouvainEdgeWeight edge_weight = LouvainEdgeWeight::hybrid;
   int max_pass_iterations = 100;
   long double min_eps_improvement = 0.0001;
@@ -88,6 +89,8 @@ inline std::ostream& operator<< (std::ostream& str, const LouvainCommunityDetect
       << params.min_eps_improvement << std::endl;
   str << "  graph edge weight:                  "
       << toString(params.edge_weight) << std::endl;
+  str << "  reuse community structure:          " << std::boolalpha
+      << params.reuse_communities << std::endl;
   return str;
 }
 
@@ -110,7 +113,7 @@ inline std::ostream& operator<< (std::ostream& str, const PreprocessingParameter
   if (params.enable_louvain_community_detection) {
     str << "-------------------------------------------------------------------------------"
         << std::endl;
-    str << params.louvain_community_detection << std::endl;
+    str << params.louvain_community_detection;
   }
 
   return str;

kahypar/partition/initial_partition.h
@@ -137,7 +137,7 @@ static inline Context createContext(const Hypergraph& hg,
       LOG << "Invalid IP technique";
       std::exit(-1);
   }
-  // We are now in initial partitioning mode, i.e. the next call to partitionInitially
+  // We are now in initial partitioning mode, i.e. the next call to partition
   // will actually trigger the computation of an initial partition of the hypergraph.
   // Computing an actual initial partition is always flat, since the graph has been coarsened
   // before in case of multilevel initial partitioning, or should not be coarsened in case
@@ -155,7 +155,6 @@ static inline Context createContext(const Hypergraph& hg,
 static inline void partition(Hypergraph& hg, const Context& context) {
   auto extracted_init_hypergraph = ds::reindex(hg);
   std::vector<HypernodeID> mapping(std::move(extracted_init_hypergraph.second));
-
   double init_alpha = context.initial_partitioning.init_alpha;
   double best_imbalance = std::numeric_limits<double>::max();
   std::vector<PartitionID> best_imbalanced_partition(
@@ -163,6 +162,8 @@ static inline void partition(Hypergraph& hg, const Context& context) {
 
   do {
     extracted_init_hypergraph.first->resetPartitioning();
+    // we do not want to use the community structure used during coarsening in initial partitioning
+    extracted_init_hypergraph.first->resetCommunities();
     Context init_context = createContext(*extracted_init_hypergraph.first, context, init_alpha);
 
     if (context.initial_partitioning.verbose_output) {

commit 753b66fc896dfcb271bfbef37c1e633e667e2b6e

Bugfix: unused communities in case of sparsification
This fixes a bug introduced in commit
901176a6fe78ccd756af84c9adba9867d63e1b26 .

Since community detecion was moved into the preprocessing method of
the partitioner, it was performed directly after single node net
removal. Unfortunately, this meant that in case of sparsifiacation,
community detection was performed on the original hypergraph and then
sparsification build the new sparsified hypergraph (with empty
community structure), which was then partitioned.

Along with the bugfix, this commit also contains assertions and a
regression testcase, which ensure that community information is used
corrently if the sparsifier is enabled.

kahypar/partition/partitioner.h
@@ -54,6 +54,17 @@ class APartitionedHypergraph;
 
 namespace partition {
 static inline void partition(Hypergraph& hypergraph, const Context& context) {
+  ASSERT([&]() {
+        if (context.partition.mode != Mode::recursive_bisection &&
+            context.preprocessing.enable_community_detection) {
+          return !std::all_of(hypergraph.communities().cbegin(),
+                              hypergraph.communities().cend(),
+                              [](auto i) {
+            return i == 0;
+          });
+        }
+        return true;
+      } ());
   switch (context.partition.mode) {
     case Mode::recursive_bisection:
       recursive_bisection::partition(hypergraph, context);
@@ -107,6 +118,8 @@ class Partitioner {
   static inline void setupContext(const Hypergraph& hypergraph, Context& context);
 
   static inline void configurePreprocessing(const Hypergraph& hypergraph, Context& context);
+
+  inline void sanitize(Hypergraph& hypergraph, const Context& context);
 
   inline void preprocess(Hypergraph& hypergraph, const Context& context);
   inline void preprocess(Hypergraph& hypergraph, Hypergraph& sparse_hypergraph,
@@ -172,7 +185,7 @@ inline void Partitioner::setupContext(const Hypergraph& hypergraph, Context& con
                                                     * context.partition.total_graph_weight);
 }
 
-inline void Partitioner::preprocess(Hypergraph& hypergraph, const Context& context) {
+inline void Partitioner::sanitize(Hypergraph& hypergraph, const Context& context) {
   io::printTopLevelPreprocessingBanner(context);
   const auto result = _single_node_he_remover.removeSingleNodeHyperedges(hypergraph);
   if (context.partition.verbose_output && result.num_removed_single_node_hes > 0) {
@@ -181,7 +194,9 @@ inline void Partitioner::preprocess(Hypergraph& hypergraph, const Context& conte
     LOG << "\033[1m\033[31m" << "===>" << result.num_unconnected_hns
         << "unconnected HNs could have been removed" << "\033[0m";
   }
+}
 
+inline void Partitioner::preprocess(Hypergraph& hypergraph, const Context& context) {
   // In recursive bisection mode, we perform community detection before each
   // bisection. Therefore the 'top-level' preprocessing is disabled in this case.
   if (context.partition.mode != Mode::recursive_bisection &&
@@ -192,7 +207,6 @@ inline void Partitioner::preprocess(Hypergraph& hypergraph, const Context& conte
 
 inline void Partitioner::preprocess(Hypergraph& hypergraph, Hypergraph& sparse_hypergraph,
                                     const Context& context) {
-  preprocess(hypergraph, context);
   ASSERT(context.preprocessing.enable_min_hash_sparsifier);
 
   const HighResClockTimepoint start = std::chrono::high_resolution_clock::now();
@@ -208,6 +222,7 @@ inline void Partitioner::preprocess(Hypergraph& hypergraph, Hypergraph& sparse_h
     LOG << "After sparsification:";
     kahypar::io::printHypergraphInfo(sparse_hypergraph, "sparsified hypergraph");
   }
+  preprocess(sparse_hypergraph, context);
 }
 
 inline void Partitioner::postprocess(Hypergraph& hypergraph, const Context& context) {
@@ -233,6 +248,9 @@ inline void Partitioner::partition(Hypergraph& hypergraph, Context& context) {
 
   setupContext(hypergraph, context);
   io::printInputInformation(context, hypergraph);
+
+  sanitize(hypergraph, context);
+
   if (context.preprocessing.min_hash_sparsifier.is_active) {
     Hypergraph sparseHypergraph;
     preprocess(hypergraph, sparseHypergraph, context);

tests/regression/kahypar_regression_tests.cc
@@ -142,4 +142,32 @@ TEST_F(KaHyParCA, ComputesDirectKwayKm1Partitioning) {
 
   test("kahypar-ca-km1.results", hypergraph, context);
 }
+
+TEST_F(KaHyParCA, UsesSparsificationAndCommunityDetection) {
+  context.partition.k = 8;
+  context.partition.epsilon = 0.03;
+  context.partition.seed = 10;
+  context.partition.objective = Objective::km1;
+  context.local_search.algorithm = RefinementAlgorithm::kway_fm_km1;
+  context.preprocessing.enable_community_detection = true;
+  context.preprocessing.enable_min_hash_sparsifier = true;
+  context.coarsening.algorithm = CoarseningAlgorithm::ml_style;
+  context.coarsening.rating.heavy_node_penalty_policy = HeavyNodePenaltyPolicy::no_penalty;
+  context.coarsening.rating.acceptance_policy = AcceptancePolicy::best_prefer_unmatched;
+  context.partition.graph_filename = "test_instances/af_4_k101.mtx.hgr";
+
+  Hypergraph hypergraph(
+    kahypar::io::createHypergraphFromFile(context.partition.graph_filename,
+                                          context.partition.k));
+
+  Partitioner partitioner;
+  partitioner.partition(hypergraph, context);
+  kahypar::io::printPartitioningResults(hypergraph, context, std::chrono::duration<double>(0.0));
+
+  Hypergraph verification_hypergraph(
+    kahypar::io::createHypergraphFromFile(context.partition.graph_filename,
+                                          context.partition.k));
+
+  test("kahypar-ca-km1-comm-sparse.results", hypergraph, context);
+}
 }  // namespace kahypar