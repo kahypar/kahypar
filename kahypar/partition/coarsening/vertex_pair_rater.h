64cdb864498b8c003f01a8874e967bb206108422
Sebastian Schlag <sebastian.schlag@kit.edu>
2017-06-06 11:03:16 +0200

    remove dead code

--- kahypar/partition/coarsening/vertex_pair_rater.h
+++ kahypar/partition/coarsening/vertex_pair_rater.h
@@ -92,19 +92,13 @@ class VertexPairRater {
   VertexPairRating rate(const HypernodeID u) {
     DBG << "Calculating rating for HN" << u;
     const HypernodeWeight weight_u = _hg.nodeWeight(u);
-    const PartitionID part_u = _hg.partID(u);
     for (const HyperedgeID& he : _hg.incidentEdges(u)) {
       ASSERT(_hg.edgeSize(he) > 1, V(he));
       if (_hg.edgeSize(he) <= _context.partition.hyperedge_size_threshold) {
         const RatingType score = ScorePolicy::score(_hg, he, _context);
         for (const HypernodeID& v : _hg.pins(he)) {
-          /* if (v != u && belowThresholdNodeWeight(weight_u, _hg.nodeWeight(v)) &&
-               part_u == _hg.partID(v)) {
-             _tmp_ratings[v] += score;
-           }*/
-          // TODO(robin): cleanup
-          RatingType accept = RatingPartitionPolicy::accept(_hg, _context, u, v);
-          if (v != u && belowThresholdNodeWeight(weight_u, _hg.nodeWeight(v)) && RatingPartitionPolicy::accept(_hg, _context, u, v)) {
+          if (v != u && belowThresholdNodeWeight(weight_u, _hg.nodeWeight(v)) &&
+              RatingPartitionPolicy::accept(_hg, _context, u, v)) {
             _tmp_ratings[v] += score;
           }
         }
