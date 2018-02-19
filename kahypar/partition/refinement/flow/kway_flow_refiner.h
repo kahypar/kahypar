fba2d62f5e113512242f0a7f90ee5dd9f593bc09
Sebastian Schlag <info@sebastianschlag.de>
2018-02-19 08:28:20 +0100

    improve const correctness and codestyle

--- kahypar/partition/refinement/flow/kway_flow_refiner.h
+++ kahypar/partition/refinement/flow/kway_flow_refiner.h
@@ -21,32 +21,32 @@
 
 #pragma once
 
+#include <algorithm>
 #include <array>
+#include <cmath>
+#include <queue>
 #include <string>
 #include <utility>
-#include <queue>
-#include <cmath>
 #include <vector>
 
-#include "kahypar/definitions.h"
-#include "kahypar/partition/metrics.h"
-#include "kahypar/partition/context.h"
-#include "kahypar/partition/refinement/i_refiner.h"
-#include "kahypar/partition/refinement/flow/2way_flow_refiner.h"
-#include "kahypar/partition/refinement/flow/quotient_graph_block_scheduler.h"
 #include "kahypar/datastructure/fast_reset_array.h"
 #include "kahypar/datastructure/fast_reset_flag_array.h"
 #include "kahypar/datastructure/sparse_set.h"
+#include "kahypar/definitions.h"
+#include "kahypar/partition/context.h"
+#include "kahypar/partition/metrics.h"
+#include "kahypar/partition/refinement/flow/2way_flow_refiner.h"
+#include "kahypar/partition/refinement/flow/quotient_graph_block_scheduler.h"
+#include "kahypar/partition/refinement/i_refiner.h"
 
 namespace kahypar {
-
 using ds::SparseSet;
 using ds::FastResetArray;
 
-template<class FlowNetworkPolicy = Mandatory,
-         class FlowExecutionPolicy = Mandatory>
+template <class FlowNetworkPolicy = Mandatory,
+          class FlowExecutionPolicy = Mandatory>
 class KWayFlowRefiner final : public IRefiner {
-using FlowNetwork = typename FlowNetworkPolicy::Network;
+  using FlowNetwork = typename FlowNetworkPolicy::Network;
 
  public:
   KWayFlowRefiner(Hypergraph& hypergraph, const Context& context) :
@@ -54,7 +54,7 @@ using FlowNetwork = typename FlowNetworkPolicy::Network;
     _context(context),
     _twoway_flow_refiner(_hg, _context),
     _flow_execution_policy(),
-    _num_improvements(context.partition.k, std::vector<size_t>(context.partition.k, 0)) {}
+    _num_improvements(context.partition.k, std::vector<size_t>(context.partition.k, 0)) { }
 
   KWayFlowRefiner(const KWayFlowRefiner&) = delete;
   KWayFlowRefiner(KWayFlowRefiner&&) = delete;
@@ -70,7 +70,7 @@ using FlowNetwork = typename FlowNetworkPolicy::Network;
                   const UncontractionGainChanges& changes,
                   Metrics& best_metrics) override final {
     if (!_flow_execution_policy.executeFlow(_hg)) {
-        return false;
+      return false;
     }
 
     DBG << "############### Active Block Scheduling ###############";
@@ -92,43 +92,43 @@ using FlowNetwork = typename FlowNetworkPolicy::Network;
     std::vector<bool> active_blocks(_context.partition.k, true);
     size_t current_round = 1;
     while (active_block_exist) {
-        scheduler.randomShuffleQoutientEdges();
-        std::vector<bool> tmp_active_blocks(_context.partition.k, false);
-        active_block_exist = false;
-        for (const auto& e : scheduler.qoutientGraphEdges()) {
-            PartitionID block_0 = e.first;
-            PartitionID block_1 = e.second;
+      scheduler.randomShuffleQoutientEdges();
+      std::vector<bool> tmp_active_blocks(_context.partition.k, false);
+      active_block_exist = false;
+      for (const auto& e : scheduler.qoutientGraphEdges()) {
+        const PartitionID block_0 = e.first;
+        const PartitionID block_1 = e.second;
 
-            // Heuristic: If a flow refinement never improved a bipartition,
-            //            we ignore the refinement for these block in the
-            //            second iteration of active block scheduling
-            if (_context.local_search.flow.use_improvement_history &&
-                current_round > 1 && _num_improvements[block_0][block_1] == 0 ) {
-                continue;
-            }
-
-            if (active_blocks[block_0] || active_blocks[block_1]) {
-                 _twoway_flow_refiner.updateConfiguration(block_0, block_1,
-                                                        &scheduler, true);
-                bool improved = _twoway_flow_refiner.refine(refinement_nodes,
-                                                          max_allowed_part_weights,
-                                                          changes,
-                                                          best_metrics);
-                if (improved) {
-                    DBG << "Improvement found beetween blocks " << block_0 << " and "
-                                                                << block_1 << " in round #"
-                                                                << current_round;
-                    printMetric();
-                    improvement = true;
-                    active_block_exist = true;
-                    tmp_active_blocks[block_0] = true;
-                    tmp_active_blocks[block_1] = true;
-                    _num_improvements[block_0][block_1]++;
-                }
-            }
+        // Heuristic: If a flow refinement never improved a bipartition,
+        //            we ignore the refinement for these block in the
+        //            second iteration of active block scheduling
+        if (_context.local_search.flow.use_improvement_history &&
+            current_round > 1 && _num_improvements[block_0][block_1] == 0) {
+          continue;
         }
-        current_round++;
-        std::swap(active_blocks, tmp_active_blocks);
+
+        if (active_blocks[block_0] || active_blocks[block_1]) {
+          _twoway_flow_refiner.updateConfiguration(block_0, block_1,
+                                                   &scheduler, true);
+          const bool improved = _twoway_flow_refiner.refine(refinement_nodes,
+                                                            max_allowed_part_weights,
+                                                            changes,
+                                                            best_metrics);
+          if (improved) {
+            DBG << "Improvement found beetween blocks " << block_0 << " and "
+                << block_1 << " in round #"
+                << current_round;
+            printMetric();
+            improvement = true;
+            active_block_exist = true;
+            tmp_active_blocks[block_0] = true;
+            tmp_active_blocks[block_1] = true;
+            _num_improvements[block_0][block_1]++;
+          }
+        }
+      }
+      current_round++;
+      std::swap(active_blocks, tmp_active_blocks);
     }
 
     printMetric(true, true);
@@ -139,13 +139,13 @@ using FlowNetwork = typename FlowNetworkPolicy::Network;
 
   void printMetric(bool newline = false, bool endline = false) {
     if (newline) {
-        DBG << "";
+      DBG << "";
     }
     DBG << V(metrics::imbalance(_hg, _context))
         << V(_context.partition.objective)
         << V(metrics::objective(_hg, _context.partition.objective));
     if (endline) {
-        DBG << "-------------------------------------------------------------";
+      DBG << "-------------------------------------------------------------";
     }
   }
 
@@ -161,6 +161,6 @@ using FlowNetwork = typename FlowNetworkPolicy::Network;
   const Context& _context;
   TwoWayFlowRefiner<FlowNetworkPolicy, FlowExecutionPolicy> _twoway_flow_refiner;
   FlowExecutionPolicy _flow_execution_policy;
-  std::vector<std::vector<size_t>> _num_improvements;
+  std::vector<std::vector<size_t> > _num_improvements;
 };
 }  // namespace kahypar
