24c798bd2992a8e77ee32f3ef648247b47364be9
kittobi1992 <tobias.heuer@gmx.net>
2018-04-05 16:19:15 +0200

    implement first version of fixed vertex assignment after recursive bisection

--- kahypar/partition/fixed_vertices.h
+++ kahypar/partition/fixed_vertices.h
@@ -22,15 +22,382 @@
 #pragma once
 
 #include <algorithm>
+#include <cmath>
+#include <limits>
+#include <queue>
 #include <vector>
+#include <utility>
 
 #include "kahypar/definitions.h"
 #include "kahypar/io/hypergraph_io.h"
 #include "kahypar/partition/context.h"
+#include "kahypar/datastructure/fast_reset_flag_array.h"
 
 namespace kahypar {
 namespace fixed_vertices {
+
+using NodeID = int32_t;
+using AdjacencyMatrix = std::vector<std::vector<HyperedgeWeight>>;
+using Matching = std::vector<std::pair<PartitionID, PartitionID>>;
+using VertexCover = std::vector<NodeID>;
 static constexpr bool debug = false;
+
+class BipartiteMaximumFlow {
+using Flow = HyperedgeWeight;
+
+ public:
+  explicit BipartiteMaximumFlow(const AdjacencyMatrix& graph) :
+    _num_nodes(2*graph.size() + 2),
+    _residualGraph(_num_nodes, std::vector<HypernodeWeight>(_num_nodes, 0)),
+    _visited(_num_nodes),
+    _parent(_num_nodes, -1),
+    _source(_num_nodes - 2),
+    _sink(_num_nodes - 1) {
+    PartitionID k = graph.size();
+    for (PartitionID i = 0; i < k; ++i) {
+      for (PartitionID j = 0; j < k; ++j) {
+        if (graph[i][j]) {
+          // All nodes from left side of the bipartite graph are labeled from 0..(k-1)
+          // All nodes from right side of the bipartite graph are labeled from k..(2k-1)
+          NodeID u = i;
+          NodeID v = j + k;
+          _residualGraph[u][v] = 1;
+        }
+      }
+    }
+
+    // Connect source to all left side nodes of the bipartite graph (Source Node: 2k)
+    for (PartitionID i = 0; i < k; ++i) {
+      _residualGraph[2*k][i] = 1;
+    }
+
+    // Connect all right side nodes of the bipartite graph to sink (Sink Node: 2k + 1)
+    for (PartitionID i = 0; i < k; ++i) {
+      _residualGraph[i + k][2*k + 1] = 1;
+    }
+  }
+
+  BipartiteMaximumFlow(const BipartiteMaximumFlow&) = delete;
+  BipartiteMaximumFlow(BipartiteMaximumFlow&&) = delete;
+  BipartiteMaximumFlow& operator= (const BipartiteMaximumFlow&) = delete;
+  BipartiteMaximumFlow& operator= (BipartiteMaximumFlow&&) = delete;
+  ~BipartiteMaximumFlow() = default;
+
+  /**
+   * Edmond-Karps Maximum Flow Algorithm
+   * Returns the value of the maximum flow.
+   */
+  Flow maximumFlow() {
+    Flow max_flow = 0;
+    while (bfs(_source)) {
+      max_flow += augment(_sink);
+    }
+    return max_flow;
+  }
+
+ private:
+  /**
+   * Increases the flow along the augmenting path found
+   * by the BFS traversal of the residual graph.
+   * Returns the amount of flow which is send over
+   * the augmenting path.
+   * NOTE: Function should be called with sink node.
+   */
+  Flow augment(NodeID u, Flow flow = std::numeric_limits<Flow>::max()) {
+    if (_parent[u] == -1) {
+      return flow;
+    }
+    Flow min_flow = augment(_parent[u], std::min(flow, _residualGraph[_parent[u]][u]));
+    _residualGraph[_parent[u]][u] -= min_flow;
+    _residualGraph[u][_parent[u]] += min_flow;
+    return min_flow;
+  }
+
+ protected:
+  /**
+   * Breath-First-Search starting from source node source 
+   * and searching for sink node sink in the residual graph.
+   * Returns true, if a path from s to t in the residual 
+   * graph exists.
+   */
+  bool bfs(NodeID node, bool reset = true) {
+    if (reset) {
+      _visited.reset();
+    }
+    std::queue<NodeID> q;
+    q.push(node);
+    _visited.set(node, true);
+    _parent[node] = -1;
+
+    while (!q.empty()) {
+      NodeID u = q.front();
+      q.pop();
+      if (u == _sink) {
+        return true;
+      }
+
+      for (NodeID v = 0; v < _num_nodes; ++v) {
+        if (!_visited[v] && _residualGraph[u][v]) {
+          q.push(v);
+          _visited.set(v, true);
+          _parent[v] = u;
+        }
+      }
+    }
+    return false;
+  }
+
+  size_t _num_nodes;
+  AdjacencyMatrix _residualGraph;
+  ds::FastResetFlagArray<> _visited;
+  std::vector<NodeID> _parent;
+  NodeID _source;
+  NodeID _sink;
+};
+
+class MaximumBipartiteMatching : public BipartiteMaximumFlow {
+using NodeID = int32_t;
+using Flow = HyperedgeWeight;
+
+ public:
+  explicit MaximumBipartiteMatching(const AdjacencyMatrix& graph) :
+    BipartiteMaximumFlow(graph) { }
+
+  MaximumBipartiteMatching(const MaximumBipartiteMatching&) = delete;
+  MaximumBipartiteMatching(MaximumBipartiteMatching&&) = delete;
+  MaximumBipartiteMatching& operator= (const MaximumBipartiteMatching&) = delete;
+  MaximumBipartiteMatching& operator= (MaximumBipartiteMatching&&) = delete;
+  ~MaximumBipartiteMatching() = default;
+
+  Matching findMaximumBipartiteMatching() {
+    Matching matching;
+    PartitionID k = _num_nodes/2 - 1;
+    NodeID source = _num_nodes - 2;
+    NodeID sink = _num_nodes - 1;
+    Flow max_flow = maximumFlow();
+
+    for (PartitionID i = 0; i < k; ++i) {
+      for (PartitionID j = 0; j < k; ++j) {
+        NodeID u = i;
+        NodeID v = j + k;
+        // If (i,j) is part of maximum bipartite matching,
+        // than edge (v,u) must be in the residual graph
+        // of a maximum flow
+        if (_residualGraph[v][u]) {
+          matching.push_back(std::make_pair(i, j));
+        }
+      }
+    }
+
+    ASSERT(max_flow == matching.size(), "Maximum Matching calculation failed");
+    return matching;
+  }
+
+ protected:
+  using BipartiteMaximumFlow::_num_nodes;
+  using BipartiteMaximumFlow::_residualGraph;
+};
+
+class MinimumBipartiteVertexCover final : public MaximumBipartiteMatching {
+using NodeID = int32_t;
+using Flow = HyperedgeWeight;
+
+ public:
+  explicit MinimumBipartiteVertexCover(const AdjacencyMatrix& graph) :
+    MaximumBipartiteMatching(graph),
+    _maximum_matching_already_calculated(false),
+    _maximum_matching() { }
+
+  MinimumBipartiteVertexCover(const MinimumBipartiteVertexCover&) = delete;
+  MinimumBipartiteVertexCover(MinimumBipartiteVertexCover&&) = delete;
+  MinimumBipartiteVertexCover& operator= (const MinimumBipartiteVertexCover&) = delete;
+  MinimumBipartiteVertexCover& operator= (MinimumBipartiteVertexCover&&) = delete;
+  ~MinimumBipartiteVertexCover() = default;
+
+  VertexCover findMinimumBipartiteVertexCover() {
+    VertexCover cover;
+    PartitionID k = _num_nodes/2 - 1;
+
+    if (_maximum_matching_already_calculated) {
+      computeMaximumBipartiteMatching();
+    }
+
+    // Compute all unmatched left side vertices
+    _visited.reset();
+    for (auto matched_edge : _maximum_matching) {
+      NodeID l = matched_edge.first;
+      _visited.set(l, true);
+    }
+    std::vector<NodeID> unmatched_vertices;
+    for (NodeID u = 0; u < k; ++u) {
+      if (!_visited[u]) {
+        unmatched_vertices.push_back(u);
+      }
+    }
+
+    // Remove source and sink from residual graph
+    for (NodeID u = 0; u < k; ++u) {
+      _residualGraph[_source][u] = 0;
+      _residualGraph[u][_source] = 0;
+      _residualGraph[u + k][_sink] = 0;
+      _residualGraph[_sink][u + k] = 0;
+    }
+
+    // Construct set H of vertices reachable via an alternating
+    // path from unmatched vertices of the left side.
+    // NOTE: Set H is marked as visited after BFS
+    _visited.reset();
+    for (NodeID u : unmatched_vertices) {
+      bfs(u, false);
+    }
+    // Let L be the left side and R be the right side
+    // of the bipartite graph, than a minimum vertex
+    // cover is defined as follows C := (L \ H) \cup (R \cap H)
+    // L \ H:
+    for (NodeID u = 0; u < k; ++u) {
+      if (!_visited[u]) {
+        cover.push_back(u);
+      }
+    }
+    // R \cap H:
+    for (NodeID u = k; u < 2*k; ++u) {
+      if (_visited[u]) {
+        cover.push_back(u);
+      }
+    }
+    // Note: L-vertices are labeled from 0..(k-1) and
+    //       R-vertices are labeled from k..(2k-1)
+    ASSERT(cover.size() == _maximum_matching.size(), "Minimum Vertex Cover computation failed");
+    return cover;
+  }
+
+  Matching computeMaximumBipartiteMatching() {
+    if (!_maximum_matching_already_calculated) {
+      _maximum_matching = findMaximumBipartiteMatching();
+      _maximum_matching_already_calculated = true;
+    }
+    return _maximum_matching;
+  }
+
+ private:
+  using BipartiteMaximumFlow::_num_nodes;
+  using BipartiteMaximumFlow::_residualGraph;
+  using BipartiteMaximumFlow::_visited;
+  using BipartiteMaximumFlow::_source;
+  using BipartiteMaximumFlow::_sink;
+
+  bool _maximum_matching_already_calculated;
+  Matching _maximum_matching;
+};
+
+static inline AdjacencyMatrix setupWeightedBipartiteMatchingGraph(Hypergraph& input_hypergraph,
+                                                                  const Context& original_context) {
+  PartitionID k = original_context.partition.k;
+  AdjacencyMatrix graph(k, std::vector<HyperedgeWeight>(k, 0));
+
+  std::vector<std::vector<HypernodeID>> fixed_vertices(k, std::vector<HypernodeID>());
+  for (const HypernodeID& hn : input_hypergraph.fixedVertices()) {
+    fixed_vertices[input_hypergraph.fixedVertexPartID(hn)].push_back(hn);
+  }
+
+  ds::FastResetFlagArray<> visited(input_hypergraph.initialNumEdges());
+  for (PartitionID i = 0; i < k; ++i) {
+    visited.reset();
+    for (const HypernodeID& hn : fixed_vertices[i]) {
+      for (const HyperedgeID& he : input_hypergraph.incidentEdges(hn)) {
+        if (!visited[he]) {
+          for (const PartitionID& j : input_hypergraph.connectivitySet(he)) {
+            graph[i][j] += input_hypergraph.edgeWeight(he);
+          }
+          visited.set(he, true);
+        }
+      }
+    }
+  }
+
+  return graph;
+}
+
+/*static inline void printAdjacencyMatrix(const AdjacencyMatrix& graph) {
+  PartitionID k = graph.size();
+  LOG << "MATCHING GRAPH:";
+  for (PartitionID i = 0; i < k; ++i) {
+    for (PartitionID j = 0; j < k; ++j) {
+      std::cout << graph[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
+}*/
+
+static inline Matching findMaximumWeightedBipartiteMatching(const AdjacencyMatrix& graph) {
+  PartitionID k = graph.size();
+  std::vector<HyperedgeWeight> u(k, 0);
+  std::vector<HyperedgeWeight> v(k, 0);
+  for (PartitionID i = 0; i < k; ++i) {
+    u[i] = *std::max_element(graph[i].begin(), graph[i].end());
+  }
+
+  auto construct_matching_graph = [&u, &v, &graph, k]() {
+    AdjacencyMatrix matching_graph(k, std::vector<HyperedgeWeight>(k, 0));
+    for (PartitionID i = 0; i < k; ++i) {
+      for (PartitionID j = 0; j < k; ++j) {
+        if (u[i] + v[j] == graph[i][j]) {
+          matching_graph[i][j] = 1;
+        }
+      }
+    }
+    return matching_graph;
+  };
+
+  auto excess = [&u, &v, &graph](PartitionID i, PartitionID j) {
+    return u[i] + v[i] - graph[i][j];
+  };
+
+  Matching matching;
+  while (true) {
+    AdjacencyMatrix matching_graph = construct_matching_graph();
+    MinimumBipartiteVertexCover vertex_cover(matching_graph);
+    matching = vertex_cover.computeMaximumBipartiteMatching();
+
+    if (matching.size() == k) {
+      break;
+    } else {
+      VertexCover cover = vertex_cover.findMinimumBipartiteVertexCover();
+
+      // (Un)Matched vertices from left and right side of bipartite graph
+      std::vector<bool> L(k, false);
+      std::vector<bool> R(k, false);
+      for (NodeID u : cover) {
+        if (u < k) {
+          L[u] = true;
+        } else {
+          R[u - k] = true;
+        }
+      }
+
+      // Compute minimum excess between all vertices not in the vertex cover
+      HyperedgeWeight min_excess = std::numeric_limits<HyperedgeWeight>::max();
+      for (PartitionID i = 0; i < k; ++i) {
+        for (PartitionID j = 0; j < k; ++j) {
+          if (!L[i] && !R[j]) {
+            min_excess = std::min(excess(i, j), min_excess);
+          }
+        }
+      }
+
+      // Update minimum cost vertex cover vectors
+      for (PartitionID i = 0; i < k; ++i) {
+        if (!L[i]) {
+          u[i] -= min_excess;
+        }
+        if (R[i]) {
+          v[i] += min_excess;
+        }
+      }
+    }
+  }
+  return matching;
+}
 
 static inline void partition(Hypergraph& input_hypergraph,
                              const Context& original_context) {
@@ -49,8 +416,23 @@ static inline void partition(Hypergraph& input_hypergraph,
       return true;
     } (), "Precondition check for fixed vertex assignment failed");
 
-  for (const HypernodeID& hn : input_hypergraph.fixedVertices()) {
-    input_hypergraph.setNodePart(hn, input_hypergraph.fixedVertexPartID(hn));
+  AdjacencyMatrix graph = setupWeightedBipartiteMatchingGraph(input_hypergraph, original_context);
+  Matching maximum_weighted_matching = findMaximumWeightedBipartiteMatching(graph);
+  std::vector<PartitionID> partition_permutation(original_context.partition.k, 0);
+  for (auto matched_edge : maximum_weighted_matching) {
+    partition_permutation[matched_edge.second] = matched_edge.first;
+  }
+
+  for (const HypernodeID& hn : input_hypergraph.nodes()) {
+    if (input_hypergraph.isFixedVertex(hn)) {
+      input_hypergraph.setNodePart(hn, input_hypergraph.fixedVertexPartID(hn));
+    } else {
+      PartitionID from = input_hypergraph.partID(hn);
+      PartitionID to = partition_permutation[from];
+      if (from != to) {
+        input_hypergraph.changeNodePart(hn, from, to);
+      }
+    }
   }
 }
 }  // namespace fixed_vertices
