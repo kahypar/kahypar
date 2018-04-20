b02d5ea53310a3dc5aa63f641457572ec1994921
Sebastian Schlag <sebastian.schlag@kit.edu>
2018-04-20 14:33:25 +0200

    improve memory management of k-way gain cache

--- kahypar/partition/refinement/kway_fm_gain_cache.h
+++ kahypar/partition/refinement/kway_fm_gain_cache.h
@@ -47,14 +47,14 @@ class KwayGainCache {
     _cache_element_size(sizeof(KFMCacheElement) +
                         _k * sizeof(typename KFMCacheElement::Element) +
                         _k * sizeof(PartitionID)),
-    _cache(std::make_unique<Byte[]>(num_hns * _cache_element_size)),
-    _deltas() {
-    for (HypernodeID hn = 0; hn < _num_hns; ++hn) {
-      new(cacheElement(hn))KFMCacheElement(k);
+    _cache(std::make_unique<KFMCacheElement*[]>(num_hns)),
+    _deltas() { }
+
+  ~KwayGainCache() {
+    for (size_t i = 0; i < _num_hns; ++i) {
+      delete[] (reinterpret_cast<Byte*>(_cache[i]));
     }
   }
-
-  ~KwayGainCache() = default;
 
   KwayGainCache(const KwayGainCache&) = delete;
   KwayGainCache& operator= (const KwayGainCache&) = delete;
@@ -64,6 +64,7 @@ class KwayGainCache {
 
   KAHYPAR_ATTRIBUTE_ALWAYS_INLINE Gain entry(const HypernodeID hn, const PartitionID part) const {
     DBGC(hn == hn_to_debug) << "entry access for HN" << hn << "and part" << part;
+    ASSERT(_cache[hn] != nullptr);
     ASSERT(part < _k, V(part));
     return cacheElement(hn)->gain(part);
   }
@@ -73,12 +74,19 @@ class KwayGainCache {
     ASSERT(part < _k, V(part));
     DBGC(hn == hn_to_debug) << "existence check for HN" << hn << "and part" << part
                             << "=" << cacheElement(hn)->contains(part);
-    return cacheElement(hn)->contains(part);
+    return _cache[hn] != nullptr && cacheElement(hn)->contains(part);
   }
+
+  KAHYPAR_ATTRIBUTE_ALWAYS_INLINE bool entryExists(const HypernodeID hn) const {
+    DBGC(hn == hn_to_debug) << "existence check for HN" << hn;
+    return _cache[hn] != nullptr;
+  }
+
 
   KAHYPAR_ATTRIBUTE_ALWAYS_INLINE void removeEntryDueToConnectivityDecrease(const HypernodeID hn,
                                                                             const PartitionID part) {
     ASSERT(part < _k, V(part));
+    ASSERT(_cache[hn] != nullptr);
     _deltas.emplace_back(hn, part, cacheElement(hn)->gain(part), RollbackAction::do_add);
     DBGC(hn == hn_to_debug) << "removeEntryDueToConnectivityDecrease for" << hn
                             << "and part" << part << "previous cache entry ="
@@ -91,6 +99,9 @@ class KwayGainCache {
                                                                          const PartitionID part,
                                                                          const Gain gain) {
     ASSERT(part < _k, V(part));
+    if (unlikely(_cache[hn] == nullptr)) {
+      _cache[hn] = new(new Byte[_cache_element_size])KFMCacheElement(_k);
+    }
     ASSERT(!entryExists(hn, part), V(hn) << V(part));
     cacheElement(hn)->add(part, gain);
     DBGC(hn == hn_to_debug) << "addEntryDueToConnectivityIncrease for" << hn
@@ -104,6 +115,7 @@ class KwayGainCache {
                                                                     const PartitionID from_part,
                                                                     const PartitionID to_part,
                                                                     const bool remains_connected_to_from_part) {
+    ASSERT(_cache[moved_hn] != nullptr);
     if (remains_connected_to_from_part) {
       DBGC(moved_hn == hn_to_debug) << "updateFromAndToPartOfMovedHN(" << moved_hn
                                     << "," << from_part << "," << to_part << ")";
@@ -125,12 +137,17 @@ class KwayGainCache {
 
   KAHYPAR_ATTRIBUTE_ALWAYS_INLINE void clear(const HypernodeID hn) {
     DBGC(hn == hn_to_debug) << "clear(" << hn << ")";
-    cacheElement(hn)->clear();
+    if (_cache[hn] != nullptr) {
+      cacheElement(hn)->clear();
+    }
   }
 
   void initializeEntry(const HypernodeID hn, const PartitionID part, const Gain value) {
     ASSERT(part < _k, V(part));
     DBGC(hn == hn_to_debug) << "initializeEntry(" << hn << "," << part << "," << value << ")";
+    if (unlikely(_cache[hn] == nullptr)) {
+      _cache[hn] = new(new Byte[_cache_element_size])KFMCacheElement(_k);
+    }
     cacheElement(hn)->add(part, value);
   }
 
@@ -146,6 +163,7 @@ class KwayGainCache {
   KAHYPAR_ATTRIBUTE_ALWAYS_INLINE void updateExistingEntry(const HypernodeID hn,
                                                            const PartitionID part,
                                                            const Gain delta) {
+    ASSERT(_cache[hn] != nullptr);
     ASSERT(part < _k, V(part));
     ASSERT(entryExists(hn, part), V(hn) << V(part));
     ASSERT(cacheElement(hn)->gain(part) != kNotCached, V(hn) << V(part));
@@ -190,24 +208,27 @@ class KwayGainCache {
 
   void clear() {
     for (HypernodeID hn = 0; hn < _num_hns; ++hn) {
-      new(cacheElement(hn))KFMCacheElement(_k);
+      if (_cache[hn] != nullptr) {  /// workaround
+        delete[] (reinterpret_cast<Byte*>(_cache[hn]));
+        _cache[hn] = new(new Byte[_cache_element_size])KFMCacheElement(_k);
+      }
     }
   }
 
  private:
   const KFMCacheElement* cacheElement(const HypernodeID hn) const {
-    return reinterpret_cast<KFMCacheElement*>(_cache.get() + hn * _cache_element_size);
+    return _cache[hn];
   }
 
   // To avoid code duplication we implement non-const version in terms of const version
-  KFMCacheElement* cacheElement(const HypernodeID he) {
-    return const_cast<KFMCacheElement*>(static_cast<const KwayGainCache&>(*this).cacheElement(he));
+  KFMCacheElement* cacheElement(const HypernodeID hn) {
+    return _cache[hn];
   }
 
   PartitionID _k;
   HypernodeID _num_hns;
   const size_t _cache_element_size;
-  std::unique_ptr<Byte[]> _cache;
+  std::unique_ptr<KFMCacheElement*[]> _cache;
   std::vector<RollbackElement> _deltas;
 };
 
