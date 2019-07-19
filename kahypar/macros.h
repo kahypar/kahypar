ccacdc86d7fe49f5087f1ae0f543b490331c408b
Sebastian Schlag <info@sebastianschlag.de>
2019-07-19 09:05:14 +0200

    allow to switch to standard assersions via cmake

--- kahypar/macros.h
+++ kahypar/macros.h
@@ -150,7 +150,11 @@ debug output will disappear.
 #endif
 
 #ifdef KAHYPAR_USE_ASSERTIONS
-  #define ASSERT_2(cond, msg)                 \
+#ifdef KAHYPAR_USE_STANDARD_ASSERTIONS
+#define ASSERT_2(cond, msg) \
+  assert(cond)
+#else
+#define ASSERT_2(cond, msg)                   \
   do {                                        \
     if (!(cond)) {                            \
       DBG1 << "Assertion `" #cond "` failed:" \
@@ -158,6 +162,7 @@ debug output will disappear.
       std::abort();                           \
     }                                         \
   } while (0)
+#endif
 
   #define ASSERT_1(cond) ASSERT_2(cond, "")
 #else
@@ -170,6 +175,10 @@ debug output will disappear.
 #define ASSERT(...) EXPAND(ASSERT_EVAL(EXPAND(NARG(__VA_ARGS__)))(__VA_ARGS__))
 
 // *** an always-on ASSERT
+#ifdef KAHYPAR_USE_STANDARD_ASSERTIONS
+#define ALWAYS_ASSERT_2(cond, msg) \
+  assert(cond)
+# else
 #define ALWAYS_ASSERT_2(cond, msg)            \
   do {                                        \
     if (!(cond)) {                            \
@@ -178,6 +187,7 @@ debug output will disappear.
       std::abort();                           \
     }                                         \
   } while (0)
+#endif
 
 #define ALWAYS_ASSERT_1(cond) ALWAYS_ASSERT_2(cond, "")
 
