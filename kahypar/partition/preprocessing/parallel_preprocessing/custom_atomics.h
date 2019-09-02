406b035da7fff44b40d6d195f8e1142fc9c81708
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2019-09-02 11:22:27 +0200

    floating point fetch_add CAS

---
+++ kahypar/partition/preprocessing/parallel_preprocessing/custom_atomics.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <atomic>
+#include <stdatomic.h>
+
+namespace kahypar {
+namespace parallel {
+
+template<typename T>
+void fetch_add(std::atomic<T>& x, T y) {
+	T cur_x = x.load();
+	while(!x.compare_exchange_weak(cur_x, cur_x + y, std::memory_order_relaxed));
+}
+
+template<typename T>
+void fetch_sub(std::atomic<T>& x, T y) {
+	T cur_x = x.load();
+	while(!x.compare_exchange_weak(cur_x, cur_x - y, std::memory_order_relaxed));
+}
+
+template<class T>
+class AtomicWrapper : public std::atomic<T> {
+public:
+	void operator+=(T other) {
+		fetch_add(*this, other);
+	}
+
+	void operator-=(T other) {
+		fetch_sub(*this, other);
+	}
+};
+
+}
+}
