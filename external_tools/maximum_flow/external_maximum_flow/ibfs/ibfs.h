28a9b66e0090554b6521e8e2e933a82f9292d332
Sebastian Schlag <sebastian.schlag@kit.edu>
2019-10-14 08:40:12 +0200

    Update ibfs.h
    silence // lgtm[external/cwe/cwe-190]

--- external_tools/maximum_flow/external_maximum_flow/ibfs/ibfs.h
+++ external_tools/maximum_flow/external_maximum_flow/ibfs/ibfs.h
@@ -192,16 +192,16 @@ public:
 	{
 		Node*		head;
 		Arc*		rev;
-		int			isRevResidual :1;
-		int			rCap :31;
+		int			isRevResidual :1; // lgtm[external/cwe/cwe-190]
+		int			rCap :31; // lgtm[external/cwe/cwe-190]
 		FlowEdge*   flowEdge;
 	};
 
 	struct Node
 	{
-		int			lastAugTimestamp:30;
-		int			isParentCurr:1;
-		int			isIncremental:1;
+		int			lastAugTimestamp:30; // lgtm[external/cwe/cwe-190]
+		int			isParentCurr:1; // lgtm[external/cwe/cwe-190]
+		int			isIncremental:1; // lgtm[external/cwe/cwe-190]
 		Arc			*firstArc;
 		Arc			*parent;
 		Node		*firstSon;
