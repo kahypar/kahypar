311003f202190e2116e1d256a482de597318b401
MalleusMalleficarum <dahonk1995>
2019-12-19 17:46:23 +0100

    added copyrights

--- tests/partition/mpi/exchanger_test.cc
+++ tests/partition/mpi/exchanger_test.cc
@@ -1,8 +1,8 @@
 /*******************************************************************************
  * This file is part of KaHyPar.
  *
- * Copyright (C) 2018 Sebastian Schlag <sebastian.schlag@kit.edu>
- * Copyright (C) 2018 Robin Andre <robinandre1995@web.de>
+ * Copyright (C) 2019 Sebastian Schlag <sebastian.schlag@kit.edu>
+ * Copyright (C) 2019 Robin Andre <robinandre1995@web.de>
  *
  * KaHyPar is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
