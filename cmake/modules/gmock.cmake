00e42b9dd1b0b8bc2a9194046ddb53c106db901d
Sebastian Schlag <info@sebastianschlag.de>
2020-01-12 14:06:30 +0100

    simplify mpi test calls

--- cmake/modules/gmock.cmake
+++ cmake/modules/gmock.cmake
@@ -13,9 +13,9 @@ function(add_gmock_test target)
                        COMMAND ${target}
                        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
                        COMMENT "Running ${target}" VERBATIM)
-  
-endfunction()                     
-function(add_gmock_mpi_test target)
+endfunction()   
+
+function(add_gmock_mpi_test target dummy nprocs)
     add_executable(${target} ${target}.cc)
     target_link_libraries(${target} gtest gtest_main ${MPI_LIBRARIES} ${Boost_LIBRARIES} ${CMAKE_THREAD_LIBS_INIT})
 
@@ -24,25 +24,9 @@ function(add_gmock_mpi_test target)
 
     add_test(${target} ${target})
     
-
     add_custom_command(TARGET ${target}
                        POST_BUILD
-                       COMMAND ${MPIEXEC} -np 2 ${target}
-                       WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-                       COMMENT "Running ${target}" VERBATIM)
-endfunction()
-function(add_gmock_mpi_test4 target)
-    add_executable(${target} ${target}.cc)
-    target_link_libraries(${target} gtest gtest_main ${MPI_LIBRARIES} ${Boost_LIBRARIES} ${CMAKE_THREAD_LIBS_INIT})
-
-    set_property(TARGET ${target} PROPERTY CXX_STANDARD 14)
-    set_property(TARGET ${target} PROPERTY CXX_STANDARD_REQUIRED ON)
-
-    add_test(${target} ${target})
-
-    add_custom_command(TARGET ${target}
-                       POST_BUILD
-                       COMMAND ${MPIEXEC} -np 4 ${target}
+                       COMMAND ${MPIEXEC} -np ${nprocs} ${target}
                        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
                        COMMENT "Running ${target}" VERBATIM)
 endfunction()
