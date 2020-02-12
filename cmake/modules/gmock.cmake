0e647796cae27082e8063b03724ea98315176667
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2020-02-12 13:19:21 +0100

    raise C++ standard to 17 on all targets

--- cmake/modules/gmock.cmake
+++ cmake/modules/gmock.cmake
@@ -3,7 +3,7 @@ function(add_gmock_test target)
     add_executable(${target} ${ARGN})
     target_link_libraries(${target} gtest gtest_main ${CMAKE_THREAD_LIBS_INIT})
 
-    set_property(TARGET ${target} PROPERTY CXX_STANDARD 14)
+    set_property(TARGET ${target} PROPERTY CXX_STANDARD 17)
     set_property(TARGET ${target} PROPERTY CXX_STANDARD_REQUIRED ON)
 
     add_test(${target} ${target})
