8e623dd3f729358c953d607a20eadd9221ae4384
Sebastian Schlag <github@sebastianschlag.de>
2020-05-13 23:58:54 +0200

    Delete install_boost.bash

--- scripts/install_boost.bash
+++
@@ -1,5 +0,0 @@
-curl -OL http://downloads.sourceforge.net/project/boost/boost/1.69.0/boost_1_69_0.tar.bz2
-tar -xjf boost_1_69_0.tar.bz2
-cd boost_1_69_0
-./bootstrap.sh --prefix=/usr/local
-./b2 install --prefix=/usr/local --with-program_options
