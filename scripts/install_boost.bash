b70573e5eb227382c6dfde87f1e424ec65f74505
Sebastian Schlag <github@sebastianschlag.de>
2020-05-13 23:41:53 +0200

    Update install_boost.bash

--- scripts/install_boost.bash
+++ scripts/install_boost.bash
@@ -1,4 +1,4 @@
-wget http://downloads.sourceforge.net/project/boost/boost/1.69.0/boost_1_69_0.tar.bz2
+curl -OL http://downloads.sourceforge.net/project/boost/boost/1.69.0/boost_1_69_0.tar.bz2
 tar -xjf boost_1_69_0.tar.bz2
 cd boost_1_69_0
 ./bootstrap.sh --prefix=/usr/local
