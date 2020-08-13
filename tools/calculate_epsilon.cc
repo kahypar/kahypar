fa171053d62b20e62f6a3d67863fd3f8ddf3f6e1
Nikolai Maas <nikolai.maas@student.kit.edu>
2020-08-13 11:26:24 +0200

    fix mail

--- tools/calculate_epsilon.cc
+++ tools/calculate_epsilon.cc
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * This file is part of KaHyPar.
  *
- * Copyright (C) 2020 Nikolai Maas <nikolai.maas@kit.edu>
+ * Copyright (C) 2020 Nikolai Maas <nikolai.maas@student.kit.edu>
  *
  * KaHyPar is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
