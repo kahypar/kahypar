c303e66bbea33d419f4b60043d99fbacfc581518
Nikolai Maas <nikolai.maas@student.kit.edu>
2020-09-24 16:54:35 +0200

    fix for some tests

--- tests/partition/metrics_test.cc
+++ tests/partition/metrics_test.cc
@@ -90,6 +90,7 @@ class APartitionedHypergraph : public Test {
     context.initial_partitioning.technique = InitialPartitioningTechnique::flat;
     context.initial_partitioning.algo = InitialPartitionerAlgorithm::pool;
     context.initial_partitioning.nruns = 20;
+    context.initial_partitioning.bp_algo = BinPackingAlgorithm::worst_fit;
     context.initial_partitioning.local_search.algorithm = RefinementAlgorithm::twoway_fm;
     context.initial_partitioning.local_search.fm.max_number_of_fruitless_moves = 50;
     context.initial_partitioning.local_search.fm.stopping_rule = RefinementStoppingRule::simple;
