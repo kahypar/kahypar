a44b4c228a58667f7d2944e3faf4c148eb6764e5
Sebastian Schlag <github@sebastianschlag.de>
2022-10-17 21:54:30 -0700

    Update build_macos_locally.sh

--- scripts/python_wheels/build_macos_locally.sh
+++ scripts/python_wheels/build_macos_locally.sh
@@ -1,14 +1,11 @@
-# using cibuildwheel
-python3 -m pip install cibuildwheel==1.4.1
-
-# python2.7 does not work well with C++17
-export CIBW_SKIP=cp27-macosx_x86_64
+#install latest version of cibuildwheel 
+python3 -m pip install cibuildwheel
 
 # make sure we use the packaged minimal boost implementation, as cibuildwheel doesn't seem to properly handle the boost dependency.
 export KAHYPAR_USE_MINIMAL_BOOST=ON
 
 # create wheels
-python3 -m cibuildwheel --output-dir wheelhouse --platform macos
+cibuildwheel --platform macos 
 
 # upload
 twine upload wheelhouse/*
